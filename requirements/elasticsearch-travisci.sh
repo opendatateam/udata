fafc01c35e94f089467efba432df994a7221bcb6
Axel Haustant <noirbizarre@gmail.com>
2014-05-06 00:03:38 +0200

    Use TravisCI provided ES

--- requirements/elasticsearch-travisci.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-wget https://download.elasticsearch.org/elasticsearch/elasticsearch/elasticsearch-1.1.0.deb
-dpkg -i elasticsearch-1.1.0.deb
-service elasticsearch start
-/usr/share/elasticsearch/bin/plugin -install elasticsearch/elasticsearch-analysis-icu/2.0.0
