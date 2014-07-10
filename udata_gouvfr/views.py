17ec85f2b344929632b9a8ff41b0b1dcfdf9d80a
Axel Haustant <noirbizarre@gmail.com>
2014-07-10 09:33:07 +0200

    Added redevances page

--- udata_gouvfr/views.py
+++ udata_gouvfr/views.py
@@ -3,10 +3,11 @@ from __future__ import unicode_literals
 
 from flask import url_for, redirect
 
+from udata.frontend import render
 from udata.i18n import I18nBlueprint
 
 
-blueprint = I18nBlueprint('gouvfr', __name__)
+blueprint = I18nBlueprint('gouvfr', __name__, template_folder='templates')
 
 
 @blueprint.route('/dataset/<dataset>/')
@@ -17,3 +18,8 @@ def redirect_datasets(dataset):
 @blueprint.route('/organization/<org>/')
 def redirect_organizations(org):
     return redirect(url_for('organizations.show', org=org))
+
+
+@blueprint.route('/Redevances')
+def redevances():
+    return render('redevances.html')
