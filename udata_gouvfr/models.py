e98197df0c3b1bb580cadd321480dd2aaaa62194
Axel Haustant <noirbizarre@gmail.com>
2014-08-04 18:54:45 +0200

    Decalre Territorial coverage as a dataset extra

--- udata_gouvfr/models.py
+++ udata_gouvfr/models.py
@@ -1,4 +1,9 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
+from udata.models import Dataset, TerritorialCoverage
+
 # TODO: service_public flag as an extension
+
+#TODO Remove territorial coverage from core
+Dataset.extras.register('territorial_coverage', TerritorialCoverage)
