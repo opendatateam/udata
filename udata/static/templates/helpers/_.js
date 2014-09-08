ef54cd4124158b46db91f30407b488f93ca9037c
Axel Haustant <noirbizarre@gmail.com>
2014-09-08 11:53:59 +0200

    Handle plural translations in Handlebars templates

--- udata/static/templates/helpers/_.js
+++ udata/static/templates/helpers/_.js
@@ -1,7 +1,7 @@
 define(['hbs/handlebars', 'i18n'], function ( Handlebars, i18n ) {
 
-    Handlebars.registerHelper('_', function(value) {
-        return value && typeof value === 'string' ? i18n._(value) : '';
+    Handlebars.registerHelper('_', function(value, options) {
+        return value && typeof value === 'string' ? i18n._(value, options.hash) : '';
     });
 
 });
