9a91a6bd06c729daa4a57387f532c07f2207559f
Axel Haustant <noirbizarre@gmail.com>
2014-09-12 03:07:05 +0200

    Search view new layouts

--- udata/static/js/search.js
+++ udata/static/js/search.js
@@ -11,6 +11,19 @@ define([
     return {
         start: function() {
             log.debug('search started');
+
+            // Display toolbar depending on active tab
+            $('.search-tabs a[data-toggle="tab"]').on('shown.bs.tab', function (e) {
+                var $tab = $(e.target);
+                $('.btn-toolbar.wrapper').each(function() {
+                    var $this = $(this);
+                    if ($this.data('tab') == $tab.attr('href')) {
+                        $this.removeClass('hide');
+                    } else {
+                        $this.addClass('hide');
+                    }
+                });
+            })
         }
     };
 });
