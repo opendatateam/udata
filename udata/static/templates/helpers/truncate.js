047edf1f42071c2eeabb95a0a748fca52fe6a973
Axel Haustant <noirbizarre@gmail.com>
2014-09-22 15:10:19 +0200

    Fix truncate for undefined values

--- udata/static/templates/helpers/truncate.js
+++ udata/static/templates/helpers/truncate.js
@@ -3,6 +3,9 @@ define(['hbs/handlebars'], function(Handlebars) {
     var DEFAULT_LENGTH = 128;
 
     Handlebars.registerHelper('truncate', function(value, length) {
+        if (!value) {
+            return;
+        }
         return new Handlebars.SafeString(value.substring(0, length) + '...');
     });
 
