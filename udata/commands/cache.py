66ffa60ac83fd9d32b0170fec7a17586fffb952d
Axel Haustant <noirbizarre@gmail.com>
2014-10-04 09:57:54 +0200

    Added cache commands

---
+++ udata/commands/cache.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+import logging
+
+from udata.commands import submanager
+from udata.app import cache
+
+log = logging.getLogger(__name__)
+
+
+m = submanager('cache',
+    help='Cache related operations',
+    description='Handle all cache related operations and maintenance'
+)
+
+
+@m.command
+def flush():
+    '''Flush the cache'''
+    print('Flusing cache')
+    cache.clear()
+    print('Cache flushed')
