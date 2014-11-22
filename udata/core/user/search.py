bc04aaad5618b9714c84d64709908d644fe9ea65
Axel Haustant <noirbizarre@gmail.com>
2014-11-22 14:35:39 +0100

    Fix typo in user serialization

--- udata/core/user/search.py
+++ udata/core/user/search.py
@@ -69,7 +69,7 @@ class UserSearch(ModelSearchAdapter):
             'metrics': user.metrics,
             'created': user.created_at.strftime('%Y-%m-%dT%H:%M:%S'),
             'user_suggest': {
-                'input': cls.completer_tokenizer(user.full_name),
+                'input': cls.completer_tokenize(user.fullname),
                 'payload': {
                     'id': str(user.id),
                     'avatar_url': user.avatar(40),
