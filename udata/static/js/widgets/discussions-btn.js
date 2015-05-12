5c253024ad5a42ab09a009a222b0a208be9604a2
David Larlet <david@larlet.fr>
2015-05-12 18:15:40 +0200

    Adding animations on discussions, refs #18

--- udata/static/js/widgets/discussions-btn.js
+++ udata/static/js/widgets/discussions-btn.js
@@ -5,6 +5,24 @@ define(['jquery', 'api', 'auth', 'i18n', 'notify'],
 function($, API, Auth, i18n, Notify) {
     'use strict';
 
+    function hideNewForm(el, form) {
+        form.addClass('fadeOutDown');
+        setTimeout(function() {
+            form.addClass('hidden');
+        }, 1000);
+    }
+
+    function submitNewComment(el, form, data) {
+        API.post(form.data('api-url'), data, function(data) {
+            var msg = i18n._('Your comment has been sent to the server');
+            Notify.success(msg);
+        }).error(function(e) {
+            var msg = i18n._('An error occured while submitting your comment');
+            Notify.error(msg);
+            console.error(e.responseJSON);
+        });
+    }
+
     $('.new-discussion').click(function(e) {
         e.preventDefault();
         var $this = $(this);
@@ -14,7 +32,7 @@ function($, API, Auth, i18n, Notify) {
         }
 
         function displayNewDiscussionForm(el) {
-            $('.list-group-form-discussion').removeClass('hidden');
+            $('.list-group-form-discussion').removeClass('hidden').addClass('fadeInDown');
         }
 
         function hideNewDiscussionButton(el) {
@@ -35,7 +53,7 @@ function($, API, Auth, i18n, Notify) {
 
         function displayNewCommentForm(el) {
             var discussionId = $this.data('discussion-id');
-            $('.list-group-form-' + discussionId).removeClass('hidden');
+            $('.list-group-form-' + discussionId).removeClass('hidden').addClass('fadeInDown');
         }
 
         function hideNewCommentButton(el) {
@@ -52,7 +70,7 @@ function($, API, Auth, i18n, Notify) {
 
         function openDiscussionThread(el) {
             var discussionId = el.parent().data('discussion-id');
-            $('.list-group-thread-' + discussionId).removeClass('hidden');
+            $('.list-group-thread-' + discussionId).removeClass('hidden').addClass('fadeInDown');
             $('.list-group-form-' + discussionId).addClass('hidden');
         }
 
@@ -63,12 +81,9 @@ function($, API, Auth, i18n, Notify) {
         e.preventDefault();
         var $this = $(this);
         var $form = $this.parent();
+        var $newMessage = $('.list-group-new-message');
 
-        if (!Auth.need_user(i18n._('You need to be logged in to add a discussion.'))) {
-            return false;
-        }
-
-        if (!$form.valid()) {
+        if (!Auth.need_user(i18n._('You need to be logged in to add a discussion.')) || !$form.valid()) {
             return false;
         }
 
@@ -77,30 +92,17 @@ function($, API, Auth, i18n, Notify) {
             comment: $form.find('#comment').val()
         };
 
-        function hideNewDiscussionForm(el, form) {
-            form.addClass('hidden');
-        }
-
-        function displayNewDiscussion(el, form, data) {
+        function displayNewDiscussion(el, form, data, newMessage) {
             form.siblings('.list-group-item-heading').text(data.title);
             form.siblings('.list-group-item-text').text(i18n._('Discussion started today with your message.'));
             form.parent().css('height', '54px');
+            newMessage.addClass('fadeInDown').removeClass('hidden');
+            newMessage.children('.list-group-item-heading').text(data.comment);
         }
 
-        function submitNewDiscussion(el, form, data) {
-            API.post(form.data('api-url'), data, function(data) {
-                var msg = i18n._('Your comment has been sent to the server');
-                Notify.success(msg);
-            }).error(function(e) {
-                var msg = i18n._('An error occured while submitting your comment');
-                Notify.error(msg);
-                console.error(e.responseJSON);
-            });
-        }
-
-        hideNewDiscussionForm($this, $form);
-        displayNewDiscussion($this, $form, data);
-        submitNewDiscussion($this, $form, data);
+        hideNewForm($this, $form);
+        displayNewDiscussion($this, $form, data, $newMessage);
+        submitNewComment($this, $form, data);
     });
 
     $('.submit-new-comment').click(function(e) {
@@ -108,21 +110,13 @@ function($, API, Auth, i18n, Notify) {
         var $this = $(this);
         var $form = $this.parent();
 
-        if (!Auth.need_user(i18n._('You need to be logged in to add a comment.'))) {
-            return false;
-        }
-
-        if (!$form.valid()) {
+        if (!Auth.need_user(i18n._('You need to be logged in to add a comment.')) || !$form.valid()) {
             return false;
         }
 
         var data = {
             comment: $form.find('#comment').val()
         };
-
-        function hideNewCommentForm(el, form) {
-            form.addClass('hidden');
-        }
 
         function displayNewComment(el, form, data) {
             var discussionId = form.parent().data('discussion-id');
@@ -132,18 +126,7 @@ function($, API, Auth, i18n, Notify) {
             $('.list-group-message-number-' + discussionId).append(i18n._('and your comment!'));
         }
 
-        function submitNewComment(el, form, data) {
-            API.post(form.data('api-url'), data, function(data) {
-                var msg = i18n._('Your comment has been sent to the server');
-                Notify.success(msg);
-            }).error(function(e) {
-                var msg = i18n._('An error occured while submitting your comment');
-                Notify.error(msg);
-                console.error(e.responseJSON);
-            });
-        }
-
-        hideNewCommentForm($this, $form);
+        hideNewForm($this, $form);
         displayNewComment($this, $form, data);
         submitNewComment($this, $form, data);
     });
