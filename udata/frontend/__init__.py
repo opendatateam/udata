f2b5b927040d230f47ca54a569eeb0d0c40ea149
David Larlet <david@larlet.fr>
2015-05-22 16:52:48 +0200

    Add a FAQ module, fix #28
    As a visitor I want to get basic information tailored for my profile
    (newcomer, producer, developer or jurist) in order to use the website
    appropriately.

--- udata/frontend/__init__.py
+++ udata/frontend/__init__.py
@@ -72,6 +72,7 @@ def init_app(app):
     from udata.core.topic.views import blueprint as topic_blueprint
     from udata.core.post.views import blueprint as post_blueprint
     from udata.core.tags.views import bp as tags_blueprint
+    from udata.core.faq.views import bp as faq_blueprint
 
     app.register_blueprint(storages_blueprint)
     app.register_blueprint(user_blueprint)
@@ -84,6 +85,7 @@ def init_app(app):
     app.register_blueprint(topic_blueprint)
     app.register_blueprint(post_blueprint)
     app.register_blueprint(tags_blueprint)
+    app.register_blueprint(faq_blueprint)
 
     # Load all plugins views and blueprints
     for plugin in app.config['PLUGINS']:
