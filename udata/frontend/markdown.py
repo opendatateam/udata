b7a8b3389670c333581283c4f37c6c64d153d299
David Larlet <david@larlet.fr>
2015-06-30 18:34:48 +0200

    Switch to CommonMark on the backend, fix #82
    Better sanitization too.

--- udata/frontend/markdown.py
+++ udata/frontend/markdown.py
@@ -1,41 +1,60 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from flask.ext.markdown import Markdown
-from mdx_linkify.mdx_linkify import LinkifyExtension
-
-from flask import current_app
+import bleach
+import CommonMark
+from html5lib.sanitizer import HTMLSanitizer
+from flask import current_app, Markup
 from werkzeug.local import LocalProxy
 from jinja2.filters import do_truncate, do_striptags
-
 
 md = LocalProxy(lambda: current_app.extensions['markdown'])
 
 EXCERPT_TOKEN = '<!--- --- -->'
 
 
-class UDataMarkdown(Markdown):
+class KeepTokenSanitizer(HTMLSanitizer):
+    """Keep the `EXCERPT_TOKEN` with bleach."""
+
+    def sanitize_token(self, token):
+        return token
+
+
+class UDataMarkdown(object):
+    """Consistent with Flask's extensions signature."""
+
+    def __init__(self, app, parser, renderer):
+        self.parser = parser
+        self.renderer = renderer
+        app.jinja_env.filters.setdefault('markdown', self.__call__)
+
     def __call__(self, stream):
-        return super(UDataMarkdown, self).__call__(stream or '')
-
-
-def set_nofollow(attrs, new=False):
-    '''Ensure that Markdown links are 'rel="nofollow"' '''
-    attrs['rel'] = 'nofollow'
-    return attrs
+        if not stream:
+            return ''
+        # Sanitize malicious attempts but keep the `EXCERPT_TOKEN`.
+        # By default, only keeps `bleach.ALLOWED_TAGS`.
+        stream = bleach.clean(stream, strip_comments=False)
+        # Turn markdown to HTML.
+        ast = self.parser.parse(stream)
+        html = self.renderer.render(ast)
+        # Turn string links into HTML ones *after* markdown transformation.
+        html = bleach.linkify(html, tokenizer=KeepTokenSanitizer)
+        # Return a `Markup` element considered as safe by Jinja.
+        return Markup(html)
 
 
 def init_app(app):
-    linkify = LinkifyExtension(configs={'linkify_callbacks': [set_nofollow]})
-    app.extensions['markdown'] = UDataMarkdown(app, extensions=[linkify])
+    parser = CommonMark.DocParser()
+    renderer = CommonMark.HTMLRenderer()
+    app.extensions['markdown'] = UDataMarkdown(app, parser, renderer)
 
     @app.template_filter()
     def mdstrip(value, length=None):
         '''
         Truncate and strip tags from a markdown source
 
-        The markdown source is truncated at the excerpt if present and smaller than the required length.
-        Then, all html tags are stripped.
+        The markdown source is truncated at the excerpt if present and
+        smaller than the required length. Then, all html tags are stripped.
         '''
         if not value:
             return ''
