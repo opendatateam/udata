b60dcd92b612396376cd6d2966b7a7b7e78abdd5
David Larlet <david@larlet.fr>
2015-07-02 15:08:58 +0200

    Turn badges into ListField, refs etalab/udata#54

--- udata_gouvfr/commands.py
+++ udata_gouvfr/commands.py
@@ -4,52 +4,40 @@ from __future__ import unicode_literals
 from os.path import exists
 
 from udata.commands import manager
-from udata.models import Organization
+from udata.models import Organization, OrganizationBadge
 
 
-def certify_org(id_or_slug):
-    organization = Organization.objects(slug=id_or_slug).first()
+def toggle(id_or_slug, badge_kind):
+    organization = Organization.objects.get_by_id_or_slug(id_or_slug)
     if not organization:
-        try:
-            organization = Organization.objects(id=id_or_slug).first()
-        except:
-            print 'No organization found for {0}'.format(id_or_slug)
-            return
-    print 'Certifying {0}'.format(organization.name)
-    organization.public_service = True
-    organization.save()
-
-
-def uncertify_org(id_or_slug):
-    organization = Organization.objects(slug=id_or_slug).first()
-    if not organization:
-        try:
-            organization = Organization.objects(id=id_or_slug).first()
-        except:
-            print 'No organization found for {0}'.format(id_or_slug)
-            return
-    print 'Uncertifying {0}'.format(organization.name)
-    organization.public_service = False
+        print 'No organization found for {0}'.format(id_or_slug)
+        return
+    print 'Toggling badge {kind} for organization {org}'.format(
+        kind=badge_kind,
+        org=organization.name
+    )
+    existed = False
+    for badge in organization.badges:
+        if badge.kind == badge_kind:
+            organization.badges.remove(badge)
+            existed = True
+            break
+    if not existed:
+        badge = OrganizationBadge(kind=badge_kind)
+        organization.badges.append(badge)
     organization.save()
 
 
 @manager.command
-def certify(path_or_id):
-    '''Certify an organization as a public service'''
+def toggle_badge(path_or_id, badge_kind):
+    '''Toggle a `badge_kind` for a given `path_or_id`
+
+    The `path_or_id` is either an id, a slug or a file containing a list
+    of ids or slugs.
+    '''
     if exists(path_or_id):
         with open(path_or_id) as open_file:
             for id_or_slug in open_file.readlines():
-                certify_org(id_or_slug.strip())
+                toggle(id_or_slug.strip(), badge_kind)
     else:
-        certify_org(path_or_id)
-
-
-@manager.command
-def uncertify(path_or_id):
-    '''Certify an organization as a public service'''
-    if exists(path_or_id):
-        with open(path_or_id) as open_file:
-            for id_or_slug in open_file.readlines():
-                uncertify_org(id_or_slug.strip())
-    else:
-        uncertify_org(path_or_id)
+        toggle(path_or_id, badge_kind)
