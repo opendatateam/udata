91d8654fec7edcb454283d3d9c777d09a1bd800d
Axel Haustant <noirbizarre@gmail.com>
2015-07-07 15:52:10 +0200

    Fix badges migration

--- migrations/2015-06-10-public-service-to-badges.js
+++ migrations/2015-06-10-public-service-to-badges.js
@@ -1,4 +1,4 @@
-var organizations = db.organization.find();
+var organizations = db.organization.find({public_service: {$exists: true}});
 var etalab = db.user.find({first_name: 'Etalab', last_name: 'Bot'})[0];
 var counter = 0;
 
@@ -17,9 +17,14 @@ organizations.map(function(organization) {
             created_by: etalab._id
         };
         var result = db.organization.update({_id: organization._id},
-                               {$set: {badges: [ps_badge, certified_badge]},
-                                $unset: {public_service: true}});
+                               {$set: {badges: [ps_badge, certified_badge]}});
         counter += result.nModified;
     }
 });
-print(counter, 'organizations modified.');
+print(counter, 'organizations badged.');
+
+
+var result = db.organization.update({public_service: {$exists: true}},
+                       {$unset: {public_service: true}},
+                       {multi: true});
+print(result.nModified, 'organizations cleaned (removed public_service attribute).');
