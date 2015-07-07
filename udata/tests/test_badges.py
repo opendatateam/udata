3cec1af99c0ab80398430c2dd8851376baa99676
Axel Haustant <noirbizarre@gmail.com>
2015-07-07 18:22:28 +0200

    Added missing badges command tests

---
+++ udata/tests/test_badges.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from tempfile import NamedTemporaryFile
+
+from udata.models import OrganizationBadge, CERTIFIED, PUBLIC_SERVICE
+from udata.tests import TestCase, DBTestMixin
+from udata.tests.factories import OrganizationFactory
+
+from udata.core.badges.commands import toggle
+
+
+class BadgeCommandTest(DBTestMixin, TestCase):
+
+    def test_toggle_badge_on(self):
+        org = OrganizationFactory()
+        toggle(str(org.id), PUBLIC_SERVICE)
+        org.reload()
+        self.assertEqual(org.badges[0].kind, PUBLIC_SERVICE)
+
+    def test_toggle_badge_off(self):
+        ps_badge = OrganizationBadge(kind=PUBLIC_SERVICE)
+        certified_badge = OrganizationBadge(kind=CERTIFIED)
+        org = OrganizationFactory(badges=[ps_badge, certified_badge])
+        toggle(str(org.id), PUBLIC_SERVICE)
+        org.reload()
+        self.assertEqual(org.badges[0].kind, CERTIFIED)
+
+    def test_toggle_badge_on_from_file(self):
+        orgs = [OrganizationFactory() for _ in range(2)]
+
+        with NamedTemporaryFile() as temp:
+            temp.write('\n'.join((str(org.id) for org in orgs)))
+            temp.flush()
+
+            toggle(temp.name, PUBLIC_SERVICE)
+
+        for org in orgs:
+            org.reload()
+            self.assertEqual(org.badges[0].kind, PUBLIC_SERVICE)
