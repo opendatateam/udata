75f88790201f1bfac9b99b5cfedc5bcfa493432e
Axel Haustant <noirbizarre@gmail.com>
2015-07-10 16:45:11 +0200

    Likify emails too

--- udata/frontend/markdown.py
+++ udata/frontend/markdown.py
@@ -42,7 +42,8 @@ class UDataMarkdown(object):
         ast = self.parser.parse(stream)
         html = self.renderer.render(ast)
         # Turn string links into HTML ones *after* markdown transformation.
-        html = bleach.linkify(html, tokenizer=KeepTokenSanitizer, skip_pre=True)
+        html = bleach.linkify(html, tokenizer=KeepTokenSanitizer,
+                              skip_pre=True, parse_email=True)
         # Return a `Markup` element considered as safe by Jinja.
         return Markup(html)
 
