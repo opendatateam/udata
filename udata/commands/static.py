1dbf5c738ee2c25e816fbbd3df0fee48b29a1d87
Axel Haustant <noirbizarre@gmail.com>
2015-07-29 19:33:38 +0200

    Improve the static collect command

--- udata/commands/static.py
+++ udata/commands/static.py
@@ -24,29 +24,45 @@ log = logging.getLogger(__name__)
 def collect(path, input):
     '''Collect static files'''
     if exists(path):
-        print('"{0}" directory already exists and will be erased'.format(path))
+        msg = '"%s" directory already exists and will be erased'
+        log.warning(msg, path)
         if input and not prompt_bool('Are you sure'):
             exit(-1)
 
-        print('Deleting static directory {0}'.format(path))
+        log.info('Deleting static directory "%s"', path)
         shutil.rmtree(path)
 
-    print('Copying assets into "{0}"'.format(path))
-    shutil.copytree(current_app.static_folder, path)
+    prefix = current_app.static_url_path or current_app.static_folder
+    if prefix.startswith('/'):
+        prefix = prefix[1:]
+    destination = join(path, prefix)
+    log.info('Copying application assets into "%s"', destination)
+    shutil.copytree(current_app.static_folder, destination)
+
+    for blueprint in current_app.blueprints.values():
+        if blueprint.has_static_folder:
+            prefix = current_app.static_prefixes.get(blueprint.name)
+            prefix = prefix or blueprint.url_prefix or ''
+            prefix += blueprint.static_url_path or ''
+            if prefix.startswith('/'):
+                prefix = prefix[1:]
+
+            log.info('Copying %s assets to %s', blueprint.name, prefix)
+            destination = join(path, prefix)
+            copy_recursive(blueprint.static_folder, destination)
 
     for prefix, source in manager.app.config['STATIC_DIRS']:
-        print('Copying %s to %s' % (source, prefix))
+        log.info('Copying %s to %s', source, prefix)
         destination = join(path, prefix)
         copy_recursive(source, destination)
 
-    print('Done')
+    log.info('Done')
 
 
 def copy_recursive(source, destination):
     if not exists(destination):
         makedirs(destination)
     for filename in iglob(join(source, '*')):
-        print(filename)
         if isdir(filename):
             suffix = filename.replace(source, '')
             if suffix.startswith(os.sep):
