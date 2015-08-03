2ff76f26bbc50d64d32c4a1b763ac0e2a01d4fa0
Axel Haustant <noirbizarre@gmail.com>
2015-08-03 10:55:59 +0200

    Move ODS Harvester into the backends package

--- udata_harvest/backends/__init__.py
+++ udata_harvest/backends/__init__.py
@@ -22,8 +22,5 @@ def get_all():
 
 from .base import BaseBackend
 from .dummy import DummyBackend
-<<<<<<< 2826324578d618b6ae757c97b2f7d1994d97a1e4
-from ..harvesters import OdsHarvester
-=======
+from .ods import OdsHarvester
 from .ckan import CkanBackend
->>>>>>> Lots of cleanup
