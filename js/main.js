334178893fe2b5f31881e98bc75a12fad29a489c
Axel Haustant <noirbizarre@gmail.com>
2015-08-20 11:21:56 +0200

    Disable Vue.cong.strict until all views are fixed for it

--- js/main.js
+++ js/main.js
@@ -20,7 +20,6 @@ var Vue = require('vue'),
 
 // Ensure retrocompatibily for 0.12.2 replace behavior
 Vue.options.replace = false;
-Vue.config.strict = true;
 Vue.config.debug = config.debug;
 
 Vue.use(require('plugins/util'));
