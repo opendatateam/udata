d94d2ce9cb36110d888f9830633900db65f9a629
Axel Haustant <noirbizarre@gmail.com>
2015-08-21 12:28:00 +0200

    Merged logger

--- js/logger.js
+++ js/logger.js
@@ -1,20 +1,14 @@
-define(['vendor/normalizeconsole'], function() {
-    var DEBUG = false,
-        Logger = {};
-    //>>excludeStart('production', pragmas.production);
-    DEBUG = true;
-    //>>excludeEnd('production');
+const Logger = {};
 
-    Logger.info = Function.prototype.bind.call(console.info, console);
-    Logger.warn = Function.prototype.bind.call(console.warn, console);
-    Logger.error = Function.prototype.bind.call(console.error, console);
+Logger.info = Function.prototype.bind.call(console.info, console);
+Logger.warn = Function.prototype.bind.call(console.warn, console);
+Logger.error = Function.prototype.bind.call(console.error, console);
 
-    if (DEBUG) {
-        Logger.log = Function.prototype.bind.call(console.log, console);
-        Logger.debug = Function.prototype.bind.call(console.debug, console);
-    } else {
-        Logger.log = Logger.debug = function() {};
-    }
+if (DEBUG) {
+    Logger.log = Function.prototype.bind.call(console.log, console);
+    Logger.debug = Function.prototype.bind.call(console.debug, console);
+} else {
+    Logger.log = Logger.debug = function() {};
+}
 
-    return Logger;
-});
+export default Logger;
