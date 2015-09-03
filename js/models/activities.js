bc6f566b5bf00bdd3d74bb94c4baaf000a711f2c
Axel Haustant <noirbizarre@gmail.com>
2015-09-03 11:24:50 +0200

    Fix activity model

--- js/models/activities.js
+++ js/models/activities.js
@@ -1,12 +1,9 @@
 import {ModelPage} from 'models/base';
 
-export default class SiteActivityPage extends ModelPage {
+export default class ActivityPage extends ModelPage {
     constructor(options) {
         super(options);
         this.$options.ns = 'site';
         this.$options.fetch = 'site_activity';
     }
 };
-
-export var activities = new SiteActivityPage().fetch();
-export default activities;
