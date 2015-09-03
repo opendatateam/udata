5662252696e5485749806f445b67c48fd53cfadf
Axel Haustant <noirbizarre@gmail.com>
2015-09-03 10:53:33 +0200

    Added postman command

--- udata/api/commands.py
+++ udata/api/commands.py
@@ -8,7 +8,7 @@ from flask import json
 from udata.api import api
 from udata.commands import submanager
 
-from flask.ext.restplus.swagger import Swagger
+# from flask.ext.restplus.swagger import Swagger
 
 log = logging.getLogger(__name__)
 
@@ -21,9 +21,22 @@ m = submanager(
 
 
 @m.option('-p', '--pretty', action='store_true', default=False,
-          help='Pretty print (sort')
+          help='Pretty print')
 def swagger(pretty):
     '''Dump the swagger specifications'''
-    data = Swagger(api).as_dict()
+    # data = Swagger(api).as_dict()
     kwargs = dict(indent=4) if pretty else {}
+    print(json.dumps(api.__schema__, **kwargs))
+
+
+@m.option('-p', '--pretty', action='store_true', default=False,
+          help='Pretty print')
+@m.option('-u', '--urlvars', action='store_true', default=False,
+          help='Export query strings')
+@m.option('-s', '--swagger', action='store_true', default=False,
+          help='Export Swagger specifications')
+def postman(pretty, urlvars, swagger):
+    '''Dump the API as a Postman collection'''
+    kwargs = dict(indent=4) if pretty else {}
+    data = api.as_postman(urlvars=urlvars, swagger=swagger)
     print(json.dumps(data, **kwargs))
