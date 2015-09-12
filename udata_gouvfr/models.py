c4efcb4fbe7362f15af12f0bab7518acda7957cd
Axel Haustant <noirbizarre@gmail.com>
2015-09-12 13:14:12 +0200

    Added dataconexions 6 page and badges

--- udata_gouvfr/models.py
+++ udata_gouvfr/models.py
@@ -1,10 +1,11 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from udata.models import db, Dataset, User, Organization, Reuse
-
-# TODO: service_public flag as an extension
-
+from udata.i18n import lazy_gettext as _
+from udata.models import (
+    db, Dataset, User, Organization, Reuse,
+    REUSE_BADGE_KINDS, DATASET_BADGE_KINDS
+)
 
 # Dataset.extras.register('territorial_coverage', TerritorialCoverage)
 
@@ -12,3 +13,19 @@ Dataset.extras.register('datagouv_ckan_last_sync', db.DateTimeField)
 Organization.extras.register('datagouv_ckan_last_sync', db.DateTimeField)
 Reuse.extras.register('datagouv_ckan_last_sync', db.DateTimeField)
 User.extras.register('datagouv_ckan_last_sync', db.DateTimeField)
+
+# Reuses badges
+DATACONNEXIONS_5_CANDIDATE = 'dataconnexions-5-candidate'
+DATACONNEXIONS_5_LAUREATE = 'dataconnexions-5-laureate'
+DATACONNEXIONS_6_CANDIDATE = 'dataconnexions-6-candidate'
+DATACONNEXIONS_6_LAUREATE = 'dataconnexions-6-laureate'
+REUSE_BADGE_KINDS.update({
+    DATACONNEXIONS_5_CANDIDATE: _('Dataconnexions 5 candidate'),
+    DATACONNEXIONS_5_LAUREATE: _('Dataconnexions 5 laureate'),
+    DATACONNEXIONS_6_CANDIDATE: _('Dataconnexions 6 candidate'),
+    DATACONNEXIONS_6_LAUREATE: _('Dataconnexions 6 laureate'),
+})
+
+# Datasets
+C3 = 'c3'
+DATASET_BADGE_KINDS[C3] = _('C³')
--- udata_gouvfr/tests.py
+++ udata_gouvfr/tests.py
@@ -4,7 +4,7 @@ from __future__ import unicode_literals
 from flask import url_for
 
 from udata.models import (
-    OrganizationBadge, ReuseBadge, PUBLIC_SERVICE, DATACONNEXIONS_CANDIDATE
+    OrganizationBadge, ReuseBadge, PUBLIC_SERVICE
 )
 from udata.tests import TestCase, DBTestMixin
 from udata.tests.factories import (
@@ -14,8 +14,8 @@ from udata.tests.factories import VisibleReuseFactory
 from udata.tests.frontend import FrontTestCase
 from udata.settings import Testing
 
-from .views import DATACONNEXIONS_CATEGORIES
-
+from .models import DATACONNEXIONS_5_CANDIDATE, DATACONNEXIONS_6_CANDIDATE
+from .views import DATACONNEXIONS_5_CATEGORIES, DATACONNEXIONS_6_CATEGORIES
 from .metrics import PublicServicesMetric
 
 
@@ -198,15 +198,31 @@ class SpecificUrlsTest(FrontTestCase):
 class DataconnexionsTest(FrontTestCase):
     settings = GouvFrSettings
 
-    def test_render_dataconnexions_without_data(self):
+    def test_redirect_to_last_dataconnexions(self):
         response = self.client.get(url_for('gouvfr.dataconnexions'))
+        self.assertRedirects(response, url_for('gouvfr.dataconnexions6'))
+
+    def test_render_dataconnexions_5_without_data(self):
+        response = self.client.get(url_for('gouvfr.dataconnexions5'))
         self.assert200(response)
 
-    def test_render_dataconnexions_with_data(self):
-        for tag, label, description in DATACONNEXIONS_CATEGORIES:
-            badge = ReuseBadge(kind=DATACONNEXIONS_CANDIDATE)
+    def test_render_dataconnexions_5_with_data(self):
+        for tag, label, description in DATACONNEXIONS_5_CATEGORIES:
+            badge = ReuseBadge(kind=DATACONNEXIONS_5_CANDIDATE)
             VisibleReuseFactory(tags=[tag], badges=[badge])
-        response = self.client.get(url_for('gouvfr.dataconnexions'))
+        response = self.client.get(url_for('gouvfr.dataconnexions5'))
+        self.assert200(response)
+
+    def test_render_dataconnexions_6_without_data(self):
+        response = self.client.get(url_for('gouvfr.dataconnexions6'))
+        self.assert200(response)
+
+    def test_render_dataconnexions_6_with_data(self):
+        # Use tags until we are sure all reuse are correctly labeled
+        for tag, label, description in DATACONNEXIONS_6_CATEGORIES:
+            badge = ReuseBadge(kind=DATACONNEXIONS_6_CANDIDATE)
+            VisibleReuseFactory(tags=['dataconnexions-6', tag], badges=[badge])
+        response = self.client.get(url_for('gouvfr.dataconnexions6'))
         self.assert200(response)
 
 
