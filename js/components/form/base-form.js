e596e2850a89fdc03277b38438065020b18b50bd
David Larlet <david@larlet.fr>
2015-11-05 16:00:33 +0100

    Refactor base-form error message display, refs #240

--- js/components/form/base-form.js
+++ js/components/form/base-form.js
@@ -203,7 +203,7 @@ export default {
                 if ('errors' in data) {
                     this.fill_errors(data.errors);
                 } else {
-                    $(this.$form).append(`<div class="form-group has-error"><p class="help-block">${data.message}</p></div>`);
+                    $(this.$form).append(this.error_element('', data.message));
                 }
             }
         },
@@ -212,10 +212,16 @@ export default {
                 if (element.name in errors) {
                     let name = element.name;
                     let error = errors[name][0];
-                    let errorElement = `<p class="help-block" id="${name}-error">${error}</p>`;
-                    $(element).closest('.form-group,.field-wrapper').removeClass('has-success').addClass('has-error').append(errorElement);
+                    $(element).closest('.form-group,.field-wrapper')
+                              .removeClass('has-success')
+                              .addClass('has-error')
+                              .append(this.error_element(name, error));
                 }
             });
         },
+        error_element: function(id, message) {
+            $(`#form-${id}-error`).remove();
+            return `<p class="form-error" id="form-${id}-error">${message}</p>`;
+        }
     }
 };
