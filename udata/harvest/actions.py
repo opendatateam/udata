1ae364762210e781fe1f2a5a41451e1780b06dd3
Axel Haustant <noirbizarre@gmail.com>
2015-11-07 08:29:39 +0100

    Use unicodecsv to prevent encoding issues

--- udata/harvest/actions.py
+++ udata/harvest/actions.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-import csv
 import logging
+import unicodecsv as csv
 
 from collections import namedtuple
 from datetime import datetime
