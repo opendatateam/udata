014eb3725c3b456814356cdcd1db126e9bbd4ab3
Vincent Agnano <vincent.agnano@scopyleft.fr>
2015-11-17 11:58:37 +0100

    fixed minor indent in migration, migration count display and ignored*.rdb

--- udata/migrations/2015-11-16-remove-supplier.js
+++ udata/migrations/2015-11-16-remove-supplier.js
@@ -2,10 +2,9 @@
  * Remove supplier property from datasets
  */
 function process_collection(collection) {
-    collection.update({supplier: {$exists: true}},
-                                 {$unset: {'supplier': 1}}, false, true);
-    var count = db.runCommand({getLastError : 1}).n;
-    print('Fixed ' + count + ' document(s) on ' + collection.name);
+    var updated = collection.update({supplier: {$exists: true}},
+                                    {$unset: {'supplier': 1}}, false, true);
+    print('Fixed ' + updated.nModified + ' document(s) on ' + collection.name);
 }
 
 process_collection(db.dataset);
