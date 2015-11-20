9c2e85339c31132e124d61a6d2106ff4bf701f4d
Vincent Agnano <vincent.agnano@scopyleft.fr>
2015-11-20 16:29:13 +0100

    fixed closed parameter on discussion API. fix #262

--- udata/tests/test_discussions.py
+++ udata/tests/test_discussions.py
@@ -312,6 +312,35 @@ class DiscussionsTest(APITestCase):
         # Metrics unchanged after attempt to delete the discussion.
         self.assertEqual(dataset.metrics['discussions'], 1)
 
+    def test_list_discussions_filter_closed(self):
+        '''Should consider the closed filtering on discussions'''
+        user = UserFactory()
+        dataset = DatasetFactory()
+        (discussion,) = DatasetDiscussion.objects.create(subject=dataset,
+                                                         title='', user=user),
+        (discussion_closed,) = DatasetDiscussion.objects.create(subject=dataset,
+            title='', closed=datetime.now(), user=user),
+
+        response_all = self.get(url_for('api.discussions'))
+        self.assert200(response_all)
+        data_all = response_all.json['data']
+        self.assertEqual(len(data_all), 2)
+        self.assertItemsEqual([str(discussion.id), str(discussion_closed.id)],
+                              [str(d['id']) for d in data_all])
+
+        response_closed = self.get(url_for('api.discussions', closed=True))
+        self.assert200(response_closed)
+        data_closed = response_closed.json['data']
+        self.assertEqual(len(data_closed), 1)
+        self.assertEqual(str(discussion_closed.id), data_closed[0]['id'])
+
+        response_open = self.get(url_for('api.discussions', closed=False))
+        self.assert200(response_open)
+        data_open = response_open.json['data']
+        self.assertEqual(len(data_open), 1)
+
+        self.assertEqual(str(discussion.id), data_open[0]['id'])
+
 
 class DiscussionCsvTest(FrontTestCase):
 
