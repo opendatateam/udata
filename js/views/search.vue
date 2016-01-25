d5dd9795f55ff297bbc1bcd01271dbdc86f61633
Axel Haustant <axel.haustant@data.gouv.fr>
2016-01-25 15:49:56 +0100

    Factorize "related to ..." subtitle in admin search

--- js/views/search.vue
+++ js/views/search.vue
@@ -1,24 +1,19 @@
 <template>
-<layout :title="_('Search')">
+<layout :title="title">
     <div class="row">
-        <datasets class="col-xs-12" :datasets="datasets" :title="datasetsTitle">
-        </datasets>
+        <datasets class="col-xs-12" :datasets="datasets"></datasets>
     </div>
     <div class="row">
-        <communities class="col-xs-12" :communities="communities" :title="communitiesTitle">
-        </communities>
+        <communities class="col-xs-12" :communities="communities"></communities>
     </div>
     <div class="row">
-        <reuses class="col-xs-12" :reuses="reuses" :title="reusesTitle">
-        </reuses>
+        <reuses class="col-xs-12" :reuses="reuses"></reuses>
     </div>
     <div class="row">
-        <issues class="col-xs-12" :issues="issues" :title="issuesTitle">
-        </issues>
+        <issues class="col-xs-12" :issues="issues"></issues>
     </div>
     <div class="row">
-        <discussions class="col-xs-12" :discussions="discussions" :title="discussionsTitle">
-        </discussions>
+        <discussions class="col-xs-12" :discussions="discussions"></discussions>
     </div>
 </layout>
 </template>
@@ -37,29 +32,34 @@ export default {
         discussions: require('components/discussions/list.vue'),
         Layout
     },
+    computed: {
+        title() {
+            let title = this._('Search');
+            if (this.subtitle) {
+                title = `${title} (${this.subtitle})`;
+            }
+            return title;
+        }
+    },
     data() {
         return {
-            datasetsTitle: '',
+            subtitle: undefined,
             datasets: new PageList({
                 ns: 'me',
                 fetch: 'my_org_datasets'
             }),
-            communitiesTitle: '',
             communities: new PageList({
                 ns: 'me',
                 fetch: 'my_org_community_resources'
             }),
-            reusesTitle: '',
             reuses: new PageList({
                 ns: 'me',
                 fetch: 'my_org_reuses'
             }),
-            issuesTitle: '',
             issues: new PageList({
                 ns: 'me',
                 fetch: 'my_org_issues',
             }),
-            discussionsTitle: '',
             discussions: new PageList({
                 ns: 'me',
                 fetch: 'my_org_discussions'
@@ -69,15 +69,11 @@ export default {
     route: {
         data() {
             const terms = this.$route.query.terms;
-            this.datasetsTitle = this._(`Datasets related to your data about "${terms}"`);
+            this.subtitle = this._('related to your data about "{terms}"', {terms: terms});
             this.datasets.fetch({'q': terms});
-            this.communitiesTitle = this._(`Community resources related to your data about "${terms}"`);
             this.communities.fetch({'q': terms});
-            this.reusesTitle = this._(`Reuses related to your data about "${terms}"`);
             this.reuses.fetch({'q': terms});
-            this.issuesTitle = this._(`Issues related to your data about "${terms}"`);
             this.issues.fetch({'q': terms});
-            this.discussionsTitle = this._(`Discussions related to your data about "${terms}"`);
             this.discussions.fetch({'q': terms});
         }
     },
