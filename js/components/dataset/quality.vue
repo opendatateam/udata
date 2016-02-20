4481575e07496542cb070aec4e08efa03ba055da
Axel Haustant <axel.haustant@data.gouv.fr>
2016-02-20 13:00:54 +0100

    Make the quality indicators collapsible

--- js/components/dataset/quality.vue
+++ js/components/dataset/quality.vue
@@ -1,92 +1,63 @@
 <template>
 <box :title="title" icon="thumbs-up" boxclass="box-solid" v-if="quality">
-    <doughnut :score="quality.score + 1"></doughnut>
+    <doughnut :score="quality.score + 1" width="200px" height="200px"></doughnut>
     <h4>{{ _('The aim of that box is to help you improve the quality of the (meta)data associated to your dataset.') }}</h4>
     <p>{{ _('It gives you an overview of what will be useful for contributors to find and reuse your data.') }}</p>
 
-    <h3>{{ _('Description') }}</h3>
-    <p>{{ _('Try to be as descriptive as you can for your resources (how you use it yourself, which edge cases you solved, what data is missing and so on).') }}</p>
-    <p>{{ _('Your description has currently {description_length} caracters.', {description_length: quality.description_length}) }}</p>
-    <p v-if="quality.description_length > 300" class="text-success">
-        <span class="fa fa-check-circle"></span>
-        <strong>{{ _('That is great!') }}</strong>
-    </p>
-    <p v-if="quality.description_length <= 300" class="text-warning">
-        <span class="fa fa-exclamation-circle"></span>
-        <strong>{{ _('Why not improve it?') }}</strong>
-    </p>
+    <qa-section :title="_('Description')"
+        :condition="quality.description_length > 300"
+        :ok="_('That is great!')"
+        :ko="_('Why not improve it?')">
+        <p>{{ _('Try to be as descriptive as you can for your resources (how you use it yourself, which edge cases you solved, what data is missing and so on).') }}</p>
+        <p>{{ _('Your description has currently {description_length} caracters.', {description_length: quality.description_length}) }}</p>
+    </qa-section>
 
-    <h3>{{ _('Tags') }}</h3>
-    <p>{{ _('Tags helps your reusers to find your resources, that is very important to make your dataset popular.') }}</p>
-    <p v-if="quality.tags_count">{{ _('You currently set {tags_count} tags for that dataset.', {tags_count: quality.tags_count}) }}</p>
-    <p v-if="!quality.tags_count" class="text-warning">
-        <span class="fa fa-exclamation-circle"></span>
-        <strong>{{ _('You currently set no tags for that dataset! Why not add some more?') }}</strong>
-    </p>
-    <p v-if="quality.tags_count > 3" class="text-success">
-        <span class="fa fa-check-circle"></span>
-        <strong>{{ _('That is great!') }}</strong>
-    </p>
-    <p v-if="quality.tags_count <= 3" class="text-warning">
-        <span class="fa fa-exclamation-circle"></span>
-        <strong>{{ _('Why not add some more?') }}</strong>
-    </p>
+    <qa-section :title="_('Tags')"
+        :condition="quality.tags_count > 3"
+        :ok="_('That is great!')"
+        :ko="_('Why not add some more?')">
+        <p>{{ _('Tags helps your reusers to find your resources, that is very important to make your dataset popular.') }} </p>
+    </qa-section>
 
-    <h3>{{ _('Open formats') }}</h3>
-    <p>{{ _('The open data community enjoy using files in open formats that can be manipulated easily through open software and tools. Make sure you publish your data at least in formats different from XLS, DOC and PDF.') }}</p>
-    <p v-if="!quality.has_only_closed_formats" class="text-success">
-        <span class="fa fa-check-circle"></span>
-        <strong>{{ _('You currently have other formats!') }}</strong>
-    </p>
-    <p v-if="quality.has_only_closed_formats" class="text-warning">
-        <span class="fa fa-exclamation-circle"></span>
-        <strong>{{ _('You only have closed formats. Cannot you export some in open ones?') }}</strong>
-        </p>
+    <qa-section :title="_('Open formats')"
+        :condition="!quality.has_only_closed_formats"
+        :ok="_('You currently have other formats!')"
+        :ko="_('You only have closed formats. Cannot you export some in open ones?')">
+        <p>{{ _('The open data community enjoy using files in open formats that can be manipulated easily through open software and tools. Make sure you publish your data at least in formats different from XLS, DOC and PDF.') }}</p>
+    </qa-section>
 
-    <h3>{{ _('Discussions') }}</h3>
-    <p>{{ _('Discussing with the community about your datasets is key to involve developers and hackers in your open process. The feedback from reusers of your data is unvaluable and should be answered rather quickly.') }}</p>
-    <p v-if="quality.discussions && !quality.has_untreated_discussions" class="text-success">
-        <span class="fa fa-check-circle"></span>
-        <strong>{{ _('You are currently involved in all discussions!') }}</strong>
-    </p>
-    <p v-if="quality.discussions && quality.has_untreated_discussions" class="text-warning">
-        <span class="fa fa-exclamation-circle"></span>
-        <strong>{{ _('You have some untreated discussion threads. Why do not you give it a look below?') }}</strong>
-    </p>
-    <p v-if="!quality.discussions" class="text-info">
-        <span class="fa fa-question-circle"></span>
-        <strong>{{ _('No discussion yet? Try to involve users via social networks and/or by email.') }}</strong>
-    </p>
+    <qa-section :title="_('Discussions')"
+        :condition="quality.discussions && !quality.has_untreated_discussions"
+        :ok="_('You are currently involved in all discussions!')"
+        :ko="_('You have some untreated discussion threads. Why do not you give it a look below?')"
+        :info="_('No discussion yet? Try to involve users via social networks and/or by email.')"
+        :show-info="!quality.discussions">
+        <p>{{ _('Discussing with the community about your datasets is key to involve developers and hackers in your open process. The feedback from reusers of your data is unvaluable and should be answered rather quickly.') }}</p>
+    </qa-section>
 
-    <h3>{{ _('Up-to-date') }}</h3>
-    <p>{{ _('Proposing up-to-date and incremental data makes it possible for reusers to establish datavisualisations on the long term.') }}</p>
-    <p v-if="quality.frequency">{{ _('You currently set your frequency to {frequency}.', {frequency: quality.frequency}) }}</p>
-    <p v-if="!quality.frequency">{{ _('You currently have no frequency set for that dataset, is that pertinent?') }}</p>
-    <p v-if="quality.update_in <= 0" class="text-success">
-        <span class="fa fa-check-circle"></span>
-        <strong>{{ _('That is great!') }}</strong>
-    </p>
-    <p v-if="quality.update_in > 0" class="text-warning">
-        <span class="fa fa-exclamation-circle"></span>
-        <strong> {{ _('Need an update since {days} days.', {days:quality.update_in}) }}</strong>
-    </p>
+    <qa-section :title="_('Up-to-date')"
+        :condition="quality.update_in <= 0"
+        :ok="_('That is great!')"
+        :ko="_('Need an update since {days} days.', {days:quality.update_in})">
+        <p>{{ _('Proposing up-to-date and incremental data makes it possible for reusers to establish datavisualisations on the long term.') }}</p>
+        <p v-if="quality.frequency">{{ _('You currently set your frequency to {frequency}.', {frequency: quality.frequency}) }}</p>
+        <p v-if="!quality.frequency">{{ _('You currently have no frequency set for that dataset, is that pertinent?') }}</p>
+    </qa-section>
 
-    <div v-if="quality.has_resources">
-        <h3>{{ _('Availability') }}</h3>
+    <qa-section :title="_('Availability')" v-if="quality.has_resources"
+        :condition="!quality.has_unavailable_resources"
+        :ok="_('All your resources looks to be directly available. That is great!')"
+        :ko="_('Some of your resources may have broken links or temporary unavailability. Try to fix it as fast as you can (see the list below).')">
         <p>{{ _('The availability of your distant resources (if any) is crucial for reusers. They trust you for the reliability of these data both in terms of reachability and ease of access.') }}</p>
-        <p v-if="!quality.has_unavailable_resources" class="text-success">
-            <span class="fa fa-check-circle"></span>
-            <strong>{{ _('All your resources looks to be directly available. That is great!') }}</strong>
-        </p>
-        <p v-if="quality.has_unavailable_resources" class="text-warning">
-            <span class="fa fa-exclamation-circle"></span>
-            <strong>{{ _('Some of your resources may have broken links or temporary unavailability. Try to fix it as fast as you can (see the list below).') }}</strong>
-        </p>
-    </div>
+    </qa-section>
 </box>
 </template>
 
 <script>
+import Box from 'components/containers/box.vue';
+import Doughnut from 'components/charts/doughnut.vue';
+import QaSection from './quality-section.vue';
+
 export default {
     name: 'dataset-quality',
     props: ['quality'],
@@ -95,9 +66,6 @@ export default {
             title: this._('Quality')
         };
     },
-    components: {
-        box: require('components/containers/box.vue'),
-        doughnut: require('components/charts/doughnut.vue')
-    }
+    components: {Box, Doughnut, QaSection},
 };
 </script>
