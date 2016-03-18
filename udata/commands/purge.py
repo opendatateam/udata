c6d89c8cb579741f0f14fd53b10f92871a65ca4f
Axel Haustant <axel.haustant@data.gouv.fr>
2016-03-18 10:38:59 +0100

    Added purge command

---
+++ udata/commands/purge.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+import logging
+
+# from flask.ext.script import Command, Option
+
+from udata.commands import manager
+
+from udata.core.dataset.tasks import purge_datasets
+from udata.core.organization.tasks import purge_organizations
+from udata.core.reuse.tasks import purge_reuses
+
+log = logging.getLogger(__name__)
+
+
+@manager.command
+def purge():
+    '''permanently remove data flagged as deleted'''
+    log.info('Purging datasets')
+    purge_datasets()
+
+    log.info('Purging reuses')
+    purge_reuses()
+
+    log.info('Purging organizations')
+    purge_organizations()
+
+    log.info('Done')
