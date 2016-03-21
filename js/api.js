a219d2e5e098ee07b7a7932dba8217a2552778b7
Axel Haustant <axel.haustant@data.gouv.fr>
2016-03-21 13:03:30 +0100

    Ensure Admin perform localized API calls

--- js/api.js
+++ js/api.js
@@ -3,6 +3,30 @@ import SwaggerClient from 'swagger-client';
 import config from 'config';
 import log from 'logger';
 
+
+function updateQueryParam(uri, key, value) {
+    const re = new RegExp(`([?&])${key}=.*?(&|$)`, 'i');
+    const separator = uri.indexOf('?') !== -1 ? '&' : '?';
+    if (uri.match(re)) {
+        return uri.replace(re, `$1${key}=${value}$2`);
+    } else {
+        return `${uri}${separator}${key}=${value}`;
+    }
+}
+
+/**
+ * This object inject the lang query parameter in every API request
+ */
+const LangParameter = function(name) {
+  this.name = name;
+};
+
+LangParameter.prototype.apply = function(obj) {
+    obj.url = updateQueryParam(obj.url, 'lang', config.lang);
+    return true;
+};
+
+
 const API = new SwaggerClient({
     debug: config.debug,
     url: config.api_specs,
@@ -12,6 +36,9 @@ const API = new SwaggerClient({
     },
     progress: function(msg) {
         log.debug(msg);
+    },
+    authorizations: {
+        lang: new LangParameter()
     }
 });
 
