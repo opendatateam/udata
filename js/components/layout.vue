315aa720972fce34d09a6e2276d33127666eb3a4
Axel Haustant <axel.haustant@data.gouv.fr>
2016-03-24 15:08:37 +0100

    New page header layout proposal

--- js/components/layout.vue
+++ js/components/layout.vue
@@ -1,22 +1,10 @@
 <style lang="less">
 .content-header {
+    h1 a {
+        color: black;
 
-    > .btn-actions {
-        background: transparent;
-        margin-top: 0px;
-        margin-bottom: 0;
-        position: absolute;
-        top: 15px;
-        right: 10px;
-
-        .btn-link {
-            color: lighten(black, 45%);
-            font-size: 1.2em;
-            padding: 5px;
-
-            &:hover {
-                color: lighten(black, 20%);
-            }
+        .fa {
+            font-size: 0.4em;
         }
     }
 }
@@ -31,49 +19,47 @@
     <router-view></router-view>
     <!-- Content Header (Page header) -->
     <section class="content-header">
+        <div v-if="main_action" class="btn-group btn-group-sm btn-actions pull-right clearfix">
+            <div v-if="menu_actions" class="btn-group btn-group-sm" role="group">
+                <button type="button" class="btn btn-info" @click="main_action.method">
+                    <span v-if="main_action.icon" class="fa fa-fw fa-{{main_action.icon}}"></span>
+                    {{main_action.label}}
+                </button>
+                <button type="button" class="btn btn-info dropdown-toggle" data-toggle="dropdown">
+                    <span class="caret"></span>
+                    <span class="sr-only">Toggle Dropdown</span>
+                </button>
+                <ul class="dropdown-menu dropdown-menu-right" role="menu">
+                    <li v-for="action in menu_actions"
+                         :role="action.divider ? 'separator' : false"
+                         :class="{ 'divider': action.divider }">
+                        <a class="pointer"
+                            v-if="!action.divider"
+                            @click="action.method" >
+                            <span v-if="action.icon" class="fa fa-fw fa-{{action.icon}}"></span>
+                            {{action.label}}
+                        </a>
+                    </li>
+                </ul>
+            </div>
+            <button v-if="!menu_actions" type="button" class="btn btn-info btn-sm">
+                <span v-if="action.icon" class="fa fa-fw fa-{{action.icon}}"></span>
+                {{main_action.label}}
+            </button>
+        </div>
         <h1>
-            {{title}}
+            <a v-if="page" :href="page" :title="_('See on the site')">
+                {{ title }}
+                <span class="fa fa-external-link"></span>
+            </a>
+            <span v-if="!page">{{title}}</span>
+
             <small v-if="subtitle">{{subtitle}}</small>
             <small v-if="badges">
                 <span v-for="badge in badges"
                     class="label label-{{badge.class}}">{{badge.label}}</span>
             </small>
         </h1>
-        <div class="btn-toolbar btn-actions pull-right clearfix">
-            <div v-if="main_action" class="btn-group btn-group-sm">
-                <div v-if="menu_actions" class="btn-group btn-group-sm" role="group">
-                    <button type="button" class="btn btn-info" @click="main_action.method">
-                        <span v-if="main_action.icon" class="fa fa-fw fa-{{main_action.icon}}"></span>
-                        {{main_action.label}}
-                    </button>
-                    <button type="button" class="btn btn-info dropdown-toggle" data-toggle="dropdown">
-                        <span class="caret"></span>
-                        <span class="sr-only">Toggle Dropdown</span>
-                    </button>
-                    <ul class="dropdown-menu" role="menu">
-                        <li v-for="action in menu_actions"
-                             :role="action.divider ? 'separator' : false"
-                             :class="{ 'divider': action.divider }">
-                            <a class="pointer"
-                                v-if="!action.divider"
-                                @click="action.method" >
-                                <span v-if="action.icon" class="fa fa-fw fa-{{action.icon}}"></span>
-                                {{action.label}}
-                            </a>
-                        </li>
-                    </ul>
-                </div>
-                <button v-if="!menu_actions" type="button" class="btn btn-info btn-sm">
-                    <span v-if="action.icon" class="fa fa-fw fa-{{action.icon}}"></span>
-                    {{main_action.label}}
-                </button>
-            </div>
-            <div class="btn-group btn-group-sm" v-if="page">
-                <a class="btn btn-link" :href="page" :title="_('See it as viewed by visitors')">
-                    {{ _('See on the site') }} →
-                </a>
-            </div>
-        </div>
     </section>
     <!-- Notifications -->
     <notification-zone></notification-zone>
