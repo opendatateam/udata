9d0201e90b212f4cd05b3dc11539ecb2c5925fc6
David Larlet <david@larlet.fr>
2016-04-11 17:45:07 +0200

    Add license and sources for embeds, fix #439

--- udata/frontend/markdown.py
+++ udata/frontend/markdown.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
+from urlparse import urlparse
+
 import bleach
 import CommonMark
 from html5lib.sanitizer import HTMLSanitizer
@@ -8,9 +10,48 @@ from flask import current_app, Markup
 from werkzeug.local import LocalProxy
 from jinja2.filters import do_truncate, do_striptags
 
+from udata.i18n import _
+
 md = LocalProxy(lambda: current_app.extensions['markdown'])
 
 EXCERPT_TOKEN = '<!--- --- -->'
+
+
+def avoid_mailto_callback(attrs, new=False):
+    """Remove completely the link containing a `mailto`."""
+    if attrs['href'].startswith('mailto:'):
+        return None
+    return attrs
+
+
+def source_tooltip_callback(attrs, new=False):
+    """
+    Add a `data-tooltip` attribute with `Source` content for embeds.
+    """
+    attrs['data-tooltip'] = _('Source')
+    return attrs
+
+
+def nofollow_callback(attrs, new=False):
+    """
+    Turn relative links into external ones and avoid `nofollow` for us,
+
+    otherwise add `nofollow`.
+    That callback is not splitted in order to parse the URL only once.
+    """
+    parsed_url = urlparse(attrs['href'])
+    if parsed_url.netloc in ('', current_app.config['SERVER_NAME']):
+        attrs['href'] = '{scheme}://{netloc}{path}'.format(
+            scheme=current_app.config['USE_SSL'] and 'https' or 'http',
+            netloc=current_app.config['SERVER_NAME'],
+            path=parsed_url.path)
+        return attrs
+    else:
+        rel = [x for x in attrs.get('rel', '').split(' ') if x]
+        if 'nofollow' not in [x.lower() for x in rel]:
+            rel.append('nofollow')
+        attrs['rel'] = ' '.join(rel)
+        return attrs
 
 
 class KeepTokenSanitizer(HTMLSanitizer):
@@ -28,7 +69,7 @@ class UDataMarkdown(object):
         self.renderer = renderer
         app.jinja_env.filters.setdefault('markdown', self.__call__)
 
-    def __call__(self, stream):
+    def __call__(self, stream, source_tooltip=False):
         if not stream:
             return ''
         # Sanitize malicious attempts but keep the `EXCERPT_TOKEN`.
@@ -42,9 +83,15 @@ class UDataMarkdown(object):
         # Turn markdown to HTML.
         ast = self.parser().parse(stream)
         html = self.renderer.render(ast)
+        # Deal with callbacks
+        callbacks = [avoid_mailto_callback, nofollow_callback]
+        if source_tooltip:
+            callbacks.append(source_tooltip_callback)
+
         # Turn string links into HTML ones *after* markdown transformation.
-        html = bleach.linkify(html, tokenizer=KeepTokenSanitizer,
-                              skip_pre=True, parse_email=True)
+        html = bleach.linkify(
+            html, tokenizer=KeepTokenSanitizer,
+            skip_pre=True, parse_email=True, callbacks=callbacks)
         # Return a `Markup` element considered as safe by Jinja.
         return Markup(html)
 
