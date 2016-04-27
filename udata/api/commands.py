1c8bf9ecb3aa2029549638f288654b9852181ba2
Axel Haustant <axel.haustant@data.gouv.fr>
2016-04-27 16:16:03 +0200

    Make the filename a positionnal parameter

--- udata/api/commands.py
+++ udata/api/commands.py
@@ -2,6 +2,7 @@
 from __future__ import unicode_literals
 
 import logging
+import os
 
 from flask import json
 
@@ -18,22 +19,33 @@ m = submanager(
 )
 
 
+def json_to_file(data, filename, pretty=False):
+    '''Dump JSON data to a file'''
+    kwargs = dict(indent=4) if pretty else {}
+    dirname = os.path.dirname(filename)
+    if not os.path.exists(dirname):
+        os.makedirs(dirname)
+    dump = json.dumps(api.__schema__, **kwargs)
+    with open(filename, 'wb') as f:
+        f.write(dump.encode('utf-8'))
+
+
+@m.option('filename', help='The output filename')
 @m.option('-p', '--pretty', action='store_true', default=False,
           help='Pretty print')
-def swagger(pretty):
+def swagger(filename, pretty):
     '''Dump the swagger specifications'''
-    kwargs = dict(indent=4) if pretty else {}
-    print(json.dumps(api.__schema__, **kwargs))
+    json_to_file(api.__schema__, filename, pretty)
 
 
+@m.option('filename', help='The output filename')
 @m.option('-p', '--pretty', action='store_true', default=False,
           help='Pretty print')
 @m.option('-u', '--urlvars', action='store_true', default=False,
           help='Export query strings')
 @m.option('-s', '--swagger', action='store_true', default=False,
           help='Export Swagger specifications')
-def postman(pretty, urlvars, swagger):
+def postman(filename, pretty, urlvars, swagger):
     '''Dump the API as a Postman collection'''
-    kwargs = dict(indent=4) if pretty else {}
     data = api.as_postman(urlvars=urlvars, swagger=swagger)
-    print(json.dumps(data, **kwargs))
+    json_to_file(data, filename, pretty)
