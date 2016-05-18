14c083a1e315c2f37ca353e514897ac5aa8d5bc0
David Larlet <david@larlet.fr>
2016-05-18 18:22:43 +0200

    Handle search results + suggest, refs #466

--- udata/features/territories/api.py
+++ udata/features/territories/api.py
@@ -1,9 +1,8 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from flask import url_for
 from udata.api import api, API
-from udata.features.territories import check_for_territories
+from udata.features.territories import check_for_counties, check_for_towns
 
 suggest_parser = api.parser()
 suggest_parser.add_argument(
@@ -15,18 +14,20 @@ suggest_parser.add_argument(
 
 
 @api.route('/territory/suggest/', endpoint='suggest_territory')
-class SuggestTerritoriesAPI(API):
-    @api.doc(id='suggest_territory', parser=suggest_parser)
+class SuggestTownsTerritoriesAPI(API):
+    @api.doc(id='suggest_territory_towns', parser=suggest_parser)
     def get(self):
         args = suggest_parser.parse_args()
-        territories = check_for_territories(args['q'].decode('utf-8'))
+        towns = check_for_towns(args['q'].decode('utf-8'))
+        counties = check_for_counties(args['q'].decode('utf-8'))
+        territories = list(set(counties) | set(towns))
         if args['size']:
             territories = territories[:args['size']]
         return [{
             'id': territory.id,
             'title': territory.name,
-            'county': territory.county.name,
+            'county': territory.county and territory.county.name or None,
+            'region': territory.region and territory.region.name or None,
             'image_url': territory.logo_url(external=True),
-            'page': url_for('territories.town',
-                            territory=territory, _external=True)
+            'page': territory.external_url
         } for territory in territories]
