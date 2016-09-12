3d7d14a191e7cd19f94fcbeebad49db4cbe0065e
Axel Haustant <axel.haustant@data.gouv.fr>
2016-09-12 09:54:25 +0200

    Fix js/models/geogranularities double export default

--- js/models/geogranularities.js
+++ js/models/geogranularities.js
@@ -1,14 +1,13 @@
 import {List} from 'models/base';
-import log from 'logger';
 
 
-export default class GeoGranularity extends List {
+export class GeoGranularity extends List {
     constructor(options) {
         super(options);
         this.$options.ns = 'spatial';
         this.$options.fetch = 'spatial_granularities';
     }
-};
+}
 
-export var granularities = new GeoGranularity().fetch();
+export const granularities = new GeoGranularity().fetch();
 export default granularities;
