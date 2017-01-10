46eef01dabe6c07c25480e2c44911363d569a0ca
Axel Haustant <axel.haustant@data.gouv.fr>
2017-01-10 12:19:56 +0100

    Update to version 0.9.1.dev for next development cycle

--- docs/changelog.md
+++ docs/changelog.md
@@ -1,5 +1,9 @@
 # Changelog
 
+## Current (in progress)
+
+- Nothing yet
+
 ## 0.9.0 (2017-01-10)
 
 - First published version
--- udata/__init__.py
+++ udata/__init__.py
@@ -5,5 +5,5 @@ uData
 '''
 from __future__ import unicode_literals
 
-__version__ = '0.9.0'
+__version__ = '0.9.1.dev'
 __description__ = 'Open data portal'
