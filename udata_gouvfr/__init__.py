6fd4b387c961973bb7581969747e761467d7ce0b
Axel Haustant <noirbizarre@gmail.com>
2017-01-10 13:44:05 +0100

    Update to version 0.9.1.dev for next development cycle

--- udata_gouvfr/__init__.py
+++ udata_gouvfr/__init__.py
@@ -4,7 +4,7 @@ uData customizations for Data.gouv.fr
 '''
 from __future__ import unicode_literals
 
-__version__ = '0.9.0'
+__version__ = '0.9.1.dev'
 __description__ = 'uData customizations for Data.gouv.fr'
 
 
