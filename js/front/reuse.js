76e23658c2ca0a180117b09949bb78ab4908abeb
Axel Haustant <axel.haustant@data.gouv.fr>
2017-01-17 18:25:11 +0100

    Enable tag suggestion on reuses (fix #704)

--- js/front/reuse.js
+++ js/front/reuse.js
@@ -10,16 +10,32 @@ import 'widgets/issues-btn';
 
 import Vue from 'vue';
 
+// Plugins
+import ScrollTo from 'plugins/scroll-to';
+
+// Components
 import FollowButton from 'components/buttons/follow.vue';
 import ShareButton from 'components/buttons/share.vue';
 import FeaturedButton from 'components/buttons/featured.vue';
 import DiscussionThreads from 'components/discussions/threads.vue';
 
+Vue.use(require('plugins/scroll-to'));
 
 new Vue({
     el: 'body',
     components: {FollowButton, ShareButton, DiscussionThreads, FeaturedButton},
     ready() {
         log.debug('Reuse display page');
+    },
+    methods: {
+        /**
+         * Suggest a tag aka.trigger a new discussion
+         */
+        suggestTag() {
+            this.$refs.discussions.start(
+                this._('New tag suggestion to improve metadata'),
+                this._('Hello,\n\nI propose this new tag: ')
+            );
+        }
     }
 });
