8fe42e280793125a49fe05a6d017654795813949
Axel Haustant <axel.haustant@data.gouv.fr>
2017-03-01 13:35:59 +0100

    Fix connection and database drop with new mongoengine

--- udata/tasks.py
+++ udata/tasks.py
@@ -92,7 +92,7 @@ def init_app(app):
 
     from udata.models import db
     with app.app_context():
-        default_url = 'mongodb://{0}:{1}'.format(*db.connection.client.address)
+        default_url = 'mongodb://{0}:{1}'.format(*db.connection.address)
     app.config.setdefault('CELERY_MONGODB_SCHEDULER_URL', default_url)
 
     celery.conf.update(app.config)
