a3818a6047553df531a628416e609402428bd311
Axel Haustant <axel.haustant@data.gouv.fr>
2017-03-04 13:50:24 +0100

    jquery free avatar cell

---
+++ js/dom-polyfills.js
@@ -0,0 +1,14 @@
+// closest polyfill
+// taken from https://developer.mozilla.org/en-US/docs/Web/API/Element/closest
+if (window.Element && !Element.prototype.closest) {
+    Element.prototype.closest = function(s) {
+        const matches = (this.document || this.ownerDocument).querySelectorAll(s);
+        let i;
+        let el = this;
+        do {
+            i = matches.length;
+            while (--i >= 0 && matches.item(i) !== el) {};
+        } while ((i < 0) && (el = el.parentElement));
+        return el;
+    };
+}
