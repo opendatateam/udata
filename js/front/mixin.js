58b6b6330385395406b0598da35c78ce67d19fa8
Axel Haustant <axel.haustant@data.gouv.fr>
2017-03-24 10:52:30 +0100

    Added notification handling to FrontMixin

--- js/front/mixin.js
+++ js/front/mixin.js
@@ -1,10 +1,29 @@
 import Vue from 'vue';
+import NotificationZone from 'components/notification-zone.vue';
 
 /**
 * Frontend views common behavior
 */
 export default {
     el: 'body',
+    components: {NotificationZone},
+    data() {
+        return {
+            notifications: [],
+        };
+    },
+    events: {
+        'notify:success': function(title, details) {
+            this.notifications.push({type: 'success', icon: 'check', title, details});
+        },
+        'notify:error': function(title, details) {
+            this.notifications.push({type: 'danger', icon: 'exclamation-triangle', title, details});
+        },
+        'notify:close': function(notification) {
+            const index = this.notifications.indexOf(notification);
+            this.notifications.splice(index, 1);
+        }
+    },
     methods: {
         /**
          * Insert a modal Vue in the application.
