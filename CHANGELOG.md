be0c35374ae7bc95c63fb1262b456c3fb3d8d362
Axel Haustant <noirbizarre@gmail.com>
2017-04-18 17:22:16 +0200

    Update to version 1.0.7.dev for next development cycle

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,9 @@
 # Changelog
 
+## Current (in progress)
+
+- Nothing yet
+
 ## 1.0.6 (2017-04-18)
 
 - Fixed numbering in system integrator FAQ (thanks to Bruno Cornec)
--- udata_gouvfr/__init__.py
+++ udata_gouvfr/__init__.py
@@ -4,7 +4,7 @@ uData customizations for Data.gouv.fr
 '''
 from __future__ import unicode_literals
 
-__version__ = '1.0.6'
+__version__ = '1.0.7.dev'
 __description__ = 'uData customizations for Data.gouv.fr'
 
 
