06f06c138b4f80c433157afa417d8d54c4cdf525
l-vincent-l <lara.vincent@gmail.com>
2017-05-17 14:55:12 +0200

    is_following is initiated in init_app

--- udata/core/followers/views.py
+++ udata/core/followers/views.py
@@ -1,18 +1,2 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
-from udata.auth import current_user
-
-from udata.i18n import I18nBlueprint
-
-from .models import Follow
-
-blueprint = I18nBlueprint('followers', __name__)
-
-
-@blueprint.app_template_global()
-@blueprint.app_template_filter()
-def is_following(obj):
-    if not current_user.is_authenticated:
-        return False
-    return Follow.objects.is_following(current_user._get_current_object(), obj)
