d3a3686ff9e2bb73cf24996094c50163f13e611c
taniki <tk@deveha.com>
2017-12-18 17:01:28 +0100

    reminder

--- js/components/tabs.vue
+++ js/components/tabs.vue
@@ -1,9 +1,12 @@
+<!--
+    REMOVE ME when https://github.com/yuche/vue-strap/issues/520 will be closed 
+-->
 <template>
   <!-- Nav tabs -->
   <ul class="nav nav-{{navStyle}}" role="tablist">
     <template v-for="t in headers">
       <li v-if="!t._tabgroup" :class="{active:t.active, disabled:t.disabled}" @click.prevent="select(t)">
-          <a href="#">{{{ t.header }}}</a>
+          <a href="#"><slot name="header">{{{ t.header }}}</slot></a>
       </li>
       <dropdown v-else :text="t.header" :class="{active:t.active}" :disabled="t.disabled">
         <li v-for="tab in t.tabs" :class="{disabled:tab.disabled}"><a href="#" @click.prevent="select(tab)">{{tab.header}}</a></li>
