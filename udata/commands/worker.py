09cac8f9d087263fdba6efcf93449b5824c56db6
Alexandre Bulté <alexandre@bulte.net>
2017-12-19 14:06:05 +0100

    udata worker status cmd

--- udata/commands/worker.py
+++ udata/commands/worker.py
@@ -1,20 +1,53 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
+import json
 import logging
+from collections import Counter
+from urlparse import urlparse
 
-from flask_script import Command
+import redis
 
-from udata.commands import manager
+from flask import current_app
+
+from udata.commands import submanager
 from udata.tasks import celery
 
 log = logging.getLogger(__name__)
 
+m = submanager(
+    'worker',
+    help='Worker related operations',
+    description='Handle all worker related operations and maintenance'
+)
 
-class Worker(Command):
-    '''Run a celery worker'''
-    def run(self, *args, **kwargs):
-        celery.start()
+
+@m.command
+def start():
+    """Start a worker"""
+    celery.start()
 
 
-manager.add_command('worker', Worker())
+@m.command
+def status():
+    """List queued tasks aggregated by name"""
+    parsed_url = urlparse(current_app.config['CELERY_BROKER_URL'])
+    db = parsed_url.path[1:] if parsed_url.path else 0
+    r = redis.StrictRedis(host=parsed_url.hostname, port=parsed_url.port,
+                          db=db)
+    queues = [q.name for q in current_app.config['CELERY_TASK_QUEUES']]
+    for queue in queues:
+        print('-' * 40)
+        queue_length = r.llen(queue)
+        print('Queue "%s": %s task(s)' % (queue, queue_length))
+        counter = Counter()
+        biggest_task_name = 0
+        for task in r.lrange(queue, 0, -1):
+            task = json.loads(task)
+            task_name = task['headers']['task']
+            if len(task_name) > biggest_task_name:
+                biggest_task_name = len(task_name)
+            counter[task_name] += 1
+        for count in counter.most_common():
+            print('* %s : %s' % (count[0].ljust(biggest_task_name), count[1]))
+    print('-' * 40)
