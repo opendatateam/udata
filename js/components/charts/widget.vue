e976c4d2bad87e6aaefb4b12b9ad4a448390a31b
Tam-Kien Duong <tk@deveha.com>
2018-01-03 11:37:21 +0100

    bump chartjs to v2 and migrate current uses

--- js/components/charts/widget.vue
+++ js/components/charts/widget.vue
@@ -43,7 +43,6 @@
 <script>
 import moment from 'moment';
 import Chart from 'chart.js';
-import 'Chart.StackedBar.js';
 
 import Box from 'components/containers/box.vue';
 
@@ -62,7 +61,13 @@ const AREA_OPTIONS = {
     // scaleShowGridLines: false,
     scaleShowHorizontalLines: true,
     scaleShowVerticalLines: true,
-    bezierCurveTension: 0.3,
+    lineTension: 0.3,
+    yAxes: [{
+        ticks: {
+            min: 0,
+            stepSize: 1
+        }
+    }]
 };
 
 const LINE_OPTIONS = Object.assign({}, AREA_OPTIONS, {
@@ -82,6 +87,14 @@ const BAR_OPTIONS = {
 };
 const STACKEDBAR_OPTIONS = Object.assign({}, BAR_OPTIONS, {
     scaleShowVerticalLines: false,
+    scales:{
+        xAxes: [{
+            stacked: true
+        }],
+        yAxes: [{
+            stacked: true
+        }]
+    }
 });
 
 const COLORS = [
@@ -133,13 +146,16 @@ export default {
                 datasets: this.y.map((serie, idx) => {
                     const dataset = {label: serie.label};
                     const color = serie.color || COLORS[idx];
-                    dataset.fillColor = this.toRGBA(color, .5);
-                    dataset.strokeColor = color;
-                    dataset.pointColor = color;
-                    // datasetpointStrokeColor: "#c1c7d1",
-                    dataset.pointHighlightFill = '#fff';
-                    dataset.pointHighlightStroke = color;
+
+                    dataset.backgroundColor = this.toRGBA(color, .5);
+                    dataset.borderColor = color;
+
+                    dataset.pointBackgroundColor = color;
+                    dataset.pointHoverBackgroundColor = '#fff';
+                    dataset.pointHoverBorderColor = color;
+
                     dataset.data = raw.map(item => item[serie.id]);
+                    
                     return dataset;
                 })
             };
@@ -174,16 +190,32 @@ export default {
             this.$els.legend.innerHTML = this.chart.generateLegend();
         },
         buildArea(ctx) {
-            return new Chart(ctx).Line(this.series, AREA_OPTIONS);
+            return new Chart(ctx, {
+                type: 'line',
+                data: this.series,
+                options: AREA_OPTIONS
+            });
         },
         buildBar(ctx) {
-            return new Chart(ctx).Bar(this.series, BAR_OPTIONS);
+            return new Chart(ctx,{
+                type: 'bar',
+                data: this.series,
+                options: BAR_OPTIONS
+            });
         },
         buildStackedBar(ctx) {
-            return new Chart(ctx).StackedBar(this.series, STACKEDBAR_OPTIONS);
+            return new Chart(ctx, {
+                type: 'bar',
+                data: this.series,
+                options: STACKEDBAR_OPTIONS
+            });
         },
         buildLine(ctx) {
-            return new Chart(ctx).Line(this.series, LINE_OPTIONS);
+            return new Chart(ctx, {
+                type: 'line',
+                data: this.series,
+                options: LINE_OPTIONS
+            });
         },
         cleanChart() {
             if (this.chart) {
