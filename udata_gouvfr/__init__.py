22a8af34bdafc2509ceaf6802e3545b882741c29
Axel Haustant <noirbizarre@gmail.com>
2018-02-10 13:38:45 +0100

    Remove useless init_app

--- udata_gouvfr/__init__.py
+++ udata_gouvfr/__init__.py
@@ -6,7 +6,3 @@ from __future__ import unicode_literals
 
 __version__ = '1.3.0.dev'
 __description__ = 'uData customizations for Data.gouv.fr'
-
-
-def init_app(app):
-    from . import harvesters  # noqa: needed for registration
