7119e795e0f4ec3407df0f9416fb38e20dacb988
Axel Haustant <axel.haustant@data.gouv.fr>
2018-03-23 12:29:56 +0100

    Fix topics mixing facets handling

--- js/front/search.js
+++ js/front/search.js
@@ -2,17 +2,14 @@
  * Search display page JS module
  */
 import FrontMixin from 'front/mixin';
+import FacetsMixin from 'front/mixins/facets';
 
 import log from 'logger';
 import Vue from 'vue';
-import velocity from 'velocity-animate';
-
-import TemporalCoverageFacet from 'components/facets/temporal-coverage.vue';
 
 
 new Vue({
-    mixins: [FrontMixin],
-    components: {TemporalCoverageFacet},
+    mixins: [FrontMixin, FacetsMixin],
     ready() {
         log.debug('Search page');
     },
@@ -47,37 +44,5 @@ new Vue({
                 }
             });
         },
-        /**
-         * Collapse or open a facet panel
-         * @param  {String} id The panel identifier to toggle
-         */
-        togglePanel(id, evt) {
-            const panel = document.getElementById(`facet-${id}`);
-            const removeBtn = document.getElementById(`facet-${id}-remove`);
-            if (removeBtn && removeBtn.contains(evt.target)) return;  // Do not react on remove button click
-            const chevrons = document.getElementById(`chevrons-${id}`);
-            if (panel.classList.contains('in')) {
-                velocity(panel, 'slideUp', {duration: 500}).then(() => {
-                    panel.classList.remove('in');
-                });
-            } else {
-                velocity(panel, 'slideDown', {duration: 500}).then(() => {
-                    panel.classList.add('in');
-                });
-            }
-            chevrons.classList.toggle('fa-chevron-up');
-            chevrons.classList.toggle('fa-chevron-down');
-        },
-        /**
-         * Expand a panel (diplay more details)
-         * @param  {String} id The panel identifier to expand
-         */
-        expandPanel(id, evt) {
-            evt.target.remove();
-            const panel = document.getElementById(`facet-${id}-more`);
-            velocity(panel, 'slideDown', {duration: 500}).then(() => {
-                panel.classList.add('in');
-            });
-        }
     }
 });
