e691de0fa3a037a26f86f81bd9f3e3f6125c7e10
Axel Haustant <axel.haustant@data.gouv.fr>
2018-03-23 09:12:51 +0100

    Standalone OEmbed loader

--- webpack.config.oembed.js
+++ webpack.config.oembed.js
@@ -1,8 +1,9 @@
 const path = require('path');
+const webpack = require('webpack');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const less_loader = ExtractTextPlugin.extract('vue-style?sourceMap', 'css?sourceMap!less?sourceMap=source-map-less-inline');
 
-module.exports = {
+const config = {
     entry: './js/oembed',
     output: {
         path: path.join(__dirname, 'udata', 'static'),
@@ -32,5 +33,33 @@ module.exports = {
         // Prevent webpack 1.x false positive
         require('webpack-fail-plugin'),
         new ExtractTextPlugin('oembed.css'),
+        new webpack.DefinePlugin({
+            'process.env': {
+                'NODE_ENV': JSON.stringify(process.env.NODE_ENV)
+            }
+        }),
     ],
 };
+
+if (process.env.NODE_ENV === 'production') {
+    config.plugins.push(
+        new webpack.optimize.UglifyJsPlugin({
+            minimize: true,
+            output: {
+                comments: false,
+                screw_ie8: true
+            },
+            mangle: {
+                screw_ie8: true
+            },
+            compress: {
+                warnings: false,
+                screw_ie8: true
+            }
+        })
+    );
+} else {
+    config.devtool = '#cheap-module-source-map';
+}
+
+module.exports = config;
