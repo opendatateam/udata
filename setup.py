8cb8631fa3f3f4c02d47fca09b49d89d3043fc9e
Axel Haustant <axel.haustant@data.gouv.fr>
2018-04-12 18:01:16 +0200

    Prepare for legacy PyPI closure and PyPI warehouse supporting markdown

--- setup.py
+++ setup.py
@@ -8,27 +8,12 @@ import re
 from setuptools import setup, find_packages
 
 RE_REQUIREMENT = re.compile(r'^\s*-r\s*(?P<filename>.*)$')
-RE_MD_CODE_BLOCK = re.compile(r'```(?P<language>\w+)?\n(?P<lines>.+?)```', re.S)
-RE_CODE = re.compile(r'`(?P<code>[^`]+)`')
-RE_SELF_LINK = re.compile(r'\[([^\]]+)\]\[\]')
-RE_LINK_TO_URL = re.compile(r'\[(?P<text>[^\]]+)\]\((?P<url>[^\)]+)\)')
-RE_LINK_TO_REF = re.compile(r'\[(?P<text>[^\]]+)\]\[(?P<ref>[^\]]+)\]')
-RE_LINK_REF = re.compile(r'^\[(?P<key>[^!].+?)\]:\s*(?P<url>.*)$', re.M)
 RE_BADGE = re.compile(r'^\[\!\[(?P<text>[^\]]+)\]\[(?P<badge>[^\]]+)\]\]\[(?P<target>[^\]]+)\]$', re.M)
-RE_TITLE = re.compile(r'^(?P<level>#+)\s*(?P<title>.+)$', re.M)
 
 BADGES_TO_KEEP = ['gitter-badge', 'readthedocs-badge']
 
-RST_TITLE_LEVELS = ['=', '-', '*']
 
-RST_BADGE = '''\
-.. image:: {badge}
-    :target: {target}
-    :alt: {text}
-'''
-
-
-def md2pypi(filename):
+def md(filename):
     '''
     Load .md (markdown) file and sanitize it for PyPI.
     Remove unsupported github tags:
@@ -37,53 +22,15 @@ def md2pypi(filename):
     '''
     content = io.open(filename).read()
 
-    for match in RE_CODE.finditer(content):
-        rst_code = '``{code}``'.format(**match.groupdict())
-        content = content.replace(match.group(0), rst_code)
-
-    for match in RE_MD_CODE_BLOCK.finditer(content):
-        rst_block = '\n'.join(
-            ['.. code-block:: {language}'.format(**match.groupdict()), ''] +
-            ['    {0}'.format(l) for l in match.group('lines').split('\n')] +
-            ['']
-        )
-        content = content.replace(match.group(0), rst_block)
-
-    refs = dict(RE_LINK_REF.findall(content))
-    content = RE_LINK_REF.sub('.. _\g<key>: \g<url>', content)
-    content = RE_SELF_LINK.sub('`\g<1>`_', content)
-    content = RE_LINK_TO_URL.sub('`\g<text> <\g<url>>`__', content)
-
     for match in RE_BADGE.finditer(content):
         if match.group('badge') not in BADGES_TO_KEEP:
             content = content.replace(match.group(0), '')
-        else:
-            params = match.groupdict()
-            params['badge'] = refs[match.group('badge')]
-            params['target'] = refs[match.group('target')]
-            content = content.replace(match.group(0),
-                                      RST_BADGE.format(**params))
-    # Must occur after badges
-    for match in RE_LINK_TO_REF.finditer(content):
-        content = content.replace(match.group(0), '`{text} <{url}>`_'.format(
-            text=match.group('text'),
-            url=refs[match.group('ref')]
-        ))
-
-    for match in RE_TITLE.finditer(content):
-        underchar = RST_TITLE_LEVELS[len(match.group('level')) - 1]
-        title = match.group('title')
-        underline = underchar * len(title)
-
-        full_title = '\n'.join((title, underline))
-        content = content.replace(match.group(0), full_title)
-
     return content
 
 
 long_description = '\n'.join((
-    md2pypi('README.md'),
-    md2pypi('CHANGELOG.md'),
+    md('README.md'),
+    md('CHANGELOG.md'),
     ''
 ))
 
@@ -111,12 +58,14 @@ setup(
     version=__import__('udata').__version__,
     description=__import__('udata').__description__,
     long_description=long_description,
+    long_description_content_type='text/markdown',
     url='https://github.com/opendatateam/udata',
     author='Opendata Team',
     author_email='opendatateam@data.gouv.fr',
     packages=find_packages(),
     include_package_data=True,
     install_requires=install_requires,
+    setup_requires=['setuptools>=38.6.0'],
     tests_require=tests_require,
     extras_require={
         'test': tests_require,
