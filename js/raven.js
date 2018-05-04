2ef2659c54e58d2e9043b287289af05169e2e5ce
Axel Haustant <axel.haustant@data.gouv.fr>
2018-05-04 20:45:25 +0200

    Mute the uncaught but managed AuthenticationRequired js error
    Fix WWW-DATAGOUVFR-5ESS
    Fix WWW-DATAGOUVFR-5JQV
    Fix WWW-DATAGOUVFR-5FJW
    Fix WWW-DATAGOUVFR-5HKJ
    Fix WWW-DATAGOUVFR-5F1R
    Fix WWW-DATAGOUVFR-5EJJ

--- js/raven.js
+++ js/raven.js
@@ -6,6 +6,9 @@ if (config.sentry) {
         logger: 'udata.js',  // Allow filtering in Sentry
         release: config.sentry.release,
         tags: config.sentry.tags,
+        ignoreErrors: [
+            'AuthenticationRequired',  // Uncaught but managed
+        ],
     };
 
     Raven.config(config.sentry.dsn, options).install();
