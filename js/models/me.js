9f12ac537d22a0aa8b3dfc61b0586c5ada0701b1
Axel Haustant <axel.haustant@data.gouv.fr>
2018-05-04 20:39:36 +0200

    Declare current user to Sentry only if enabled

--- js/models/me.js
+++ js/models/me.js
@@ -1,6 +1,6 @@
-import log from 'logger';
 import User from 'models/user';
 import Raven from 'raven';
+import config from 'config';
 
 class Me extends User {
     fetch() {
@@ -13,18 +13,20 @@ class Me extends User {
     }
 
     on_user_fetched(response) {
-        Raven.setUserContext({
-            id: response.obj.id,
-            email: response.obj.email,
-            slug: response.obj.slug,
-            fullname: `${response.obj.first_name} ${response.obj.last_name}`,
-            is_authenticated: true,
-            is_anonymous: false
-        });
+        if (config.sentry) {
+            Raven.setUserContext({
+                id: response.obj.id,
+                email: response.obj.email,
+                slug: response.obj.slug,
+                fullname: `${response.obj.first_name} ${response.obj.last_name}`,
+                is_authenticated: true,
+                is_anonymous: false
+            });
+        }
         this.on_fetched(response);
     }
 }
 
-var me = new Me();
+const me = new Me();
 
 export default me.fetch();
