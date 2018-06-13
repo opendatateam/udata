ec3a39f59116d634e39e724b908b012bd691b361
Alexandre Bulté <alexandre@bulte.net>
2018-06-13 14:05:35 +0000

    API: enforce application/json content-type for forms (#1751)

--- udata/tests/api/test_base_api.py
+++ udata/tests/api/test_base_api.py
@@ -4,6 +4,7 @@ from __future__ import unicode_literals
 from flask import url_for
 
 from udata.api import api, API
+from udata.forms import Form, fields
 
 from . import APITestCase
 
@@ -16,7 +17,16 @@ class FakeAPI(API):
         return {'success': True}
 
 
-class APIAuthTest(APITestCase):
+@ns.route('/fake-form', endpoint='fake-form')
+class FakeFormAPI(API):
+    def post(self):
+        class FakeForm(Form):
+            pass
+        api.validate(FakeForm)
+        return {'success': True}
+
+
+class OptionsCORSTest(APITestCase):
     def test_should_allow_options_and_cors(self):
         '''Should allow OPTIONS operation and give cors parameters'''
         response = self.client.options(url_for('api.fake-options'))
@@ -27,3 +37,21 @@ class APIAuthTest(APITestCase):
         self.assertIn('HEAD', allowed_methods)
         self.assertIn('OPTIONS', allowed_methods)
         self.assertIn('GET', allowed_methods)
+
+
+class JSONFormRequestTest(APITestCase):
+    def test_non_json_content_type(self):
+        '''We expect JSON requests for forms and enforce it'''
+        response = self.client.post(url_for('api.fake-form'), {}, headers={
+            'Content-Type': 'multipart/form-data'
+        })
+        self.assert400(response)
+        self.assertEquals(
+            response.json,
+            {'errors': {'Content-Type': 'expecting application/json'}}
+        )
+
+    def test_json_content_type(self):
+        '''We expect JSON requests for forms and enforce it'''
+        response = self.post(url_for('api.fake-form'), {})
+        self.assert200(response)
