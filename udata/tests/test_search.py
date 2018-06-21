743672943a1f65dea5040406b18d224ae146cf6f
Axel Haustant <noirbizarre@gmail.com>
2018-06-21 11:17:28 +0200

    Fix search tests

--- udata/tests/test_search.py
+++ udata/tests/test_search.py
@@ -1306,7 +1306,7 @@ class SearchAdaptorTest(SearchTestMixin, TestCase):
 
         # query + facets selector + tag and other facets + sorts + pagination
         self.assertEqual(len(parser.args), 7)
-        self.assertHasArgument(parser, 'q', unicode)
+        self.assertHasArgument(parser, 'q', str)
         self.assertHasArgument(parser, 'sort', str)
         self.assertHasArgument(parser, 'facets', str)
         self.assertHasArgument(parser, 'tag', clean_string)
@@ -1320,7 +1320,7 @@ class SearchAdaptorTest(SearchTestMixin, TestCase):
 
         # query + facets selector + boolean facet + sorts + pagination
         self.assertEqual(len(parser.args), 6)
-        self.assertHasArgument(parser, 'q', unicode)
+        self.assertHasArgument(parser, 'q', str)
         self.assertHasArgument(parser, 'sort', str)
         self.assertHasArgument(parser, 'facets', str)
         self.assertHasArgument(parser, 'boolean', inputs.boolean)
@@ -1334,7 +1334,7 @@ class SearchAdaptorTest(SearchTestMixin, TestCase):
 
         # query + facets selector + range facet + sorts + pagination
         self.assertEqual(len(parser.args), 6)
-        self.assertHasArgument(parser, 'q', unicode)
+        self.assertHasArgument(parser, 'q', str)
         self.assertHasArgument(parser, 'sort', str)
         self.assertHasArgument(parser, 'facets', str)
         self.assertHasArgument(parser, 'range', facet.validate_parameter,
@@ -1349,7 +1349,7 @@ class SearchAdaptorTest(SearchTestMixin, TestCase):
 
         # query + facets selector + range facet + sorts + pagination
         self.assertEqual(len(parser.args), 6)
-        self.assertHasArgument(parser, 'q', unicode)
+        self.assertHasArgument(parser, 'q', str)
         self.assertHasArgument(parser, 'sort', str)
         self.assertHasArgument(parser, 'facets', str)
         self.assertHasArgument(parser, 'coverage', facet.validate_parameter)
