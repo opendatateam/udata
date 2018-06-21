a3bf10ffe67863959c0bfa0c7f482fcf30114d3e
Alexandre Bulté <alexandre@bulte.net>
2018-06-21 11:03:25 +0200

    xrange -> range

--- udata/utils.py
+++ udata/utils.py
@@ -75,7 +75,7 @@ class Paginable(object):
     def iter_pages(self, left_edge=2, left_current=2, right_current=5,
                    right_edge=2):
         last = 0
-        for num in xrange(1, self.pages + 1):
+        for num in range(1, self.pages + 1):
             if (num <= left_edge or
                     (num > self.page - left_current - 1 and
                         num < self.page + right_current) or
