9d1730c663060789136a1acf157399f9459fa7c2
Axel Haustant <axel.haustant@data.gouv.fr>
2018-08-17 17:50:21 +0200

    Port frontend filters test to pytest

--- udata/tests/frontend/test_frontend_filters.py
+++ udata/tests/frontend/test_frontend_filters.py
@@ -1,46 +1,46 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
+import pytest
+
 from datetime import date
 
 from flask import url_for, render_template_string, g, Blueprint
 
-from . import FrontTestCase
-
 from udata.i18n import I18nBlueprint
 from udata.models import db
 from udata.frontend.helpers import in_url
-from udata.tests.helpers import assert_urls_equal
+from udata.tests.helpers import assert_urls_equal, full_url
 
 
 def iso2date(string):
     return date(*[int(v) for v in string.split('-')])
 
 
-def dr(start, end):
-    return db.DateRange(start=iso2date(start), end=iso2date(end))
+def dr(start, end, **kwargs):
+    return db.DateRange(start=iso2date(start), end=iso2date(end), **kwargs)
 
 
-class FrontEndRootTest(FrontTestCase):
-    modules = ['core.site']
-
-    def test_rewrite(self):
+@pytest.mark.usefixtures('app')
+@pytest.mark.frontend(['core.site'])
+class FrontEndRootTest:
+    def test_rewrite(self, app):
         '''url_rewrite should replace a parameter in the URL if present'''
         url = url_for('site.home', one='value', two='two')
-        expected = self.full_url('site.home', one='other-value', two=2)
+        expected = full_url('site.home', one='other-value', two=2)
 
-        with self.app.test_request_context(url):
+        with app.test_request_context(url):
             result = render_template_string(
                 "{{ url_rewrite(one='other-value', two=2) }}")
 
         assert_urls_equal(result, expected)
 
-    def test_rewrite_append(self):
+    def test_rewrite_append(self, app):
         '''url_rewrite should replace a parameter in the URL if present'''
         url = url_for('site.home')
-        expected = self.full_url('site.home', one='value')
+        expected = full_url('site.home', one='value')
 
-        with self.app.test_request_context(url):
+        with app.test_request_context(url):
             result = render_template_string("{{ url_rewrite(one='value') }}")
 
         assert_urls_equal(result, expected)
@@ -93,28 +93,28 @@ class FrontEndRootTest(FrontTestCase):
 
         assert_urls_equal(result, expected)
 
-    def test_args_in_url(self):
+    def test_args_in_url(self, app):
         '''in_url() should test the presence of a key in url'''
         url = url_for('site.home', key='value', other='other')
 
-        with self.app.test_request_context(url):
-            self.assertTrue(in_url('key'))
-            self.assertTrue(in_url('other'))
-            self.assertTrue(in_url('key', 'other'))
-            self.assertFalse(in_url('fake'))
-            self.assertFalse(in_url('key', 'fake'))
+        with app.test_request_context(url):
+            assert in_url('key')
+            assert in_url('other')
+            assert in_url('key', 'other')
+            assert not in_url('fake')
+            assert not in_url('key', 'fake')
 
-    def test_kwargs_in_url(self):
+    def test_kwargs_in_url(self, app):
         '''in_url() should test the presence of key/value pair in url'''
         url = url_for('site.home', key='value', other='other')
 
-        with self.app.test_request_context(url):
-            self.assertTrue(in_url(key='value'))
-            self.assertTrue(in_url(key='value', other='other'))
-            self.assertFalse(in_url(key='other'))
-            self.assertFalse(in_url(key='value', other='value'))
+        with app.test_request_context(url):
+            assert in_url(key='value')
+            assert in_url(key='value', other='other')
+            assert not in_url(key='other')
+            assert not in_url(key='value', other='value')
 
-            self.assertTrue(in_url('other', key='value'))
+            assert in_url('other', key='value')
 
     def test_as_filter(self):
         '''URL helpers should exists as filter'''
@@ -154,125 +154,99 @@ class FrontEndRootTest(FrontTestCase):
             url_for('site.home')
         )
 
-    def test_as_global_default(self):
+    def test_as_global_default(self, app):
         '''URL helpers should exists as global function without url param'''
         url = url_for('site.home', one='value')
 
-        with self.app.test_request_context(url):
+        with app.test_request_context(url):
             assert_urls_equal(
                 render_template_string("{{ url_rewrite(one='other-value') }}"),
-                self.full_url('site.home', one='other-value')
+                full_url('site.home', one='other-value')
             )
             assert_urls_equal(
                 render_template_string("{{ url_add(two='other-value') }}"),
-                self.full_url('site.home', one='value', two='other-value')
+                full_url('site.home', one='value', two='other-value')
             )
             assert_urls_equal(
                 render_template_string("{{ url_del(None, 'one') }}"),
-                self.full_url('site.home')
+                full_url('site.home')
             )
-            self.assertEqual(
-                render_template_string("{{ in_url('one') }}"),
-                'True'
-            )
-            self.assertEqual(
-                render_template_string("{{ in_url('one') }}"),
-                'True'
-            )
-            self.assertEqual(
-                render_template_string("{{ in_url('two') }}"),
-                'False'
-            )
+            assert render_template_string("{{ in_url('one') }}") == 'True'
+            assert render_template_string("{{ in_url('one') }}") == 'True'
+            assert render_template_string("{{ in_url('two') }}") == 'False'
 
-    def test_daterange(self):
+    @pytest.mark.parametrize('dates,expected', (
+        (('2014-02-01', '2014-02-01'), '2014'),
+        (('2012-01-01', '2012-01-31'), '2012'),
+        (('2012-01-01', '2012-01-14'), '2012'),
+        (('2012-01-01', '2012-03-31'), '2012'),
+        (('2012-01-01', '2012-02-29'), '2012'),
+        (('2012-01-01', '2012-12-31'), '2012'),
+        (('2012-01-01', '2014-12-31'), '2012–2014'),
+        (('2012-02-02', '2014-12-25'), '2012–2014'),
+        # Before 1900
+        (('1234-02-01', '1234-02-01'), '1234'),
+        (('1232-01-01', '1232-01-31'), '1232'),
+        (('1232-01-01', '1232-01-14'), '1232'),
+        (('1232-01-01', '1232-03-31'), '1232'),
+        (('1232-01-01', '1232-02-29'), '1232'),
+        (('1232-01-01', '1232-12-31'), '1232'),
+        (('1232-01-01', '1234-12-31'), '1232–1234'),
+        (('1232-02-02', '1234-12-25'), '1232–1234'),
+    ))
+    def test_daterange(self, dates, expected):
         '''Daterange filter should display range in an adaptive'''
         g.lang_code = 'en'
+        tpl = '{{dates|daterange}}'
 
-        specs = (
-            (dr('2014-02-01', '2014-02-01'), '2014'),
-            (dr('2012-01-01', '2012-01-31'), '2012'),
-            (dr('2012-01-01', '2012-01-14'), '2012'),
-            (dr('2012-01-01', '2012-03-31'), '2012'),
-            (dr('2012-01-01', '2012-02-29'), '2012'),
-            (dr('2012-01-01', '2012-12-31'), '2012'),
-            (dr('2012-01-01', '2014-12-31'), '2012–2014'),
-            (dr('2012-02-02', '2014-12-25'), '2012–2014'),
-        )
-        for given, expected in specs:
-            self.assertEqual(
-                render_template_string('{{given|daterange}}', given=given),
-                expected
-            )
-
-    def test_daterange_before_1900(self):
-        '''Daterange filter should display range in an adaptive'''
-        g.lang_code = 'en'
-
-        specs = (
-            (dr('1234-02-01', '1234-02-01'), '1234'),
-            (dr('1232-01-01', '1232-01-31'), '1232'),
-            (dr('1232-01-01', '1232-01-14'), '1232'),
-            (dr('1232-01-01', '1232-03-31'), '1232'),
-            (dr('1232-01-01', '1232-02-29'), '1232'),
-            (dr('1232-01-01', '1232-12-31'), '1232'),
-            (dr('1232-01-01', '1234-12-31'), '1232–1234'),
-            (dr('1232-02-02', '1234-12-25'), '1232–1234'),
-        )
-        for given, expected in specs:
-            self.assertEqual(
-                render_template_string('{{given|daterange}}', given=given),
-                expected
-            )
+        assert render_template_string(tpl, dates=dr(*dates)) == expected
 
     def test_daterange_bad_type(self):
         '''Daterange filter should only accept db.DateRange as parameter'''
-        with self.assertRaises(ValueError):
+        with pytest.raises(ValueError):
             render_template_string('{{"value"|daterange}}')
 
     def test_ficon(self):
         '''Should render a font-awesome icon class'''
-        self.assertEqual(render_template_string('{{ficon("icon")}}'),
-                         'fa fa-icon')
-        self.assertEqual(render_template_string('{{ficon("fa-icon")}}'),
-                         'fa fa-icon')
+        assert render_template_string('{{ficon("icon")}}') == 'fa fa-icon'
+        assert render_template_string('{{ficon("fa-icon")}}') == 'fa fa-icon'
 
-    def test_i18n_alternate_links(self):
+    def test_i18n_alternate_links(self, app, client):
         test = I18nBlueprint('test', __name__)
 
         @test.route('/i18n/<key>/')
         def i18n(key):
             return render_template_string('{{ i18n_alternate_links() }}')
 
-        self.app.register_blueprint(test)
-        self.app.config['DEFAULT_LANGUAGE'] = 'en'
-        self.app.config['LANGUAGES'] = {
+        app.register_blueprint(test)
+        app.config['DEFAULT_LANGUAGE'] = 'en'
+        app.config['LANGUAGES'] = {
             'en': 'English',
             'fr': 'Français',
             'de': 'German',
         }
 
-        response = self.get(url_for('test.i18n', key='value', param='other'))
+        response = client.get(url_for('test.i18n', key='value', param='other'))
         link = ('<link rel="alternate" '
                 'href="/{lang}/i18n/value/?param=other" '
                 'hreflang="{lang}" />')
-        self.assertEqual(
-            response.data,
-            ''.join([link.format(lang='fr'), link.format(lang='de')]))
+        assert response.data == ''.join([link.format(lang='fr'),
+                                         link.format(lang='de')])
 
-    def test_i18n_alternate_links_outside_i18n_blueprint(self):
+    def test_i18n_alternate_links_outside_i18n_blueprint(self, app, client):
         test = Blueprint('test', __name__)
 
         @test.route('/not-i18n/<key>/')
         def i18n(key):
             return render_template_string('{{ i18n_alternate_links() }}')
 
-        self.app.register_blueprint(test)
-        self.app.config['DEFAULT_LANGUAGE'] = 'en'
-        self.app.config['LANGUAGES'] = {
+        app.register_blueprint(test)
+        app.config['DEFAULT_LANGUAGE'] = 'en'
+        app.config['LANGUAGES'] = {
             'en': 'English',
             'fr': 'Français',
             'de': 'German',
         }
 
-        response = self.get(url_for('test.i18n', key='value', param='other'))
-        self.assertEqual(response.data, '')
+        response = client.get(url_for('test.i18n', key='value', param='other'))
+        assert response.data == ''
