75c6e281b98c17c81ac3ef9a0206ccfa1d88c2a9
Alexandre Bulté <alexandre@bulte.net>
2018-10-02 11:36:20 +0200

    Fix dataset follower route in admin (client)

--- js/models/followers.js
+++ js/models/followers.js
@@ -6,6 +6,6 @@ export default class FollowPage extends ModelPage {
     constructor(options) {
         super(options);
         // this.$options.ns = 'datasets';
-        this.$options.fetch = 'list_followers';
+        this.$options.fetch = 'list_dataset_followers';
     }
 };
