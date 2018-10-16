2c74e06cef982c9753f0e67a3481ff872a7e8949
Alexandre Bulté <alexandre@bulte.net>
2018-10-16 12:26:30 +0200

    Harvest items can now match source.id before source.domain

--- udata/harvest/backends/base.py
+++ udata/harvest/backends/base.py
@@ -246,10 +246,18 @@ class BaseBackend(object):
         after_harvest_job.send(self)
 
     def get_dataset(self, remote_id):
-        '''Get or create a dataset given its remote ID (and its source)'''
+        '''Get or create a dataset given its remote ID (and its source)
+        We first try to match `source_id` to be source domain independent
+        '''
+        dataset = Dataset.objects(__raw__={
+            'extras.harvest:source_id': self.source.id,
+            'extras.harvest:remote_id': remote_id,
+        }).first()
+        if dataset:
+            return dataset
         dataset = Dataset.objects(__raw__={
             'extras.harvest:remote_id': remote_id,
-            'extras.harvest:domain': self.source.domain
+            'extras.harvest:domain': self.source.domain,
         }).first()
         return dataset or Dataset()
 
