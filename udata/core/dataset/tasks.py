97efb2c88e4d003e3422c63644305607bcd8c92e
Alexandre Bulté <alexandre@bulte.net>
2019-08-08 17:25:22 +0200

    review fix

--- udata/core/dataset/tasks.py
+++ udata/core/dataset/tasks.py
@@ -158,13 +158,13 @@ def export_csv_for_model(model, dataset):
         csvfile.seek(0)
         # make a resource from this tmp file
         created, resource = store_resource(csvfile, model, dataset)
-        csvfile.close()
         # add it to the dataset
         if created:
             dataset.add_resource(resource)
         dataset.last_modified = datetime.now()
         dataset.save()
     finally:
+        csvfile.close()
         os.unlink(csvfile.name)
 
 
