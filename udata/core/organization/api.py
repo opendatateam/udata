3dd7360262652266d6bbaed01a93829e3d4e7fa0
Axel Haustant <axel.haustant@data.gouv.fr>
2019-09-02 16:12:22 +0200

    Organization/Membership celery tasks compatibility

--- udata/core/organization/api.py
+++ udata/core/organization/api.py
@@ -186,7 +186,7 @@ class MembershipRequestAPI(API):
         form.populate_obj(membership_request)
         org.save()
 
-        notify_membership_request.delay(org, membership_request)
+        notify_membership_request.delay(str(org.id), str(membership_request.id))
 
         return membership_request, code
 
@@ -221,7 +221,7 @@ class MembershipAcceptAPI(MembershipAPI):
         org.members.append(member)
         org.save()
 
-        notify_membership_response.delay(org, membership_request)
+        notify_membership_response.delay(str(org.id), str(membership_request.id))
 
         return member
 
@@ -244,7 +244,7 @@ class MembershipRefuseAPI(MembershipAPI):
 
         org.save()
 
-        notify_membership_response.delay(org, membership_request)
+        notify_membership_response.delay(str(org.id), str(membership_request.id))
 
         return {}, 200
 
