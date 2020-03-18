2487ddc1eb38e0b43dfef3cc8f8d2533520acbe3
quaxsze <service@plum.eu.com>
2020-03-18 19:47:46 +0100

    fix2\ test

--- udata/tests/site/test_site_metrics.py
+++ udata/tests/site/test_site_metrics.py
@@ -10,19 +10,22 @@ from udata.tests.helpers import assert_emit
 @pytest.mark.usefixtures('clean_db')
 class SiteMetricTest:
     def test_dataset_metric(self, app):
+        site = SiteFactory.create(
+            id=app.config['SITE_ID']
+        )
         DatasetFactory.create_batch(2)
         VisibleDatasetFactory.create_batch(3)
-
-        site = Site.objects.get(id=app.config['SITE_ID'])
 
         site.count_datasets()
 
         assert site.get_metrics()['datasets'] == 3
 
     def test_resources_metric(self, app):
-        DatasetFactory.create_batch(3, nb_resources=3)
+        site = SiteFactory.create(
+            id=app.config['SITE_ID']
+        )
 
-        site = Site.objects.get(id=app.config['SITE_ID'])
+        DatasetFactory.create_batch(3, nb_resources=3)
 
         site.count_datasets()
         site.count_resources()
