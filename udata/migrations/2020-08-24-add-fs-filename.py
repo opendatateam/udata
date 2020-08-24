6f95492593b39d5c44349890c1e08483f8af8461
quaxsze <service@plum.eu.com>
2020-08-24 17:00:39 +0200

    refactor else statement

--- udata/migrations/2020-08-24-add-fs-filename.py
+++ udata/migrations/2020-08-24-add-fs-filename.py
@@ -22,8 +22,7 @@ def migrate(db):
                 fs_name = parsed.path.replace('/resources/', '')
                 resource.fs_filename = fs_name
                 save_res = True
-            else:
-                if resource.fs_filename is not None:
+            elif resource.fs_filename is not None:
                     resource.fs_filename = None
                     save_res = True
         if save_res:
@@ -37,17 +36,20 @@ def migrate(db):
 
     community_resources = CommunityResource.objects().no_cache().timeout(False)
     for community_resource in community_resources:
+        save_res = False
         if community_resource.url.startswith(storages.resources.base_url):
             parsed = urlparse(community_resource.url)
             fs_name = parsed.path.replace('/resources/', '')
             community_resource.fs_filename = fs_name
-        else:
-            if community_resource.fs_filename is not None:
+            save_res = True
+        elif community_resource.fs_filename is not None:
                 community_resource.fs_filename = None
-        try:
-            community_resource.save()
-        except Exception as e:
-            log.warning(e)
-            pass
+                save_res = True
+        if save_res:
+            try:
+                community_resource.save()
+            except Exception as e:
+                log.warning(e)
+                pass
 
     log.info('Completed.')
