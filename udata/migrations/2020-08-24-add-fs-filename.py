c2c4fadafefb6056c3cbcde9785aa6f84253ce0c
Stanislas P <57526019+quaxsze@users.noreply.github.com>
2020-08-25 11:01:27 +0200

    Fix static files deletion (#2526)

--- udata/migrations/2020-08-24-add-fs-filename.py
+++ udata/migrations/2020-08-24-add-fs-filename.py
@@ -4,6 +4,7 @@ The purpose here is to fill every resource with a fs_filename string field.
 import logging
 from urllib.parse import urlparse
 
+from udata.core import storages
 from udata.models import Dataset, CommunityResource
 
 log = logging.getLogger(__name__)
@@ -16,10 +17,13 @@ def migrate(db):
     for dataset in datasets:
         save_res = False
         for resource in dataset.resources:
-            if resource.url.startswith('https://static.data.gouv.fr'):
+            if resource.url.startswith(storages.resources.base_url):
                 parsed = urlparse(resource.url)
-                fs_name = parsed.path.strip('/resource/')
+                fs_name = parsed.path.replace('/resources/', '')
                 resource.fs_filename = fs_name
+                save_res = True
+            elif resource.fs_filename is not None:
+                resource.fs_filename = None
                 save_res = True
         if save_res:
             try:
@@ -32,13 +36,20 @@ def migrate(db):
 
     community_resources = CommunityResource.objects().no_cache().timeout(False)
     for community_resource in community_resources:
-        parsed = urlparse(community_resource.url)
-        fs_name = parsed.path.strip('/resource/')
-        community_resource.fs_filename = fs_name
-        try:
-            community_resource.save()
-        except Exception as e:
-            log.warning(e)
-            pass
+        save_res = False
+        if community_resource.url.startswith(storages.resources.base_url):
+            parsed = urlparse(community_resource.url)
+            fs_name = parsed.path.replace('/resources/', '')
+            community_resource.fs_filename = fs_name
+            save_res = True
+        elif community_resource.fs_filename is not None:
+            community_resource.fs_filename = None
+            save_res = True
+        if save_res:
+            try:
+                community_resource.save()
+            except Exception as e:
+                log.warning(e)
+                pass
 
     log.info('Completed.')
