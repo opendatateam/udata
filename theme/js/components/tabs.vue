37135dd5bc9a13d3e6e75355179516e6dfbded8a
Grégoire Mougeolle <gregoire@nextmap.io>
2020-09-04 17:02:23 +0200

    (config) parcel is now compiling VueJS SFCs
    Signed-off-by: Grégoire Mougeolle <gregoire@nextmap.io>
    Signed-off-by: Grégoire Mougeolle <gregoire@nextmap.io>
    Signed-off-by: Grégoire Mougeolle <gregoire@nextmap.io>

---
+++ theme/js/components/tabs.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="tabby">
+    Coucou !
+    <Slot />
+  </div>
+</template>
+
+<script>
+export default {
+  created: () => {
+    console.log('Im compiling')
+  },
+};
+</script>
---
+++ theme/js/index.js
@@ -0,0 +1,9 @@
+import Vue from "vue/dist/vue.js";
+import Tabs from "./components/tabs.vue";
+
+new Vue({
+  el: "#app",
+  components: { Tabs },
+});
+
+console.log("JS is injected !");
