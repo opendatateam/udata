75483f1fbba3603c298602ccc9ff77f818949f31
Grégoire Mougeolle <gregoire@nextmap.io>
2020-09-22 14:05:42 +0200

    (oops) APIGOUVFR_EXTRAS_KEY took a bullet for nothing
    Signed-off-by: Grégoire Mougeolle <gregoire@nextmap.io>

--- udata_gouvfr/__init__.py
+++ udata_gouvfr/__init__.py
@@ -4,3 +4,14 @@ udata customizations for data.gouv.fr
 
 __version__ = '3.0.0.dev'
 __description__ = 'udata customizations for data.gouv.fr'
+
+APIGOUVFR_EXTRAS_KEY = 'apigouvfr:apis'
+APIGOUVFR_EXPECTED_FIELDS = [
+    'title',
+    'tagline',
+    'path',
+    'slug',
+    'owner',
+    'openness',
+    'logo',
+]
