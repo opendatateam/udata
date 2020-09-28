f7aa94e54720892f94fd57f1990f5a173135a361
quaxsze <service@plum.eu.com>
2020-09-28 17:44:29 +0200

    add migration

---
+++ udata/migrations/2020-09-28-update-reuses-datasets-metrics.py
@@ -0,0 +1,19 @@
+'''
+The purpose here is to update every reuse's datasets metrics
+after the PR https://github.com/opendatateam/udata/pull/2531
+'''
+import logging
+
+from udata.models import Reuse
+
+log = logging.getLogger(__name__)
+
+
+def migrate(db):
+    log.info('Processing reuses.')
+
+    reuses = Reuse.objects().no_cache().timeout(False)
+    for reuse in reuses:
+        reuse.count_datasets()
+
+    log.info('Completed.')
