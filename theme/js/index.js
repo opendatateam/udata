6ee183b0106ec433e803207fba1c98959eeeecbb
Grégoire Mougeolle <gregoire@nextmap.io>
2020-11-13 18:46:37 +0100

    (js) Auth plugin
    Signed-off-by: Grégoire Mougeolle <gregoire@nextmap.io>

--- theme/js/index.js
+++ theme/js/index.js
@@ -7,9 +7,13 @@ import Clipboard from "v-clipboard";
 import VModal from "vue-js-modal";
 
 import { showModal } from "./components/vue/modals";
+import Api from "./plugins/api";
+import Auth from "./plugins/auth";
 
 Vue.use(Clipboard);
 Vue.use(VModal);
+Vue.use(Api);
+Vue.use(Auth);
 
 new Vue({
   el: "#app",
