733d5ef92c3ecca561b98d05c41b24f52fb0a5fe
Stanislas P <57526019+quaxsze@users.noreply.github.com>
2020-11-23 14:28:02 +0100

    Prevent unpublished posts to be retrieved (#2578)

--- udata/core/post/api.py
+++ udata/core/post/api.py
@@ -72,7 +72,7 @@ class PostsAPI(API):
     def get(self):
         '''List all posts'''
         args = parser.parse_args()
-        return (Post.objects.order_by(args['sort'])
+        return (Post.objects.published().order_by(args['sort'])
                             .paginate(args['page'], args['page_size']))
 
     @api.doc('create_post')
