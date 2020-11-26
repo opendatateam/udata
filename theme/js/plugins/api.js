6ec46639de8fc393cf4161c69755fdbc2279343c
Grégoire Mougeolle <gregoire@nextmap.io>
2020-11-26 17:31:46 +0100

    (api) Updated todo : no need for CSRF
    Signed-off-by: Grégoire Mougeolle <gregoire@nextmap.io>

--- theme/js/plugins/api.js
+++ theme/js/plugins/api.js
@@ -2,11 +2,11 @@ import axios from "axios";
 import config from "../config";
 
 //Instanciate axios with base URL from config
+//No need for CSRF or anything fancy here
+//TODO : maybe add interceptor to better handle errors ?
 const api = axios.create({
   baseURL: config.api_root,
 });
-
-//TODO, probably : add interceptors to add CSRF info and credentials
 
 export const install = (Vue) => {
   Vue.prototype.$api = Vue.api = api;
