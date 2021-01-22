ad25ca087fc5d4c08b09c084196e8494b55dc138
Grégoire Mougeolle <gregoire@nextmap.io>
2021-01-22 16:06:15 +0100

    (i18n) Removed fr lang, will be used as fallback
    Signed-off-by: Grégoire Mougeolle <gregoire@nextmap.io>

--- theme/js/plugins/i18n.js
+++ theme/js/plugins/i18n.js
@@ -5,13 +5,12 @@ import config from "../config";
 import { createI18n } from "vue-i18n";
 
 //TODO : use a glob when supported in Parcel 2 : https://github.com/parcel-bundler/parcel/issues/4683
-import fr from "../locales/fr.json";
 import en from "../locales/en.json";
 
 export default i18n = createI18n({
   locale: config.lang,
+  silentTranslationWarn: true,
   messages: {
     en,
-    fr
-  }
+  },
 });
