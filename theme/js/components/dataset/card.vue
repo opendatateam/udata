cab3f2868ad720681e5dbe08f9aa77998e78a425
Grégoire Mougeolle <gregoire@nextmap.io>
2021-01-28 12:39:01 +0100

    (js) Dataset card component
    Signed-off-by: Grégoire Mougeolle <gregoire@nextmap.io>

---
+++ theme/js/components/dataset/card.vue
@@ -0,0 +1,32 @@
+<template>
+  <article class="dataset-card">
+    <div class="card-logo">
+      <Placeholder type="reuse" :src="image_url" :alt="title" />
+    </div>
+    <div class="card-data">
+      <h4 class="card-body">
+        {{ title }}
+      </h4>
+      <div class="card-footer">
+        <ul class="card-features">
+          <li>252 ressources</li>
+          <li>2 réutilisations</li>
+        </ul>
+      </div>
+    </div>
+  </article>
+</template>
+
+<script>
+import Placeholder from "../utils/placeholder";
+
+export default {
+    props: {
+        title: String,
+        image_url: String,
+    },
+    components: {
+        Placeholder
+    }
+};
+</script>
