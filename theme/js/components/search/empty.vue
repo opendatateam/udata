2c86e05e55287c4e2aa3a09fb3dc2d8a700acd10
Grégoire Mougeolle <gregoire@nextmap.io>
2021-03-16 17:49:41 +0100

    (search) Empty state better empty state®
    Signed-off-by: Grégoire Mougeolle <gregoire@nextmap.io>

--- theme/js/components/search/empty.vue
+++ theme/js/components/search/empty.vue
@@ -11,7 +11,7 @@ Nothing to see here ! Well actually it's a small component that says this.
       </h4>
       <p>
         {{ copy }}
-        "{{ queryString }}"
+        {{ queryString && ('"' + queryString + '"') }}
       </p>
       <p v-if="copyAfter">{{ copyAfter }}</p>
       <a class="nav-link mt-md" @click="onClick" v-if="onClick">{{ cta }}</a>
