8b2a06bda60585242a0e8b3e425f0054f9eaadd3
Grégoire Mougeolle <gregoire@nextmap.io>
2021-03-17 16:05:32 +0100

    (datepicker) Clear button for date picker
    Signed-off-by: Grégoire Mougeolle <gregoire@nextmap.io>

--- theme/js/components/search/rangepicker.vue
+++ theme/js/components/search/rangepicker.vue
@@ -19,25 +19,41 @@ d1.yyyy-d1.mm-d1.dd-d2.yyyy-d2.mm-d2.dd
   <div class="rangepicker row-inline w-100 justify-between">
     <div class="row-inline align-items-center">
       <strong class="text-grey-300 f-normal mr-xxs">{{ $t("@@du") }}</strong>
-      <Datepicker
-        v-model="dateRange.start"
-        :upperLimit="dateRange.end"
-        @update:modelValue="_onChange"
-        placeholder="__/__/__"
-        :locale="locale"
-      />
+      <div class="datepicker">
+        <Datepicker
+          v-model="dateRange.start"
+          :upperLimit="dateRange.end"
+          @update:modelValue="_onChange"
+          placeholder="__/__/__"
+          :locale="locale"
+        />
+        <span
+          class="clear"
+          @click="clear"
+          v-html="closeIcon"
+          v-if="dateRange.start"
+        />
+      </div>
     </div>
     <div class="row-inline align-items-center">
       <strong class="text-grey-300 f-normal mr-xxs ml-xs">{{
         $t("@@au")
       }}</strong>
-      <Datepicker
-        v-model="dateRange.end"
-        :lowerLimit="dateRange.start"
-        @update:modelValue="_onChange"
-        placeholder="__/__/__"
-        :locale="locale"
-      />
+      <div class="datepicker">
+        <Datepicker
+          v-model="dateRange.end"
+          :lowerLimit="dateRange.start"
+          @update:modelValue="_onChange"
+          placeholder="__/__/__"
+          :locale="locale"
+        />
+        <span
+          class="clear"
+          @click="clear"
+          v-html="closeIcon"
+          v-if="dateRange.end"
+        />
+      </div>
     </div>
   </div>
 </template>
@@ -45,7 +61,8 @@ d1.yyyy-d1.mm-d1.dd-d2.yyyy-d2.mm-d2.dd
 <script>
 import config from "../../config";
 import Datepicker from "vue3-datepicker";
-import 'vue3-datepicker/dist/vue3-datepicker.css';
+import "vue3-datepicker/dist/vue3-datepicker.css";
+import closeIcon from "svg/close.svg";
 import { format } from "date-fns";
 import fr from "date-fns/locale/fr";
 import en from "date-fns/locale/en-GB";
@@ -58,6 +75,7 @@ export default {
     Datepicker,
   },
   created() {
+    this.closeIcon = closeIcon;
     this.locale = locales[config.lang];
 
     if (!this.value) return;
@@ -96,6 +114,10 @@ export default {
           "-" +
           format(this.dateRange.end, "yyyy-MM-dd")
       );
+    },
+    clear: function () {
+      this.dateRange.start = null;
+      this.dateRange.end = null;
     },
   },
 };
