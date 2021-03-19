2d773b2afa868ee44b9a993cf6402b10eb3c8040
Geoffrey Aldebert <geoffrey.aldebert@data.gouv.fr>
2021-03-19 16:24:12 +0100

    Allow possibility to add version to schema metadata

--- udata/core/dataset/forms.py
+++ udata/core/dataset/forms.py
@@ -34,7 +34,7 @@ def normalize_format(data):
 def enforce_allowed_schemas(form, field):
     schema = field.data
     allowed_schemas = [s['id'] for s in ResourceSchema.objects()]
-    if schema.startswith(tuple(allowed_schemas)):
+    if not schema.startswith(tuple(allowed_schemas)):
         message = _('Schema "{schema}" is not an allowed value. Allowed values: {values}')
         raise validators.ValidationError(message.format(
             schema=schema,
