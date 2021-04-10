a91b6650b142d5bba010787ea222879248d4d69d
Geoffrey Aldebert <geoffrey.aldebert@data.gouv.fr>
2021-04-10 19:55:55 +0200

    Add migration to adapt schema field to new structure

---
+++ udata/migrations/2021-04-08-update-schema-with-new-structure.py
@@ -0,0 +1,32 @@
+'''
+The purpose here is to fill every resource with a fs_filename string field.
+'''
+import logging
+
+from udata.models import Dataset
+
+log = logging.getLogger(__name__)
+
+
+def migrate(db):
+    log.info('Processing resources.')
+
+    datasets = Dataset.objects().no_cache().timeout(False)
+    for dataset in datasets:
+        save_res = False
+        for resource in dataset.resources:
+            if hasattr(resource, 'schema'):
+                schema = resource.schema
+                resource.schema = { "name": None }
+                if schema is not None: 
+                    if isinstance(schema, str):
+                        resource.schema = { "name": schema }   
+                save_res = True
+        if save_res:
+            try:
+                dataset.save()
+            except Exception as e:
+                log.warning(e)
+                pass
+
+    log.info('Completed.')
