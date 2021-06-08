adcdd8c67ae0a3ca2d08d5e532b1cbde582caec2
quaxsze <service@plum.eu.com>
2021-06-08 16:09:00 +0200

    fix merge

--- udata/__init__.py
+++ udata/__init__.py
@@ -4,10 +4,5 @@
 udata
 '''
 
-<<<<<<< HEAD
 __version__ = '3.0.0.dev'
 __description__ = 'Open data portal'
-=======
-__version__ = '2.7.2.dev'
-__description__ = 'Open data portal'
->>>>>>> master
