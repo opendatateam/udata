8446be47e19d60ae95a98de7c2bbabf03ef6f90f
Estelle Maudet <estelle.maudet@data.gouv.fr>
2021-06-25 16:54:48 +0200

    Import theme translations

--- udata/i18n.py
+++ udata/i18n.py
@@ -77,15 +77,18 @@ class PluggableDomain(Domain):
                                                              domain=domain))
 
                 # Allows the theme to provide or override translations
-                # from . import theme
-
-                # theme_translations_dir = join(theme.current.path, 'translations')
-                # if exists(theme_translations_dir):
-                #     domain = theme.current.identifier
-                #     theme_translations = Translations.load(theme_translations_dir,
-                #                                            locale,
-                #                                            domain=domain)
-                #     translations.merge(theme_translations)
+                try:
+                    from udata_gouvfr import theme
+                    theme_translations_dir = join(theme.current.path, 'translations')
+                    if exists(theme_translations_dir):
+                        domain = theme.current.identifier
+                        theme_translations = Translations.load(theme_translations_dir,
+                                                               locale,
+                                                               domain=domain)
+                        translations.merge(theme_translations)
+                except ImportError:
+                    # No theme loaded
+                    pass
 
                 cache[str(locale)] = translations
 
