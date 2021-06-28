54800f7e66974fadbd7d7160538a622baed71068
Grégoire Mougeolle <gregoire@nextmap.io>
2021-06-28 18:11:47 +0200

    (js) Clipboard now uses clipboard.js vanilla
    Fix etalab/data.gouv.fr#254
    
    Signed-off-by: Grégoire Mougeolle <gregoire@nextmap.io>

---
+++ theme/js/components/vanilla/clipboard.js
@@ -0,0 +1,25 @@
+/*
+---
+name: Clipboard
+category: Interactions
+---
+
+# Interaction
+Copy to clipboard thanks to clipboardjs.com
+Just have an element with an `id` ending with `-copy` and use clipboardJS API for endless possibilities !
+
+```clipboard.html
+<input id="something-copy" value="Some value" />
+<button class="btn" data-clipboard-target="#something-copy">
+    Copy !
+</button>
+```
+*/
+
+import ClipboardJS from "clipboard";
+
+export default (() => {
+  document.addEventListener("DOMContentLoaded", () => {
+    new ClipboardJS('[id$="-copy"]');
+  });
+})();
