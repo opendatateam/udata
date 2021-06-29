dc05ab02a862658fc227bd879f058172e1166f9f
maudetes <maudet.estelle@gmail.com>
2021-06-29 09:37:55 +0200

    Fix translations errors (content in French, newlines) (#551)
    * Fix translations errors (content in French, newlines)
    
    * Load fr locale in JS

--- theme/js/plugins/i18n.js
+++ theme/js/plugins/i18n.js
@@ -6,6 +6,7 @@ import { createI18n } from "vue-i18n";
 
 //TODO : use a glob when supported in Parcel 2 : https://github.com/parcel-bundler/parcel/issues/4683
 import en from "../locales/en.json";
+import fr from "../locales/fr.json"
 
 const missing = (locale, key) => {
   if (key.startsWith("@@")) return key.slice(2);
@@ -23,6 +24,7 @@ export default i18n = createI18n({
   silentTranslationWarn: true,
   missing,
   messages: {
-    en
+    en,
+    fr
   },
 });
