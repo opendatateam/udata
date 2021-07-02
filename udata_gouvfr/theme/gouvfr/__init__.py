d81d92da58911e00a1848a3fe4b627bf49b4a628
maudetes <maudet.estelle@gmail.com>
2021-07-02 14:16:03 +0200

    Add accessibility and etalab licence link (#566)

--- udata_gouvfr/theme/gouvfr/__init__.py
+++ udata_gouvfr/theme/gouvfr/__init__.py
@@ -86,6 +86,7 @@ footer_links = [
     nav.Item(_('Licences'), 'gouvfr.show_page', args={'slug': 'legal/licences'}),
     nav.Item(_('Terms of use'), 'site.terms'),
     nav.Item(_('Tracking and privacy'), 'gouvfr.suivi'),
+    nav.Item(_('Accessibility'), 'gouvfr.show_page', args={'slug': 'legal/accessibility'}),
 ]
 
 nav.Bar('gouvfr_footer', footer_links)
