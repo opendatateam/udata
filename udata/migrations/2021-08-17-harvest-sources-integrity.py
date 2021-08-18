68e3217697f28828c82113d2cd001b750e238045
Alexandre Bulté <alexandre@bulte.net>
2021-08-18 14:08:36 +0200

    review fix

--- udata/migrations/2021-08-17-harvest-sources-integrity.py
+++ udata/migrations/2021-08-17-harvest-sources-integrity.py
@@ -12,7 +12,8 @@ log = logging.getLogger(__name__)
 def migrate(db):
     log.info('Processing PeriodicTask objects.')
 
-    harvest_schedules = PeriodicTask.objects.filter(name__startswith='Harvest')
+    harvest_schedules = PeriodicTask.objects.filter(name__startswith='Harvest',
+                                                    description='Periodic Harvesting')
     hs = HarvestSource.objects.filter(periodic_task__in=harvest_schedules)
     unlegit_schedules = harvest_schedules.filter(id__nin=[h.periodic_task.id for h in hs])
     count = unlegit_schedules.delete()
