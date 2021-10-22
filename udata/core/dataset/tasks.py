bf60accd1b9a9ed9dc12291e8947a5b73c7c5f8a
Stanislas P <57526019+quaxsze@users.noreply.github.com>
2021-10-22 17:35:59 +0200

    remove useless print

--- udata/core/dataset/tasks.py
+++ udata/core/dataset/tasks.py
@@ -176,7 +176,6 @@ def export_csv_for_model(model, dataset):
     try:
         # write adapter results into a tmp file
         writer = csv.get_writer(csvfile)
-        print(adapter.header())
         writer.writerow(adapter.header())
         for row in adapter.rows():
             writer.writerow(row)
