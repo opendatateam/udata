51e972d9d0dd9e737d8cdad1297fd5f67cd6dc83
Stanislas P <57526019+quaxsze@users.noreply.github.com>
2021-11-04 18:49:43 +0100

    more realistic fixtures

--- udata/commands/fixtures.py
+++ udata/commands/fixtures.py
@@ -1,12 +1,17 @@
 import logging
+import yaml
+import sys
 
 import click
+from flask import current_app
 
 from udata.commands import cli
-from udata.core.dataset.factories import VisibleDatasetFactory
+from udata.core.dataset.commands import frequency_reminder
+from udata.core.dataset.factories import DatasetFactory, ResourceFactory
 from udata.core.discussions.factories import DiscussionFactory
+from udata.core.discussions.models import Message, Discussion
 from udata.core.organization.factories import OrganizationFactory
-from udata.core.organization.models import Member
+from udata.core.organization.models import Member, Organization
 from udata.core.reuse.factories import ReuseFactory
 from udata.core.user.factories import UserFactory
 
@@ -14,22 +19,34 @@ log = logging.getLogger(__name__)
 
 
 @cli.command()
-@click.option('-d', '--datasets', type=int, default=5,
-              help='Number of datasets to generate')
-@click.option('-r', '--reuses', type=int, default=1,
-              help='Number of reuses by dataset')
-def generate_fixtures(datasets, reuses):
+def generate_fixtures():
     '''Build sample fixture data (users, datasets and reuses).'''
-    user = UserFactory()
-    log.info('Generated user "{user.email}".'.format(user=user))
+    try:
+        fictures_path = current_app.config['FIXTURES_ROOT']
+    except KeyError:
+        raise click.ClickException('FIXTURES_ROOT setting not set.')
+    try:
+        stream = open(fictures_path, 'r')
+        dictionary = yaml.load(stream, Loader=yaml.FullLoader)
+    except FileNotFoundError:
+        raise click.FileError(fictures_path, 'File not found.')
 
-    organization = OrganizationFactory(members=[Member(user=user)])
-    log.info('Generated organization "{org.name}".'.format(org=organization))
-
-    for _ in range(datasets):
-        dataset = VisibleDatasetFactory(organization=organization)
-        DiscussionFactory(subject=dataset, user=user)
-        ReuseFactory.create_batch(reuses, datasets=[dataset], owner=user)
-
-    msg = 'Generated {datasets} dataset(s) with {reuses} reuse(s) each.'
-    log.info(msg.format(**locals()))
+    for fixture in dictionary:
+        user = UserFactory()
+        org = OrganizationFactory(**fixture['organization'], members=[Member(user=user)])
+        dataset = DatasetFactory(**fixture['dataset'], organization=org)
+        for resource in fixture['resources']:
+            res = ResourceFactory(**resource)
+            dataset.add_resource(res)
+            # for discussion in data['discussions']:
+            #     user = UserFactory()
+            #     messages = []
+            #     for message in discussion['messages']:
+            #         message = Message(content=message['content'], posted_by=user)
+            #     discussion = DiscussionFactory(
+            #         subject=dataset,
+            #         user=user,
+            #         title=discussion['title'],
+            #         discussion=messages)
+    # log.info('Generated organization "{org.name}".'.format(org=organization))
+        # ReuseFactory.create_batch(reuses, datasets=[dataset], owner=user)
