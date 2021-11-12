e2e1b09db66ae4a33e7a5dd4ba2d7d722774c2a1
Stanislas P <57526019+quaxsze@users.noreply.github.com>
2021-11-12 16:22:11 +0100

    rollback user search

---
+++ udata/core/user/search.py
@@ -0,0 +1,92 @@
+from elasticsearch_dsl import Completion, Date, String, Boolean
+
+from udata.i18n import lazy_gettext as _
+from udata.models import User, Organization
+from udata.search import ModelSearchAdapter
+from udata.search import i18n_analyzer, register
+from udata.search.fields import ModelTermsFacet, RangeFacet
+from udata.search.fields import GaussDecay
+from udata.search.analysis import simple
+from udata.utils import to_iso_datetime
+
+
+__all__ = ('UserSearch', )
+
+
+@register
+class UserSearch(ModelSearchAdapter):
+    model = User
+    fuzzy = True
+
+    class Meta:
+        doc_type = 'User'
+
+    first_name = String()
+    last_name = String()
+    about = String(analyzer=i18n_analyzer)
+    organizations = String(index='not_analyzed')
+    visible = Boolean()
+    metrics = User.__search_metrics__
+    created = Date(format='date_hour_minute_second')
+    user_suggest = Completion(analyzer=simple,
+                              search_analyzer=simple,
+                              payloads=True)
+
+    sorts = {
+        'last_name': 'last_name',
+        'first_name': 'first_name',
+        'datasets': 'metrics.datasets',
+        'reuses': 'metrics.reuses',
+        'followers': 'metrics.followers',
+        'views': 'metrics.views',
+        'created': 'created',
+    }
+    facets = {
+        'organization': ModelTermsFacet(field='organizations',
+                                        model=Organization),
+        'datasets': RangeFacet(field='metrics.datasets',
+                               ranges=[('none', (None, 1)),
+                                       ('few', (1, 5)),
+                                       ('many', (5, None))],
+                               labels={
+                                    'none': _('No datasets'),
+                                    'few': _('Few datasets'),
+                                    'many': _('Many datasets'),
+                               }),
+        'followers': RangeFacet(field='metrics.followers',
+                                ranges=[('none', (None, 1)),
+                                        ('few', (1, 5)),
+                                        ('many', (5, None))],
+                                labels={
+                                    'none': _('No followers'),
+                                    'few': _('Few followers'),
+                                    'many': _('Many followers'),
+                                }),
+    }
+    boosters = [
+        GaussDecay('metrics.reuses', 50, decay=0.8),
+        GaussDecay('metrics.datasets', 50, decay=0.8),
+        GaussDecay('metrics.followers', 200, 200, decay=0.8),
+    ]
+
+    @classmethod
+    def serialize(cls, user):
+        return {
+            'first_name': user.first_name,
+            'last_name': user.last_name,
+            'about': user.about,
+            'organizations': [str(o.id) for o in user.organizations],
+            'metrics': user.metrics,
+            'created': to_iso_datetime(user.created_at),
+            'user_suggest': {
+                'input': cls.completer_tokenize(user.fullname) + [user.id],
+                'output': str(user.id),
+                'payload': {
+                    'avatar_url': user.avatar(40, external=True),
+                    'first_name': user.first_name,
+                    'last_name': user.last_name,
+                    'slug': user.slug,
+                },
+            },
+            'visible': user.visible,
+        }
