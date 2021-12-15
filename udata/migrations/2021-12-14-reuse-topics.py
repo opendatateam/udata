039b615b84cb56633fdcfa09dcc698076c7d7e47
maudetes <maudet.estelle@gmail.com>
2021-12-15 12:11:57 +0100

    Add reuse migrations

---
+++ udata/migrations/2021-12-14-reuse-topics.py
@@ -0,0 +1,21 @@
+'''
+Add a default topic to all reuses in db
+'''
+import logging
+
+from udata.models import Reuse
+
+log = logging.getLogger(__name__)
+
+
+def migrate(db):
+    log.info('Processing Reuse.')
+
+    reuses = Reuse.objects().no_cache().timeout(False)
+    count = 0
+    for reuse in reuses:
+        reuse.topic = 'others'
+        reuse.save()
+
+    log.info(f'Modified {count} Reuses objects')
+    log.info('Done')
