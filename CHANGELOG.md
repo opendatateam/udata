f6f346ffe949695bc0ea77d0040877fa28dcf288
Nicolas KEMPF <nicolas@conciergerie.dev>
2022-01-26 16:35:46 +0100

    Don't wrap thread header above md screens (#64)
    * fix: don't wrap thread header above md screens

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -5,6 +5,7 @@
 - Fix modals not working [#71](https://github.com/etalab/udata-front/pull/71)
 - Fix auth messages not shown from query parameter [#68](https://github.com/etalab/udata-front/pull/68)
 - Fix RGAA criterion 10.14 [#72](https://github.com/etalab/udata-front/pull/72)
+- Fix thread header wrapped when title is too long [#64](https://github.com/etalab/udata-front/pull/64)
 
 ## 1.2.2 (2022-01-21)
 
--- theme/js/components/discussions/thread.vue
+++ theme/js/components/discussions/thread.vue
@@ -1,10 +1,10 @@
 <template>
   <div class="bg-contrast-grey fr-mt-2w" :id="discussionUrl(id)">
-    <header class="fr-grid-row fr-grid-row--middle justify-between fr-py-2w fr-px-3w">
+    <header class="fr-grid-row fr-grid-row--middle justify-between fr-py-2w fr-px-3w no-wrap wrap-md">
       <div class="fr-col-auto text-default-warning fr-text--bold fr-pr-2w" v-if="closed">
         <span>{{ $t("Discussion closed") }}</span>
       </div>
-      <h3 class="fr-p-2w fr-p-md-0 fr-h6 fr-mb-0">{{ title }}</h3>
+      <h3 class="fr-mx-3v fr-mx-md-0 fr-h6 fr-mb-0">{{ title }}</h3>
       <div class="text-align-right">
         <a
           :id="id + '-copy'"
