e903722edccb1d446e1180582eb1d1fec29c80d7
nikolakovacevic4github <nikolakovacevic@gmail.com>
2022-03-05 13:15:59 +0100

    Add Serbian latin language to default settings

--- udata/settings.py
+++ udata/settings.py
@@ -19,6 +19,7 @@ class Defaults(object):
         'es': 'Español',
         'pt': 'Português',
         'sr': 'Српски',
+        'srl': 'Srpski',
     }
     DEFAULT_LANGUAGE = 'en'
     SECRET_KEY = 'Default uData secret key'
