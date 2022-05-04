13331cf34639b7150d58fba94717b32309a04324
Stanislas P <57526019+quaxsze@users.noreply.github.com>
2022-05-04 16:03:18 +0200

    Feat/add html support for posts (#2731)

--- udata/core/post/forms.py
+++ udata/core/post/forms.py
@@ -24,3 +24,7 @@ class PostForm(ModelForm):
     credit_url = fields.URLField(_('Credit URL'))
 
     tags = fields.TagField(_('Tags'))
+
+    body_type = fields.StringField(
+        _('body type'),
+        description=_('Specify your body type (HTML or markdown)'))
