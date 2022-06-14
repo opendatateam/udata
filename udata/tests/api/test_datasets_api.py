2b79a02ba1f5a9e03c243512ffcd602b89e8973d
quaxsze <plum.stanislas@gmail.com>
2022-06-14 17:00:45 +0200

    fix tests

--- udata/tests/api/test_datasets_api.py
+++ udata/tests/api/test_datasets_api.py
@@ -49,7 +49,6 @@ class DatasetAPITest(APITestCase):
         self.assert200(response)
         self.assertEqual(len(response.json['data']), len(datasets))
         self.assertFalse('quality' in response.json['data'][0])
-        assert False
 
     def test_dataset_api_full_text_search(self):
         '''Should proceed to full text search on datasets'''
