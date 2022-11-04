4581dec1c4a092fe6edb19dbce27949e8567faa4
maudetes <maudet.estelle@gmail.com>
2022-11-04 18:42:55 +0100

    Clean additionnal harvest extras in migration

--- udata/migrations/2022-10-10-migrate-harvest-extras.py
+++ udata/migrations/2022-10-10-migrate-harvest-extras.py
@@ -1,6 +1,6 @@
 '''
 Move identifying harvest fields from extras to dedicated harvest metadata fields.
-Ignore other extras set at harvest time, they will be re-created if needed.
+Clean other extras set at harvest time, they will be re-created if needed.
 '''
 import logging
 
@@ -25,9 +25,23 @@ def migrate(db):
     for dataset in datasets:
         if not dataset.harvest:
             dataset.harvest = HarvestDatasetMetadata()
-        dataset.harvest.remote_id = dataset.extras.get('harvest:remote_id')
-        dataset.harvest.source_id = dataset.extras.get('harvest:source_id')
-        dataset.harvest.domain = dataset.extras.get('harvest:domain')
+        dataset.harvest.remote_id = dataset.extras.pop('harvest:remote_id')
+        dataset.harvest.source_id = dataset.extras.pop('harvest:source_id')
+        dataset.harvest.domain = dataset.extras.pop('harvest:domain')
+        # Keep any archived reason and date
+        dataset.harvest.archived = dataset.extras.pop('harvest:archived')
+        dataset.harvest.archived_at = dataset.extras.pop('harvest:archived_at')
+
+        more_harvest_extras = ['remote_url', 'dct:identifier', 'uri']
+        for key in dataset.extras:
+            if key.startwith('harvest:') or key.startwith('ods:') or key in more_harvest_extras:
+                dataset.extras.pop(key)
+
+        for resource in dataset.resources:
+            for key in dataset.resource.extras:
+                if key.startwith('harvest:') or key.startwith('ods:'):
+                    dataset.extras.pop(key)
+
         try:
             dataset.save()
         except ValidationError as e:
