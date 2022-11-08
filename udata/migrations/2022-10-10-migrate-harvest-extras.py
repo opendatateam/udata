47888095ea8815523badb732ce226e1d77b48b36
maudetes <maudet.estelle@gmail.com>
2022-11-08 12:13:27 +0100

    Fix harvest migration

--- udata/migrations/2022-10-10-migrate-harvest-extras.py
+++ udata/migrations/2022-10-10-migrate-harvest-extras.py
@@ -11,6 +11,13 @@ from udata.core.dataset.models import HarvestDatasetMetadata
 log = logging.getLogger(__name__)
 
 
+def should_keep(extra):
+    extras_to_remove = ['remote_url', 'dct:identifier', 'uri', 'ckan:name', 'ckan:source']
+    if extra.startswith('harvest:') or extra.startswith('ods:') or extra in extras_to_remove:
+        return False
+    return True
+
+
 def migrate(db):
     log.info('Processing Datasets.')
 
@@ -22,30 +29,29 @@ def migrate(db):
             ],
         }).no_cache().timeout(False)
 
+    datasets_count = datasets.count()
     for dataset in datasets:
         if not dataset.harvest:
             dataset.harvest = HarvestDatasetMetadata()
-        dataset.harvest.remote_id = dataset.extras.pop('harvest:remote_id')
-        dataset.harvest.source_id = dataset.extras.pop('harvest:source_id')
-        dataset.harvest.domain = dataset.extras.pop('harvest:domain')
+        dataset.harvest.remote_id = dataset.extras.pop('harvest:remote_id', None)
+        dataset.harvest.source_id = dataset.extras.pop('harvest:source_id', None)
+        dataset.harvest.domain = dataset.extras.pop('harvest:domain', None)
+        # Keep last_update for archiving reason
+        dataset.harvest.last_update = dataset.extras.pop('harvest:last_update', None)
         # Keep any archived reason and date
-        dataset.harvest.archived = dataset.extras.pop('harvest:archived')
-        dataset.harvest.archived_at = dataset.extras.pop('harvest:archived_at')
+        dataset.harvest.archived = dataset.extras.pop('harvest:archived', None)
+        dataset.harvest.archived_at = dataset.extras.pop('harvest:archived_at', None)
 
-        more_harvest_extras = ['remote_url', 'dct:identifier', 'uri']
-        for key in dataset.extras:
-            if key.startwith('harvest:') or key.startwith('ods:') or key in more_harvest_extras:
-                dataset.extras.pop(key)
-
+        dataset.extras = {extra: dataset.extras[extra] for extra in dataset.extras
+                          if should_keep(extra)}
         for resource in dataset.resources:
-            for key in dataset.resource.extras:
-                if key.startwith('harvest:') or key.startwith('ods:'):
-                    dataset.extras.pop(key)
+            resource.extras = {extra: resource.extras[extra] for extra in resource.extras
+                               if should_keep(extra)}
 
         try:
             dataset.save()
         except ValidationError as e:
             log.error(f'Failed to save dataset {dataset.id}: {e}')
 
-    log.info(f'Modified {datasets.count()} datasets objects')
+    log.info(f'Modified {datasets_count} datasets objects')
     log.info('Done')
