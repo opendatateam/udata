cb5ac70c1bf5b74e5c943efc33c2c790d0311ef2
quaxsze <plum.stanislas@gmail.com>
2023-01-26 15:19:05 +0100

    fix isinstance and tzinfo on field

--- udata/utils.py
+++ udata/utils.py
@@ -140,7 +140,7 @@ def daterange_end(value):
 
 
 def to_naive_datetime(given_date):
-    if isinstance(given_date, date) and not isinstance(value, datetime.datetime):
+    if isinstance(given_date, date) and not isinstance(given_date, datetime):
         return datetime(
             given_date.year,
             given_date.month,
