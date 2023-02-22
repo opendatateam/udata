66d43c6681b1c0ae9ae8c54fbf852de96c47c72e
quaxsze <plum.stanislas@gmail.com>
2023-02-22 18:06:35 +0100

    fix migration

--- udata/migrations/2023-02-08-rename-internal-dates.py
+++ udata/migrations/2023-02-08-rename-internal-dates.py
@@ -2,6 +2,7 @@
 The purpose here is to change the dates name in the dataset and resource model.
 '''
 import logging
+import pymongo
 
 from mongoengine.connection import get_db
 from udata.models import Dataset
@@ -26,13 +27,16 @@ def migrate(db):
     log.info(f'{result.modified_count} Community Resources processed.')
 
     for dataset in dataset_collection.find():
-        resources = dataset['resources']
+        log.info(dataset['_id'])
+        resources = dataset.get('resources', [])
         for resource in resources:
-            resource['created_at_internal'] = resource.pop('created_at')
-            resource['last_modified_internal'] = resource.pop('modified')
             log.info('Processing resource %s', resource['_id'])
-            if resource.get('harvest', None) and resource['harvest']['modified_at']:
-                resource['harvest']['last_modified'] = resource.harvest.pop('modified_at')
+            resource['created_at_internal'] = resource.pop('created_at')
+            if resource.get('modified', None):
+                resource['last_modified_internal'] = resource.pop('modified')
+            if resource.get('harvest', None):
+                if resource['harvest'].get('modified_at', None):
+                    resource['harvest']['last_modified'] = resource['harvest'].pop('modified_at')
         dataset_collection.update_one({'_id': dataset['_id']}, {'$set': {'resources': resources}})
 
     log.info('Completed.')
