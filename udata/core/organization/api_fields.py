8d03d5d610b9c0cad143090013153e98138b4adb
quaxsze <plum.stanislas@gmail.com>
2023-07-18 17:58:40 +0200

    fix

--- udata/core/organization/api_fields.py
+++ udata/core/organization/api_fields.py
@@ -61,7 +61,7 @@ org_fields = api.model('Organization', {
         required=True),
     'description': fields.Markdown(
         description='The organization description in Markdown', required=True),
-    'siret': fields.String(description='The organization\'s business identification number.'),
+    'establishment_number_id': fields.String(description='The organization\'s establishment identification number.'),
     'created_at': fields.ISODateTime(
         description='The organization creation date', readonly=True),
     'last_modified': fields.ISODateTime(
--- udata/core/organization/models.py
+++ udata/core/organization/models.py
@@ -107,7 +107,7 @@ class Organization(WithMetrics, BadgeMixin, db.Datetimed, db.Document):
     image_url = db.StringField()
     logo = db.ImageField(fs=avatars, basename=default_image_basename,
                          max_size=LOGO_MAX_SIZE, thumbnails=LOGO_SIZES)
-    siret = db.StringField(max_length=14)
+    establishment_number_id = db.StringField(max_length=128)
 
     members = db.ListField(db.EmbeddedDocumentField(Member))
     teams = db.ListField(db.EmbeddedDocumentField(Team))
