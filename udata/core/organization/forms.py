7d02cf4c6067319ec29c9c5e2f3fcbea744717f3
quaxsze <plum.stanislas@gmail.com>
2023-07-18 15:21:20 +0200

    enhancement

--- udata/core/organization/forms.py
+++ udata/core/organization/forms.py
@@ -17,10 +17,7 @@ __all__ = (
 
 def siret_check(form, field):
     numero_siren = str(field.data)
-    if len(numero_siren) < 9:
-        raise validators.ValidationError(_('Invalid Siret number'))
 
-    # Calcul de la clé de contrôle
     chiffres = [int(chiffre) for chiffre in numero_siren]
     chiffres[1::2] = [chiffre * 2 for chiffre in chiffres[1::2]]
     chiffres = [chiffre - 9 if chiffre > 9 else chiffre for chiffre in chiffres]
@@ -42,7 +39,7 @@ class OrganizationForm(ModelForm):
     url = fields.URLField(
         _('Website'), description=_('The organization website URL'))
     logo = fields.ImageField(_('Logo'), sizes=LOGO_SIZES)
-    siret = fields.StringField(_('Siret'), [validators.Length(max=14), siret_check])
+    siret = fields.StringField(_('Siret'), [validators.Length(min=9, max=14), siret_check])
 
     deleted = fields.DateTimeField()
     extras = fields.ExtrasField()
