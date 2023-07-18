fbaa5df90c9c13c5b61f59b3c7ec32fbae9ec1cb
maudetes <maudet.estelle@gmail.com>
2023-07-18 17:12:18 +0200

    Quality score is now a decimal

--- js/components/datatable/cells/progress-bars.vue
+++ js/components/datatable/cells/progress-bars.vue
@@ -1,8 +1,8 @@
 <template>
 <div class="progress progress-sm">
     <span class="progress-bar progress-bar-{{ progress_class }}"
-        :style="{width: value + 1 + '0%'}"
-        :title="_('Score:') + ' ' + value">
+        :style="{width: value * 100 + '%'}"
+        :title="_('Score:') + ' ' + value * 100">
     </span>
 </div>
 </template>
@@ -12,13 +12,13 @@ export default {
     name: 'datatable-cell-progress-bars',
     computed: {
         progress_class: function() {
-            if (this.value < 2) {
+            if (this.value < 1/3) {
                 return 'danger';
             }
-            else if (this.value < 5) {
+            else if (this.value < 2/3) {
                 return 'warning';
             }
-            else if (this.value < 9) {
+            else if (this.value < 0.9) {
                 return 'primary';
             }
             else {
