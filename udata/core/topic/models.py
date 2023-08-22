bbac4ac6c9612b9fb2eac3c515b2c799b7ec62d4
quaxsze <plum.stanislas@gmail.com>
2023-08-22 03:45:29 +0200

    Add index in topic pre save

--- udata/core/topic/models.py
+++ udata/core/topic/models.py
@@ -1,6 +1,9 @@
 from flask import url_for
 
+from mongoengine.signals import pre_save
 from udata.models import db
+from udata.search import reindex
+from udata.tasks import as_task_param
 
 
 __all__ = ('Topic', )
@@ -28,6 +31,25 @@ class Topic(db.Document):
     def __str__(self):
         return self.name
 
+    @classmethod
+    def pre_save(cls, sender, document, **kwargs):
+        try:
+            original_doc = sender.objects.get(id=document.id)
+            datasets_list_dif = list(set(original_doc.datasets).symmetric_difference(set(document.datasets)))
+            reuses_list_dif = list(set(original_doc.reuses).symmetric_difference(set(document.reuses)))
+        except cls.DoesNotExist:
+            datasets_list_dif = document.datasets
+            reuses_list_dif = document.reuses
+        if datasets_list_dif:
+            for dataset in datasets_list_dif:
+                reindex.delay(*as_task_param(dataset))
+        if reuses_list_dif:
+            for reuse in reuses_list_dif:
+                reindex.delay(*as_task_param(reuse))
+
     @property
     def display_url(self):
         return url_for('topics.display', topic=self)
+
+
+pre_save.connect(Topic.pre_save, sender=Topic)
