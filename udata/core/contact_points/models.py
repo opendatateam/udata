5831a919185fba91251521e93fc80f9e4b94da72
quaxsze <plum.stanislas@gmail.com>
2023-10-19 15:26:43 +0200

    fix model

--- udata/core/contact_points/models.py
+++ udata/core/contact_points/models.py
@@ -1,6 +1,9 @@
 from udata.models import db
 
 
+__all__ = ('ContactPoint', )
+
+
 class ContactPoint(db.Document):
     email = db.StringField(max_length=255, required=True)
     name = db.StringField(max_length=255, required=True)
