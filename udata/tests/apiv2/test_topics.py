ebee1db34d64d751d63ce8b28a4892a31a97a598
Alexandre Bulté <alexandre@bulte.net>
2023-10-19 12:16:30 +0200

    pytest style assertions

--- udata/tests/apiv2/test_topics.py
+++ udata/tests/apiv2/test_topics.py
@@ -14,28 +14,28 @@ class TopicsAPITest(APITestCase):
         name_topic = TopicFactory(name='topic-for-query')
 
         response = self.get(url_for('apiv2.topics_list'))
-        self.assert200(response)
+        assert response.status_code == 200
         data = response.json['data']
-        self.assertEqual(len(data), 5)
+        assert len(data) == 5
 
         assert all(k in data[0]["datasets"] for k in ["rel", "href", "type", "total"])
         assert all(k in data[0]["reuses"] for k in ["rel", "href", "type", "total"])
 
         response = self.get(url_for('apiv2.topics_list', q='topic-for'))
-        self.assert200(response)
-        self.assertEqual(len(response.json['data']), 1)
-        self.assertEqual(response.json['data'][0]['id'], str(name_topic.id))
+        assert response.status_code == 200
+        assert len(response.json['data']) == 1
+        assert response.json['data'][0]['id'] == str(name_topic.id)
 
         response = self.get(url_for('apiv2.topics_list', tag='energy'))
-        self.assert200(response)
-        self.assertEqual(len(response.json['data']), 1)
-        self.assertEqual(response.json['data'][0]['id'], str(tag_topic.id))
+        assert response.status_code == 200
+        assert len(response.json['data']) == 1
+        assert response.json['data'][0]['id'] == str(tag_topic.id)
 
     def test_topic_api_get(self):
         '''It should fetch a topic from the API'''
         topic = TopicFactory()
         topic_response = self.get(url_for('apiv2.topic', topic=topic))
-        self.assert200(topic_response)
+        assert topic_response.status_code == 200
 
         response = self.get(topic_response.json['datasets']['href'])
         data = response.json
@@ -50,7 +50,7 @@ class TopicDatasetsAPITest(APITestCase):
     def test_topic_datasets_list(self):
         topic = TopicFactory()
         response = self.get(url_for('apiv2.topic_datasets', topic=topic))
-        self.assert200(response)
+        assert response.status_code == 200
         data = response.json['data']
         assert len(data) == 3
         assert all(str(d.id) in (_d['id'] for _d in data) for d in topic.datasets)
@@ -60,7 +60,7 @@ class TopicReusesAPITest(APITestCase):
     def test_topic_datasets_list(self):
         topic = TopicFactory()
         response = self.get(url_for('apiv2.topic_reuses', topic=topic))
-        self.assert200(response)
+        assert response.status_code == 200
         data = response.json['data']
         assert len(data) == 3
         assert all(str(r.id) in (_r['id'] for _r in data) for r in topic.reuses)
