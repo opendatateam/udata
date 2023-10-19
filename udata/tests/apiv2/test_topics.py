dbec5413d9bcce607b5d8eff62a355dc2ad92aba
Alexandre Bulté <alexandre@bulte.net>
2023-10-19 12:54:51 +0200

    More tests

--- udata/tests/apiv2/test_topics.py
+++ udata/tests/apiv2/test_topics.py
@@ -1,7 +1,9 @@
 from flask import url_for
 
 from udata.tests.api import APITestCase
+from udata.core.dataset.factories import DatasetFactory
 from udata.core.topic.factories import TopicFactory
+from udata.core.user.factories import UserFactory
 
 
 class TopicsAPITest(APITestCase):
@@ -18,8 +20,9 @@ class TopicsAPITest(APITestCase):
         data = response.json['data']
         assert len(data) == 5
 
-        assert all(k in data[0]["datasets"] for k in ["rel", "href", "type", "total"])
-        assert all(k in data[0]["reuses"] for k in ["rel", "href", "type", "total"])
+        hateoas_fields = ["rel", "href", "type", "total"]
+        assert all(k in data[0]["datasets"] for k in hateoas_fields)
+        assert all(k in data[0]["reuses"] for k in hateoas_fields)
 
         response = self.get(url_for('apiv2.topics_list', q='topic-for'))
         assert response.status_code == 200
@@ -47,7 +50,7 @@ class TopicsAPITest(APITestCase):
 
 
 class TopicDatasetsAPITest(APITestCase):
-    def test_topic_datasets_list(self):
+    def test_list(self):
         topic = TopicFactory()
         response = self.get(url_for('apiv2.topic_datasets', topic=topic))
         assert response.status_code == 200
@@ -55,9 +58,81 @@ class TopicDatasetsAPITest(APITestCase):
         assert len(data) == 3
         assert all(str(d.id) in (_d['id'] for _d in data) for d in topic.datasets)
 
+    def test_add_datasets(self):
+        owner = self.login()
+        topic = TopicFactory(owner=owner)
+        d1, d2 = DatasetFactory.create_batch(2)
+        response = self.post(url_for('apiv2.topic_datasets', topic=topic), [
+            {'id': d1.id}, {'id': d2.id}
+        ])
+        assert response.status_code == 201
+        topic.reload()
+        assert len(topic.datasets) == 5
+        assert all(d.id in (_d.id for _d in topic.datasets) for d in (d1, d2))
+
+    def test_add_datasets_double(self):
+        owner = self.login()
+        topic = TopicFactory(owner=owner)
+        dataset = DatasetFactory()
+        response = self.post(url_for('apiv2.topic_datasets', topic=topic), [
+            {'id': dataset.id}, {'id': dataset.id}
+        ])
+        assert response.status_code == 201
+        topic.reload()
+        assert len(topic.datasets) == 4
+        response = self.post(url_for('apiv2.topic_datasets', topic=topic), [
+            {'id': dataset.id}
+        ])
+        assert response.status_code == 201
+        topic.reload()
+        assert len(topic.datasets) == 4
+
+    def test_add_datasets_perm(self):
+        user = UserFactory()
+        topic = TopicFactory(owner=user)
+        dataset = DatasetFactory()
+        self.login()
+        response = self.post(url_for('apiv2.topic_datasets', topic=topic), [
+            {'id': dataset.id}
+        ])
+        assert response.status_code == 403
+
+    def test_add_datasets_wrong_payload(self):
+        owner = self.login()
+        topic = TopicFactory(owner=owner)
+        response = self.post(url_for('apiv2.topic_datasets', topic=topic), [
+            {'id': 'xxx'}
+        ])
+        assert response.status_code == 400
+        response = self.post(url_for('apiv2.topic_datasets', topic=topic), [
+            {'nain': 'portekoi'}
+        ])
+        assert response.status_code == 400
+        response = self.post(url_for('apiv2.topic_datasets', topic=topic), {'non': 'mais'})
+        assert response.status_code == 400
+
+
+class TopicDatasetAPITest(APITestCase):
+    def test_delete_dataset(self):
+        owner = self.login()
+        topic = TopicFactory(owner=owner)
+        dataset = topic.datasets[0]
+        response = self.delete(url_for('apiv2.topic_dataset', topic=topic, dataset=dataset))
+        assert response.status_code == 204
+        topic.reload()
+        assert len(topic.datasets) == 2
+        assert dataset not in (d.id for d in topic.datasets)
+
+    def test_delete_dataset_perm(self):
+        topic = TopicFactory(owner=UserFactory())
+        dataset = topic.datasets[0]
+        self.login()
+        response = self.delete(url_for('apiv2.topic_dataset', topic=topic, dataset=dataset))
+        assert response.status_code == 403
+
 
 class TopicReusesAPITest(APITestCase):
-    def test_topic_datasets_list(self):
+    def test_list(self):
         topic = TopicFactory()
         response = self.get(url_for('apiv2.topic_reuses', topic=topic))
         assert response.status_code == 200
