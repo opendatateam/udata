04cdf11b87fd5f5b03db870babe09808e855bc6e
quaxsze <plum.stanislas@gmail.com>
2023-11-21 18:56:08 +0100

    fix test

--- udata/core/contact_point/models.py
+++ udata/core/contact_point/models.py
@@ -7,3 +7,7 @@ __all__ = ('ContactPoint', )
 class ContactPoint(db.Document, db.Owned):
     email = db.StringField(max_length=255, required=True)
     name = db.StringField(max_length=255, required=True)
+
+    meta = {
+        'queryset_class': db.OwnedQuerySet
+    }
