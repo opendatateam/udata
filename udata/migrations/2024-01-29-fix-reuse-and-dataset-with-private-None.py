6f36233d9c8dcc233a5f55ffb9e34be0d406fec9
Thibaud Dauce <git@hadibut.fr>
2024-02-05 11:04:35 +0100

    Simplify and fix logic

--- udata/migrations/2024-01-29-fix-reuse-and-dataset-with-private-None.py
+++ udata/migrations/2024-01-29-fix-reuse-and-dataset-with-private-None.py
@@ -7,30 +7,15 @@ from udata.models import Reuse, Dataset
 
 log = logging.getLogger(__name__)
 
-
 def migrate(db):
-    log.info('Processing Reuse.')
+    log.info('Processing Reuse…')
 
-    reuses = Reuse.objects().no_cache().timeout(False)
-    count = 0
-    for reuse in reuses:
-        if reuse.private == None: 
-            reuse.private = False
-            reuse.save()
-            count += 1
-
+    count = Reuse.objects(private=None).update(private=False)
     log.info(f'Fixed {count} Reuse objects from private None to private False.')
 
-    log.info('Processing Reuse.')
-
-    datasets = Dataset.objects().no_cache().timeout(False)
-    count = 0
-    for dataset in datasets:
-        if dataset.private == None: 
-            dataset.private = False
-            dataset.save()
-            count += 1
-
+    log.info('Processing Datasets…')
+    count = Dataset.objects(private=None).update(private=False)
     log.info(f'Fixed {count} Dataset objects from private None to private False.')
 
     log.info('Done')
+    raise 'oups'
