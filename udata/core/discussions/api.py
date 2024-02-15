d7fbea1719d7f141964bbafc980650c1ec2eeaa8
Thibaud Dauce <ThibaudDauce@users.noreply.github.com>
2024-02-15 10:10:01 +0100

    Improved spam prevention (#2954)
    * Backend of spam prevention
    
    * Add comments and small tweaks
    
    * Add language detection
    
    * Initial mattermost notification system
    
    * Fix listener broken on unknown arguments
    
    * Update requirements
    
    * Fix potentiel
    
    * Add trailing slash
    
    Co-authored-by: maudetes <maudet.estelle@gmail.com>
    
    * fix discussion without first message
    
    * Fix breadcrumbs and enable Mattermost notifications
    
    * Add admin permissions on delete spam
    
    * Use inheritence instead of signals to detect spam
    
    * Fix updates of models for spam detection
    
    * Use _created
    
    * Small tweaks
    
    * Fix two notifications sent
    
    * Add API endpoint to fetch all potential spam
    
    * Try again tests
    
    * Remove comment
    
    * Fix test
    
    * Add new env to settings
    
    * Add SPAM_MINIMUM_STRING_LENGTH_FOR_LANG_CHECK conf
    
    ---------
    
    Co-authored-by: maudetes <maudet.estelle@gmail.com>

--- udata/core/discussions/api.py
+++ udata/core/discussions/api.py
@@ -6,6 +6,8 @@ from flask_restx.inputs import boolean
 
 from udata.auth import admin_permission
 from udata.api import api, API, fields
+from udata.core.spam.api import SpamAPIMixin
+from udata.core.spam.fields import spam_fields
 from udata.utils import id_or_404
 from udata.core.user.api_fields import user_ref_fields
 
@@ -24,6 +26,7 @@ message_fields = api.model('DiscussionMessage', {
     'posted_by': fields.Nested(user_ref_fields,
                                description='The message author'),
     'posted_on': fields.ISODateTime(description='The message posting date'),
+    'spam': fields.Nested(spam_fields),
 })
 
 discussion_fields = api.model('Discussion', {
@@ -43,6 +46,7 @@ discussion_fields = api.model('Discussion', {
     'url': fields.UrlFor('api.discussion',
                          description='The discussion API URI'),
     'extras': fields.Raw(description='Extra attributes as key-value pairs'),
+    'spam': fields.Nested(spam_fields),
 })
 
 start_discussion_fields = api.model('DiscussionStart', {
@@ -86,6 +90,12 @@ parser.add_argument(
     help='The page size to fetch')
 
 
+@ns.route('/<id>/spam/', endpoint='discussion_spam')
+@ns.doc(delete={'id': 'unspam'})
+class DiscussionSpamAPI(SpamAPIMixin):
+    model = Discussion
+
+
 @ns.route('/<id>/', endpoint='discussion')
 class DiscussionAPI(API):
     '''
@@ -123,9 +133,9 @@ class DiscussionAPI(API):
             discussion.closed = datetime.utcnow()
         discussion.save()
         if close:
-            on_discussion_closed.send(discussion, message=message_idx)
+            discussion.signal_close(message=message_idx)
         else:
-            on_new_discussion_comment.send(discussion, message=message_idx)
+            discussion.signal_comment(message=message_idx)
         return discussion
 
     @api.secure(admin_permission)
@@ -138,6 +148,17 @@ class DiscussionAPI(API):
         on_discussion_deleted.send(discussion)
         return '', 204
 
+
+@ns.route('/<id>/comments/<int:cidx>/spam', endpoint='discussion_comment_spam')
+@ns.doc(delete={'id': 'unspam'})
+class DiscussionSpamAPI(SpamAPIMixin):
+    def get_model(self, id, cidx):
+        discussion = Discussion.objects.get_or_404(id=id_or_404(id))
+        if len(discussion.discussion) <= cidx:
+            api.abort(404, 'Comment does not exist')
+        elif cidx == 0:
+            api.abort(400, 'You cannot unspam the first comment of a discussion')
+        return discussion, discussion.discussion[cidx]
 
 @ns.route('/<id>/comments/<int:cidx>', endpoint='discussion_comment')
 class DiscussionCommentAPI(API):
@@ -196,6 +217,7 @@ class DiscussionsAPI(API):
         discussion = Discussion(user=current_user.id, discussion=[message])
         form.populate_obj(discussion)
         discussion.save()
-        on_new_discussion.send(discussion)
+
+        discussion.signal_new()
 
         return discussion, 201
