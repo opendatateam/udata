a990a4e521954bddfe13e219f7708d594cdd6f76
Thibaud Dauce <git@hadibut.fr>
2024-02-20 10:03:02 +0100

    wip

--- udata/core/organization/csv.py
+++ udata/core/organization/csv.py
@@ -1,3 +1,4 @@
+from udata.core.dataset.models import Dataset
 from udata.frontend import csv
 
 from .models import Organization
@@ -5,6 +6,8 @@ from .models import Organization
 
 @csv.adapter(Organization)
 class OrganizationCsvAdapter(csv.Adapter):
+    downloads_counts = None
+
     fields = (
         'id',
         'name',
@@ -18,4 +21,20 @@ class OrganizationCsvAdapter(csv.Adapter):
     )
 
     def dynamic_fields(self):
+        self.get_downloads_counts()
         return csv.metric_fields(Organization)
+    
+    def get_downloads_counts(self):
+        if self.downloads_counts is not None:
+            return self.downloads_counts
+
+        self.downloads_counts = {}
+
+        ids = [o.id for o in self.queryset]
+        for dataset in Dataset.objects(organization__in=ids):
+            if self.downloads_counts.get(dataset.organization) is None:
+                self.downloads_counts[dataset.organization] = 0
+
+            self.downloads_counts[dataset.organization] += sum(resource.metrics.get('views', 0) for resource in dataset.resources)
+
+        return {}
