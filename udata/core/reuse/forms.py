326a2d9bbc2aac55e9c30c2c861e3793342aedfb
Thibaud Dauce <git@hadibut.fr>
2024-03-28 10:22:52 +0100

    Update reuse constants

--- udata/core/reuse/forms.py
+++ udata/core/reuse/forms.py
@@ -1,6 +1,7 @@
 from udata.forms import ModelForm, fields, validators
 from udata.i18n import lazy_gettext as _
-from udata.models import Reuse, REUSE_TYPES, REUSE_TOPICS
+from udata.models import Reuse
+from udata.core.reuse.constants import REUSE_TYPES, REUSE_TOPICS
 
 from .models import IMAGE_SIZES, TITLE_SIZE_LIMIT, DESCRIPTION_SIZE_LIMIT
 
