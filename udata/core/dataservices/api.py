e063a6efaa1f02e2ef17c5c1697f6ad741fe121c
Thibaud Dauce <git@hadibut.fr>
2024-04-16 14:54:20 +0200

    Add (beta) to dataservices namespace

--- udata/core/dataservices/api.py
+++ udata/core/dataservices/api.py
@@ -9,8 +9,7 @@ from udata.core.dataset.permissions import OwnablePermission
 from .models import Dataservice
 from udata.models import db
 
-ns = api.namespace('dataservices', 'Dataservices related operations')
-
+ns = api.namespace('dataservices (beta)', 'Dataservices related operations')
 
 @ns.route('/', endpoint='dataservices')
 class DataservicesAPI(API):
