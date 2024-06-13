a2d96dc17a5a3aa7e94f0f01fb50b4a0925280bc
Thibaud Dauce <git@hadibut.fr>
2024-06-13 09:02:04 +0200

    Add check_integrity task

--- udata/tasks.py
+++ udata/tasks.py
@@ -170,6 +170,7 @@ def init_app(app):
     import udata.core.badges.tasks  # noqa
     import udata.core.storages.tasks  # noqa
     import udata.harvest.tasks  # noqa
+    import udata.db.tasks  # noqa
 
     entrypoints.get_enabled('udata.tasks', app)
 
