ac614040706d7b50bdbe9e2c340781b2c75cb909
Mathieu Agopian <mathieu@agopian.info>
2024-07-29 17:49:57 +0200

    tests not running: remove leftover test, rename and fix fixtures test (#3105)
    * Remove leftover test, rename and fix fixtures test
    
    * Add a change to the changelog

--- udata/tests/features/territories/commands.py
+++
@@ -1,9 +0,0 @@
-from udata.tests import TestCase
-
-
-class CommandsTest(TestCase):
-    def test_import_commands(self):
-        try:
-            from udata.features.territories import commands  # noqa
-        except ImportError as e:
-            self.fail(e)
