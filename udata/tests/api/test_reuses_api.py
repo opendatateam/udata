924226d139cead4d049e1c8f0812ca5908a2009a
Thibaud Dauce <git@hadibut.fr>
2024-07-29 08:05:21 +0200

    Remove unused imports

--- udata/tests/api/test_reuses_api.py
+++ udata/tests/api/test_reuses_api.py
@@ -5,11 +5,9 @@ from flask import url_for
 
 from udata.core.badges.factories import badge_factory
 from udata.core.dataset.factories import DatasetFactory
-from udata.core.organization import metrics
 from udata.core.organization.factories import OrganizationFactory
 from udata.core.reuse.constants import REUSE_TOPICS, REUSE_TYPES
 from udata.core.reuse.factories import ReuseFactory
-from udata.core.user import metrics
 from udata.core.user.factories import AdminFactory, UserFactory
 from udata.models import Follow, Member, Reuse
 from udata.tests.helpers import (
