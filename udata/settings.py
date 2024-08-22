7004ab845ff9f50727c079a481eec4a0fc1b9b01
Mathieu Agopian <mathieu@agopian.info>
2024-08-22 11:11:58 +0200

    @ThibaudDauce's review: set `RESOURCES_ANALYSER_API_KEY` to `None` by default
    Co-authored-by: Thibaud Dauce <ThibaudDauce@users.noreply.github.com>

--- udata/settings.py
+++ udata/settings.py
@@ -522,7 +522,7 @@ class Defaults(object):
     FIXTURE_DATASET_SLUGS = []
     PUBLISH_ON_RESOURCE_EVENTS = False
     RESOURCES_ANALYSER_URI = "http://localhost:8000"
-    RESOURCES_ANALYSER_API_KEY = "api_key_to_change"
+    RESOURCES_ANALYSER_API_KEY = None
 
     # Datasets quality settings
     ###########################################################################
