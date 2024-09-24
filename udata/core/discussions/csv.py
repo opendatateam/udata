3dece54e423a6d3b6bd8f704f61b7cf62d77728a
Pierlou <pierlou.ramade@data.gouv.fr>
2024-09-24 17:07:27 +0200

    fix: no duplicates in participants ids

--- udata/core/discussions/csv.py
+++ udata/core/discussions/csv.py
@@ -13,7 +13,7 @@ class DiscussionCsvAdapter(csv.Adapter):
         ("subject_id", "subject.id"),
         "title",
         ("size", lambda o: len(o.discussion)),
-        ("participants", lambda o: ",".join(msg.posted_by.id for msg in o.discussion)),
+        ("participants", lambda o: ",".join(set(msg.posted_by.id for msg in o.discussion))),
         ("messages", lambda o: "\n".join(msg.content.replace("\n", " ") for msg in o.discussion)),
         "created",
         "closed",
