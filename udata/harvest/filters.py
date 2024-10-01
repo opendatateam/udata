06778a666d842c7580f176fdf7b4d7f3e02f7ec0
Alexandre Bulté <alexandre@bulte.net>
2024-10-01 15:40:43 +0200

    Revert "chore: remove unused harvest.filters and tests"
    This reverts commit 2853142ca9884eea061d0f1411cd355c111d73f1.

---
+++ udata/harvest/filters.py
@@ -0,0 +1,113 @@
+import dateutil.parser
+from voluptuous import Invalid
+
+from udata import tags, uris
+
+
+def boolean(value):
+    """
+    Convert the content of a string (or a number) to a boolean.
+    Do nothing when input value is already a boolean.
+
+    This filter accepts usual values for ``True`` and ``False``:
+    "0", "f", "false", "n", etc.
+    """
+    if value is None or isinstance(value, bool):
+        return value
+
+    try:
+        return bool(int(value))
+    except ValueError:
+        lower_value = value.strip().lower()
+        if not lower_value:
+            return None
+        if lower_value in ("f", "false", "n", "no", "off"):
+            return False
+        if lower_value in ("on", "t", "true", "y", "yes"):
+            return True
+        raise Invalid("Unable to parse boolean {0}".format(value))
+
+
+def to_date(value):
+    """Parse a date"""
+    return dateutil.parser.parse(value).date()
+
+
+def email(value):
+    """Validate an email"""
+    if "@" not in value:
+        raise Invalid("This email is invalid.")
+    return value
+
+
+def force_list(value):
+    """
+    Ensure single elements are wrapped into list
+    """
+    if not isinstance(value, (list, tuple)):
+        return [value]
+    return value
+
+
+def slug(value):
+    return tags.slug(value)
+
+
+def normalize_tag(value):
+    return tags.normalize(value)
+
+
+def taglist(value):
+    return tags.tags_list(value)
+
+
+def empty_none(value):
+    """Replace falsy values with None"""
+    return value if value else None
+
+
+def strip(value):
+    """Strip spaces from a string and remove it when empty."""
+    return empty_none(value.strip())
+
+
+def line_endings(value):
+    """Replaces CR + LF or CR to LF in a string,
+    then strip spaces and remove it when empty.
+    """
+    return value.replace("\r\n", "\n").replace("\r", "\n")
+
+
+def normalize_string(value):
+    return strip(line_endings(value))
+
+
+def is_url(default_scheme="http", **kwargs):
+    """Return a converter that converts a clean string to an URL."""
+
+    def converter(value):
+        if value is None:
+            return value
+        if "://" not in value and default_scheme:
+            value = "://".join((default_scheme, value.strip()))
+        try:
+            return uris.validate(value)
+        except uris.ValidationError as e:
+            raise Invalid(str(e))
+
+    return converter
+
+
+def hash(value):
+    """Detect an hash type"""
+    if not value:
+        return
+    elif len(value) == 32:
+        type = "md5"
+    elif len(value) == 40:
+        type = "sha1"
+    elif len(value) == 64:
+        type = "sha256"
+    else:
+        return None
+    return {"type": type, "value": value}
---
+++ udata/harvest/tests/test_filters.py
@@ -0,0 +1,88 @@
+import logging
+
+import pytest
+from voluptuous import Invalid
+
+from udata.harvest import filters
+
+log = logging.getLogger(__name__)
+
+
+class FiltersTest:
+    def test_boolean(self):
+        true_values = (
+            "1",
+            "on",
+            "t",
+            "TRUE",
+            "true",
+            "y",
+            "yes",
+            "  1  ",
+            "  tRuE  ",
+            True,
+            1,
+            2,
+            -1,
+        )
+        false_values = ("0", "f", "FALSE", "false", "n", "no", "off", "  0  ", "  f  ", False, 0)
+        none_values = ("", "   ", None)
+
+        for value in true_values:
+            assert filters.boolean(value)
+
+        for value in false_values:
+            assert filters.boolean(value) is False
+
+        for value in none_values:
+            assert filters.boolean(value) is None
+
+        with pytest.raises(Invalid):
+            filters.boolean("vrai")
+
+    def test_empty_none(self):
+        empty_values = 0, "", [], {}
+        non_empty_values = "hello", "  hello  "
+
+        for value in empty_values:
+            assert filters.empty_none(value) is None
+
+        for value in non_empty_values:
+            assert filters.empty_none(value) == value
+
+    def test_strip(self):
+        assert filters.strip("  hello   ") == "hello"
+        assert filters.strip("    ") is None
+
+    def test_line_endings(self):
+        result = filters.line_endings("hello\r\nworld!\r ")
+        assert result == "hello\nworld!\n "
+
+    def test_hash(self):
+        hashes = {
+            "md5": "bd8668597bfba2d1843441d7199bea65",
+            "sha1": "f2f0249827f501286b4713683e526d541d2cc7e2",
+            "sha256": ("c4373e1d81eb44882bf9ff539d0e5f" "faf03a114abf9306591117d781966268f9"),
+        }
+
+        for type, value in hashes.items():
+            assert filters.hash(value) == {"type": type, "value": value}
+
+    def test_unknown_hash(self):
+        assert filters.hash("xxxx") is None
+        assert filters.hash(None) is None
+
+
+class IsUrlFilterTest:
+    def test_valid_url_with_defaults(self):
+        f = filters.is_url()
+        assert f("https://somewhere.com/path") == "https://somewhere.com/path"
+
+    def test_allowed_scheme_not_allowed(self):
+        f = filters.is_url()
+        with pytest.raises(Invalid):
+            f("not-allowed://somewhere.com")
+
+    def test_valid_url_with_default_scheme(self):
+        f = filters.is_url()
+        assert f("somewhere.com/path") == "http://somewhere.com/path"
