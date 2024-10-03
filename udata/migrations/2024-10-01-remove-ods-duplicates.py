2ace95699442253681da1350239e4e566f54ad06
maudetes <maudet.estelle@gmail.com>
2024-10-03 11:17:33 +0200

    Improve readability and save dataset only if it has been updated

--- udata/migrations/2024-10-01-remove-ods-duplicates.py
+++ udata/migrations/2024-10-01-remove-ods-duplicates.py
@@ -21,14 +21,19 @@ def migrate(db):
     count = 0
     errors = 0
 
+    original = "/exports/{0}"
+    first_duplication = "?use_labels=false"
+    second_duplication = "?use_labels=true"
+
     # Datasets with use_labels=false are the one that were most surely impacted by the URL modifications
-    datasets = Dataset.objects(resources__url__contains="use_labels=false")
+    datasets = Dataset.objects(resources__url__contains=first_duplication)
 
     log.info("Starting")
     log.info(f"{datasets.count()} datasets to process...")
     for dat in datasets:
+        to_save = False
         for res_format in ["csv", "xlsx"]:
-            resources = [res for res in dat.resources if f"/exports/{res_format}" in res.url]
+            resources = [res for res in dat.resources if original.format(res_format) in res.url]
             if not resources:
                 # This dataset doesn't have csv or xlsx resources
                 continue
@@ -41,19 +46,19 @@ def migrate(db):
                 res_original = next(
                     res
                     for res in resources
-                    if res.url.endswith(f"/exports/{res_format}")
+                    if res.url.endswith(original.format(res_format))
                     and res.title.endswith(f".{res_format}")
                 )
-                res_label_false = next(
-                    res for res in resources if res.url.endswith("?use_labels=false")
+                res_first_duplication = next(
+                    res for res in resources if res.url.endswith(first_duplication)
                 )
-                res_label_true = next(
-                    res for res in resources if res.url.endswith("?use_labels=true")
+                res_second_duplication = next(
+                    res for res in resources if res.url.endswith(second_duplication)
                 )
 
-                # Resource that exists prior to ODS to DCAT migration, cf https://github.com/opendatateam/udata-ods/pull/247
-                # and somehow did not get migrated properly
-                res_old_school = next(
+                # Resource that exists prior to ODS to DCAT migration and somehow did not get migrated properly
+                # cf https://github.com/opendatateam/udata-ods/pull/247
+                res_migration_duplicate = next(
                     (
                         res
                         for res in resources
@@ -69,21 +74,24 @@ def migrate(db):
                 errors += 1
                 continue
 
-            # We want to keep the original resource and update its URL
-            # and remove all the other duplicates
-            res_original.url += "?use_labels=true"
-            dat.remove_resource(res_label_false)
-            dat.remove_resource(res_label_true)
-            if res_old_school:
-                dat.remove_resource(res_old_school)
+            # Keep the original resource and update its URL
+            res_original.url += second_duplication
+            # Remove all the other duplicates
+            dat.remove_resource(res_first_duplication)
+            dat.remove_resource(res_second_duplication)
+            if res_migration_duplicate:
+                dat.remove_resource(res_migration_duplicate)
 
-        try:
-            dat.save()
-            count += 1
-        except ValidationError:
-            log.info(f"Could not save dataset {dat.id}")
-            log.info(traceback.format_exc())
-            errors += 1
+            to_save = True
+
+        if to_save:
+            try:
+                dat.save()
+                count += 1
+            except ValidationError:
+                log.info(f"Could not save dataset {dat.id}")
+                log.info(traceback.format_exc())
+                errors += 1
 
     log.info("Done !")
     log.info(f"Updated {count} datasets. Failed on {errors} objects.")
