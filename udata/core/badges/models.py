6b0e26a2fb4a21bb8fdd1e4de8600f7b7d38b5bb
Thibaud Dauce <git@hadibut.fr>
2024-10-03 12:10:53 +0200

    Filter by org badge complex

--- udata/core/badges/models.py
+++ udata/core/badges/models.py
@@ -3,7 +3,7 @@ from datetime import datetime
 
 from mongoengine.signals import post_save
 
-from udata.api_fields import field
+from udata.api_fields import field, generate_fields
 from udata.auth import current_user
 from udata.core.badges.fields import badge_fields
 from udata.mongo import db
@@ -12,27 +12,23 @@ from .signals import on_badge_added, on_badge_removed
 
 log = logging.getLogger(__name__)
 
-__all__ = ("Badge", "BadgeMixin")
 
+def new_badge_type(choices):
+    @generate_fields(default_filterable_field="kind")
+    class Badge(db.EmbeddedDocument):
+        kind = db.StringField(required=True, choices=choices)
+        created = db.DateTimeField(default=datetime.utcnow, required=True)
+        created_by = db.ReferenceField("User")
 
-class Badge(db.EmbeddedDocument):
-    kind = db.StringField(required=True)
-    created = db.DateTimeField(default=datetime.utcnow, required=True)
-    created_by = db.ReferenceField("User")
+        def __str__(self):
+            return self.kind
 
-    def __str__(self):
-        return self.kind
-
-    def validate(self, clean=True):
-        badges = getattr(self._instance, "__badges__", {})
-        if self.kind not in badges.keys():
-            raise db.ValidationError("Unknown badge type %s" % self.kind)
-        return super(Badge, self).validate(clean=clean)
+    return Badge
 
 
 class BadgesList(db.EmbeddedDocumentListField):
-    def __init__(self, *args, **kwargs):
-        return super(BadgesList, self).__init__(Badge, *args, **kwargs)
+    def __init__(self, badge_type, *args, **kwargs):
+        return super(BadgesList, self).__init__(badge_type, *args, **kwargs)
 
     def validate(self, value):
         kinds = [b.kind for b in value]
@@ -41,52 +37,66 @@ class BadgesList(db.EmbeddedDocumentListField):
         return super(BadgesList, self).validate(value)
 
 
-class BadgeMixin(object):
-    badges = field(
-        BadgesList(),
-        readonly=True,
-        inner_field_info={"nested_fields": badge_fields},
-    )
+def badge_mixin(choices):
+    badge_type = new_badge_type(choices)
 
-    def get_badge(self, kind):
-        """Get a badge given its kind if present"""
-        candidates = [b for b in self.badges if b.kind == kind]
-        return candidates[0] if candidates else None
+    class BadgeMixin(object):
+        badges = field(
+            BadgesList(badge_type),
+            readonly=True,
+            inner_field_info={"nested_fields": badge_fields},
+        )
 
-    def add_badge(self, kind):
-        """Perform an atomic prepend for a new badge"""
-        badge = self.get_badge(kind)
-        if badge:
-            return badge
-        if kind not in getattr(self, "__badges__", {}):
-            msg = "Unknown badge type for {model}: {kind}"
-            raise db.ValidationError(msg.format(model=self.__class__.__name__, kind=kind))
-        badge = Badge(kind=kind)
-        if current_user.is_authenticated:
-            badge.created_by = current_user.id
+        def get_badge(self, kind):
+            """Get a badge given its kind if present"""
+            candidates = [b for b in self.badges if b.kind == kind]
+            return candidates[0] if candidates else None
 
-        self.update(__raw__={"$push": {"badges": {"$each": [badge.to_mongo()], "$position": 0}}})
-        self.reload()
-        post_save.send(self.__class__, document=self)
-        on_badge_added.send(self, kind=kind)
-        return self.get_badge(kind)
+        def add_badge(self, kind):
+            """Perform an atomic prepend for a new badge"""
+            badge = self.get_badge(kind)
+            if badge:
+                return badge
+            if kind not in getattr(self, "__badges__", {}):
+                msg = "Unknown badge type for {model}: {kind}"
+                raise db.ValidationError(msg.format(model=self.__class__.__name__, kind=kind))
+            badge = self.badge_type(kind=kind)
+            if current_user.is_authenticated:
+                badge.created_by = current_user.id
 
-    def remove_badge(self, kind):
-        """Perform an atomic removal for a given badge"""
-        self.update(__raw__={"$pull": {"badges": {"kind": kind}}})
-        self.reload()
-        on_badge_removed.send(self, kind=kind)
-        post_save.send(self.__class__, document=self)
+            self.update(
+                __raw__={"$push": {"badges": {"$each": [badge.to_mongo()], "$position": 0}}}
+            )
+            self.reload()
+            post_save.send(self.__class__, document=self)
+            on_badge_added.send(self, kind=kind)
+            return self.get_badge(kind)
 
-    def toggle_badge(self, kind):
-        """Toggle a bdage given its kind"""
-        badge = self.get_badge(kind)
-        if badge:
-            return self.remove_badge(kind)
-        else:
-            return self.add_badge(kind)
+        def remove_badge(self, kind):
+            """Perform an atomic removal for a given badge"""
+            self.update(__raw__={"$pull": {"badges": {"kind": kind}}})
+            self.reload()
+            on_badge_removed.send(self, kind=kind)
+            post_save.send(self.__class__, document=self)
 
-    def badge_label(self, badge):
-        """Display the badge label for a given kind"""
-        kind = badge.kind if isinstance(badge, Badge) else badge
-        return self.__badges__[kind]
+        def toggle_badge(self, kind):
+            """Toggle a bdage given its kind"""
+            badge = self.get_badge(kind)
+            if badge:
+                return self.remove_badge(kind)
+            else:
+                return self.add_badge(kind)
+
+        def badge_label(self, badge):
+            """Display the badge label for a given kind"""
+            kind = badge.kind if isinstance(badge, self.badge_type) else badge
+            return self.__badges__[kind]
+
+    setattr(BadgeMixin, "badge_type", badge_type)
+
+    return BadgeMixin
+
+
+# For backward compatibility create a badge type without any choices
+Badge = new_badge_type(None)
+BadgeMixin = badge_mixin(None)
