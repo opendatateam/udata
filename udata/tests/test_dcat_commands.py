ee53c3e80adc22a88efc09a9761819afff249677
Mathieu Agopian <mathieu@agopian.info>
2024-12-10 11:23:29 +0100

    Better testing of "click" without brittle monkeypatching that fails on the CI

--- udata/tests/test_dcat_commands.py
+++ udata/tests/test_dcat_commands.py
@@ -1,17 +1,13 @@
 import pytest
 
-import udata.commands as udata_commands
-
 
 @pytest.mark.usefixtures("clean_db")
 class ParseUrlCommandTest:
-    def test_parse_url(self, cli, requests_mock, caplog, monkeypatch) -> None:
+    def test_parse_url(self, cli, requests_mock, caplog) -> None:
         logs = []
 
         def mock_echo(message: str) -> None:
             logs.append(message)
-
-        monkeypatch.setattr(udata_commands, "echo", mock_echo)
 
         local_file: str = "./udata/harvest/tests/dcat/sig.oreme.rdf"
         # This mock_url is to make requests' url parsing happy, but the mock will return the local file content.
@@ -20,11 +16,11 @@ class ParseUrlCommandTest:
             requests_mock.get(mock_url, text=test_rdf_file.read())
             requests_mock.head(mock_url, text="sig.oreme.rdf")
         dataset_id = "0437a976-cff1-4fa6-807a-c23006df2f8f"
-        cli(
+        result = cli(
             "dcat",
             "parse-url",
             mock_url,
             "--rid",
             dataset_id,
         )
-        assert "\x1b[32m\x1b[1mDataset is valid ✅\x1b[0m" in logs
+        assert "Dataset is valid" in result.stdout
