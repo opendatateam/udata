e0317967dbb54d5d9a89ec6c85a6e290479bccbc
Mathieu Agopian <mathieu@agopian.info>
2024-12-17 17:32:57 +0100

    Update the migration to add a default `contact` role for each existing contact point

--- udata/migrations/2024-12-05-contact-point-is-now-a-list.py
+++ udata/migrations/2024-12-05-contact-point-is-now-a-list.py
@@ -14,9 +14,14 @@ def migrate(db):
     log.info("Processing Contact Point references.")
 
     db = get_db()
+
+    # Add a `contact` role to each existing contact point.
+    db.contact_point.update_many({}, {"$set": {"role": "contact"}})
+
     count = 0
     for collection in [db.dataset, db.dataservice]:
         for obj in collection.find({"contact_point": {"$exists": True}}):
+            # Change `contact_point` to be a list of contact points.
             collection.update_one(
                 {"_id": obj["_id"]}, {"$set": {"contact_point": [obj["contact_point"]]}}
             )
