156b6da57ad9528f9bea51f8196e39eee6484a3b
maudetes <maudet.estelle@gmail.com>
2025-02-21 14:10:06 +0100

    Add inactive_deletion_notified_at field in User model

--- udata/core/user/tasks.py
+++ udata/core/user/tasks.py
@@ -20,18 +20,18 @@ def send_test_mail(email):
 
 
 # TODO: some questions to answer
-# 1. how to make sure that we have the minimum time between notify and delete if for a reason
-#    or another, the notify job is not working.
-#    Should we store a notification_date?
-#    Should it run only on the 1st of each month?
+# 1. We went with storing a inactive_deletion_notified_at to make sure we don't delete
+#    accounts that haven't been notified long enough in advance. Are we bulletproof with this logic?
 # 2. How to deactivate/delete account?
 #    Should we set active=false instead of deleting account?
 #    Can we keep comments?
 #    What about other contents (Dataset, Org, etc.)?
 #    Our confidentiality policy states : "Données relatives au Contributeur qui s’inscrit"
+#    We may explicitely states the contents that will be deleted or not in mail
 # 3. Should we add a link to contact us if they can't connect?
 # 4. Can we deal with ooooold inactive users with this system? Or should we do a specific Brevo campaign?
 # 5. How to make the + / - timedelta more readable?
+# 6. There is room for wording improvement here!
 
 
 @job("notify-inactive-users")
@@ -41,14 +41,16 @@ def notify_inactive_users(self):
             "YEARS_OF_INACTIVITY_BEFORE_DEACTIVATION setting is not set, no deletion planned"
         )
         return
-    last_login_notification_date = (
+    notification_comparison_date = (
         datetime.utcnow()
         - timedelta(days=current_app.config["YEARS_OF_INACTIVITY_BEFORE_DEACTIVATION"] * 365)
         + timedelta(days=current_app.config["DAYS_BEFORE_ACCOUNT_INACTIVITY_NOTIFY_DELAY"])
     )
 
-    for user in User.objects(last_login_at__lte=last_login_notification_date):
+    for user in User.objects(current_login_at__lte=notification_comparison_date):
         mail.send(_("Account inactivity"), user, "account_inactivity", user=user)
+        user.inactive_deletion_notified_at = datetime.utcnow()
+        user.save()
 
 
 @job("delete-inactive-users")
@@ -58,10 +60,24 @@ def delete_inactive_users(self):
             "YEARS_OF_INACTIVITY_BEFORE_DEACTIVATION setting is not set, no deletion planned"
         )
         return
-    last_login_deletion_date = datetime.utcnow() - timedelta(
+
+    # Clear inactive_deletion_notified_at field if user has logged in since notification
+    for user in User.objects(inactive_deletion_notified_at__exists=True):
+        if user.current_login_at > user.inactive_deletion_notified_at:
+            user.inactive_deletion_notified_at = None
+            user.save()
+
+    # Delete inactive users upon notification delay if user still hasn't logged in
+    deletion_comparison_date = datetime.utcnow() - timedelta(
         days=current_app.config["YEARS_OF_INACTIVITY_BEFORE_DEACTIVATION"] * 365
     )
-    for user in User.objects(last_login_at__lte=last_login_deletion_date):
+    notified_at = datetime.utcnow() - timedelta(
+        days=current_app.config["DAYS_BEFORE_ACCOUNT_INACTIVITY_NOTIFY_DELAY"]
+    )
+    for user in User.objects(
+        current_login_at__lte=deletion_comparison_date,
+        inactive_deletion_notified_at__lte=notified_at,
+    ):
         copied_user = copy(user)
         user.mark_as_deleted(notify=False, delete_comments=False)
         mail.send(_("Inactive account deletion"), copied_user, "inactive_account_deleted")
--- udata/tests/user/test_user_tasks.py
+++ udata/tests/user/test_user_tasks.py
@@ -6,6 +6,7 @@ from flask import current_app
 from udata.core.discussions.factories import DiscussionFactory
 from udata.core.user import tasks
 from udata.core.user.factories import UserFactory
+from udata.core.user.models import User
 from udata.i18n import gettext as _
 from udata.tests.api import APITestCase
 from udata.tests.helpers import capture_mails
@@ -14,15 +15,15 @@ from udata.tests.helpers import capture_mails
 class UserTasksTest(APITestCase):
     @pytest.mark.options(YEARS_OF_INACTIVITY_BEFORE_DEACTIVATION=3)
     def test_notify_inactive_users(self):
-        last_login_notification_date = (
+        notification_comparison_date = (
             datetime.utcnow()
             - timedelta(days=current_app.config["YEARS_OF_INACTIVITY_BEFORE_DEACTIVATION"] * 365)
             + timedelta(days=current_app.config["DAYS_BEFORE_ACCOUNT_INACTIVITY_NOTIFY_DELAY"])
             - timedelta(days=1)  # add margin
         )
 
-        inactive_user = UserFactory(last_login_at=last_login_notification_date)
-        UserFactory(last_login_at=datetime.utcnow())  # Active user
+        inactive_user = UserFactory(current_login_at=notification_comparison_date)
+        UserFactory(current_login_at=datetime.utcnow())  # Active user
 
         with capture_mails() as mails:
             tasks.notify_inactive_users()
@@ -34,14 +35,23 @@ class UserTasksTest(APITestCase):
 
     @pytest.mark.options(YEARS_OF_INACTIVITY_BEFORE_DEACTIVATION=3)
     def test_delete_inactive_users(self):
-        last_login_deletion_date = (
+        deletion_comparison_date = (
             datetime.utcnow()
             - timedelta(days=current_app.config["YEARS_OF_INACTIVITY_BEFORE_DEACTIVATION"] * 365)
             - timedelta(days=1)  # add margin
         )
 
-        inactive_user_to_delete = UserFactory(last_login_at=last_login_deletion_date)
-        UserFactory(last_login_at=datetime.utcnow())  # Active user
+        notification_comparison_date = (
+            datetime.utcnow()
+            - timedelta(days=current_app.config["DAYS_BEFORE_ACCOUNT_INACTIVITY_NOTIFY_DELAY"])
+            - timedelta(days=1)  # add margin
+        )
+
+        inactive_user_to_delete = UserFactory(
+            current_login_at=deletion_comparison_date,
+            inactive_deletion_notified_at=notification_comparison_date,
+        )
+        UserFactory(current_login_at=datetime.utcnow())  # Active user
         discussion = DiscussionFactory(user=inactive_user_to_delete)
         discussion_title = discussion.title
 
@@ -58,3 +68,28 @@ class UserTasksTest(APITestCase):
         discussion.reload()
         self.assertEqual(inactive_user_to_delete.fullname, "DELETED DELETED")
         self.assertEqual(discussion.title, discussion_title)
+
+    @pytest.mark.options(YEARS_OF_INACTIVITY_BEFORE_DEACTIVATION=3)
+    def test_keep_inactive_users_that_logged_in(self):
+        notification_comparison_date = (
+            datetime.utcnow()
+            - timedelta(days=current_app.config["DAYS_BEFORE_ACCOUNT_INACTIVITY_NOTIFY_DELAY"])
+            - timedelta(days=1)  # add margin
+        )
+
+        inactive_user_that_logged_in_since_notification = UserFactory(
+            current_login_at=datetime.utcnow(),
+            inactive_deletion_notified_at=notification_comparison_date,
+        )
+
+        with capture_mails() as mails:
+            tasks.delete_inactive_users()
+
+        # Assert no mail has been sent
+        self.assertEqual(len(mails), 0)
+
+        # Assert user hasn't been deleted and won't be deleted
+        self.assertEqual(User.objects().count(), 1)
+        user = User.objects().first()
+        self.assertEqual(user, inactive_user_that_logged_in_since_notification)
+        self.assertIsNone(user.inactive_deletion_notified_at)
