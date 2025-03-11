4581644094930c615394491207463b66301aba01
Thibaud Dauce <git@hadibut.fr>
2025-03-11 12:04:40 +0100

    Fix anonymous user?

--- udata/core/user/api_fields.py
+++ udata/core/user/api_fields.py
@@ -65,8 +65,8 @@ user_fields = api.model(
         ),
         "last_login_at": fields.Raw(
             attribute=lambda o: o.current_login_at
-            if current_user and current_user.sysadmin
-            else None,
+            if current_user.is_authenticated and current_user.sysadmin
+            else None,  
             description="The user last connection date (only present for global admins)",
             readonly=True,
         ),
