c4c597bc774e888391a86661d8a2b5069b0be8c0
Nicolas Kempf Le Stanc <nicolas@conciergerie.dev>
2025-04-30 16:46:17 +0200

    feat: add dataservice activities

--- udata/core/activity/__init__.py
+++ udata/core/activity/__init__.py
@@ -6,6 +6,7 @@ log = logging.getLogger(__name__)
 def init_app(app):
     # Load all core actvitiess
     import udata.core.user.activities  # noqa
+    import udata.core.dataservices.activities  # noqa
     import udata.core.dataset.activities  # noqa
     import udata.core.reuse.activities  # noqa
     import udata.core.organization.activities  # noqa
