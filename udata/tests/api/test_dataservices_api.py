207eb31f05beec81396be7485366c54984614e85
maudetes <maudet.estelle@gmail.com>
2025-05-14 13:55:57 +0200

    Private objects should return 404 by api (#3311)

--- udata/tests/api/test_dataservices_api.py
+++ udata/tests/api/test_dataservices_api.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from xml.etree.ElementTree import XML
 
 import pytest
@@ -17,7 +18,7 @@ from udata.core.organization.factories import OrganizationFactory
 from udata.core.organization.models import Member
 from udata.core.user.factories import AdminFactory, UserFactory
 from udata.i18n import gettext as _
-from udata.tests.helpers import assert200, assert400, assert_redirects
+from udata.tests.helpers import assert200, assert400, assert410, assert_redirects
 
 from . import APITestCase
 
@@ -29,6 +30,40 @@ def dataservice_in_response(response: TestResponse, dataservice: Dataservice) ->
 
 class DataserviceAPITest(APITestCase):
     modules = []
+
+    def test_dataservice_api_get(self):
+        """It should fetch a dataservice from the API"""
+        dataservice = DataserviceFactory()
+        response = self.get(url_for("api.dataservice", dataservice=dataservice))
+        assert200(response)
+
+    def test_dataservice_api_get_deleted(self):
+        """It should not fetch a deleted dataservice from the API and raise 410"""
+        dataservice = DataserviceFactory(deleted_at=datetime.utcnow())
+        response = self.get(url_for("api.dataservice", dataservice=dataservice))
+        assert410(response)
+
+    def test_dataservice_api_get_deleted_but_authorized(self):
+        """It should fetch a deleted dataservice from the API if authorized"""
+        user = self.login()
+        dataservice = DataserviceFactory(deleted_at=datetime.utcnow(), owner=user)
+        response = self.get(url_for("api.dataservice", dataservice=dataservice))
+        assert200(response)
+
+    def test_dataservice_api_get_private(self):
+        """It should not fetch a private dataservice from the API and raise 404"""
+        dataservice = DataserviceFactory(private=True)
+
+        response = self.get(url_for("api.dataservice", dataservice=dataservice))
+        self.assert404(response)
+
+    def test_dataservice_api_get_private_but_authorized(self):
+        """It should fetch a private dataservice from the API if user is authorized"""
+        self.login()
+        dataservice = DataserviceFactory(owner=self.user, private=True)
+
+        response = self.get(url_for("api.dataservice", dataservice=dataservice))
+        self.assert200(response)
 
     def test_dataservices_api_list_with_filters(self):
         """Should filters dataservices results based on query filters"""
@@ -174,10 +209,10 @@ class DataserviceAPITest(APITestCase):
         response = self.get(url_for("api.dataservice", dataservice=dataservice))
         self.assert200(response)
 
-        # We cannot access deleted element as random user
+        # We cannot access private element as random user
         self.login()
         response = self.get(url_for("api.dataservice", dataservice=dataservice))
-        self.assert410(response)
+        self.assert404(response)
 
         # We can undelete with a patch
         self.login(user)
