6fe8f3c472901bc00ddca0bff8c903097edf9597
Thibaud Dauce <git@hadibut.fr>
2025-07-24 12:21:47 +0200

    Add more bloc types

--- udata/core/pages/models.py
+++ udata/core/pages/models.py
@@ -3,7 +3,9 @@ from datetime import datetime
 import udata.core.dataset.api_fields as datasets_api_fields
 from udata.api_fields import field, generate_fields
 from udata.core.activity.models import Auditable
+from udata.core.dataservices.models import Dataservice
 from udata.core.owned import Owned
+from udata.core.reuse.models import Reuse
 from udata.i18n import lazy_gettext as _
 from udata.models import db
 
@@ -16,11 +18,13 @@ class Bloc(db.EmbeddedDocument):
     meta = {"allow_inheritance": True}
 
 
-@generate_fields()
-class DatasetsListBloc(Bloc):
+class BlocWithTitleMixin:
     title = field(db.StringField(required=True))
     subtitle = field(db.StringField())
 
+
+@generate_fields()
+class DatasetsListBloc(BlocWithTitleMixin, Bloc):
     datasets = field(
         db.ListField(
             field(
@@ -32,11 +36,54 @@ class DatasetsListBloc(Bloc):
 
 
 @generate_fields()
+class ReusesListBloc(BlocWithTitleMixin, Bloc):
+    reuses = field(
+        db.ListField(
+            field(
+                db.ReferenceField("Reuse"),
+                nested_fields=Reuse.__read_fields__,
+            )
+        )
+    )
+
+
+@generate_fields()
+class DataservicesListBloc(BlocWithTitleMixin, Bloc):
+    dataservices = field(
+        db.ListField(
+            field(
+                db.ReferenceField("Dataservice"),
+                nested_fields=Dataservice.__read_fields__,
+            )
+        )
+    )
+
+
+@generate_fields()
+class LinkInBloc(db.EmbeddedDocument):
+    title = field(db.StringField(required=True))
+    color = field(db.StringField())
+    url = field(db.StringField())
+
+
+@generate_fields()
+class LinksListBloc(BlocWithTitleMixin, Bloc):
+    paragraph = field(db.StringField())
+    main_link_url = field(db.StringField())
+    main_link_title = field(db.StringField())
+
+    links = field(db.EmbeddedDocumentListField(LinkInBloc))
+
+
+@generate_fields()
 class Page(Auditable, Owned, db.Document):
     blocs = field(
         db.EmbeddedDocumentListField(Bloc),
         generic={
             "datasets_list": DatasetsListBloc,
+            "reuses_list": ReusesListBloc,
+            "dataservices_list": DataservicesListBloc,
+            "links_list": LinksListBloc,
         },
     )
 
