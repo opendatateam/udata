1ee5a3fecfe8ddb0566b10f1d660a40e9d01c367
Thibaud Dauce <git@hadibut.fr>
2025-07-30 13:47:56 +0200

    Some cleanups

--- udata/harvest/tests/test_snapshots.py
+++ udata/harvest/tests/test_snapshots.py
@@ -1,6 +1,5 @@
 import json
 import os
-import xml.etree.ElementTree as ET
 from os.path import dirname, isfile, join
 
 import pytest
@@ -8,6 +7,7 @@ import requests_mock
 from deepdiff import DeepDiff
 from pyld import jsonld
 from rdflib import Graph
+from xmldiff import formatting, main
 
 from udata.core.dataservices.models import Dataservice
 from udata.core.dataset.models import Dataset
@@ -65,12 +65,6 @@ class SnapshotsTest:
         if not refresh:
             data = json.load(open(data_path))
 
-        def match_body(request, history):
-            return parse_request_body(request.text) == history["request"]["body"]
-
-        def make_matcher(history):
-            return lambda request: match_body(request, history)
-
         with MyMock(real_http=refresh) as m:
             if not refresh:
                 for history in data["requests_history"]:
@@ -98,8 +92,6 @@ class SnapshotsTest:
         if refresh:
             json.dump(new_data, open(data_path, "w"), indent=2, default=str)
         else:
-            from xmldiff import formatting, main
-
             if "graphs" in data["job"]["data"]:
                 for index, graph in enumerate(data["job"]["data"]["graphs"]):
                     diff = main.diff_texts(
@@ -107,14 +99,20 @@ class SnapshotsTest:
                         new_data["job"]["data"]["graphs"][index].encode("utf-8"),
                         formatter=formatting.DiffFormatter(),
                     )
-                    print(graph, file=open("/tmp/1.xml", "w"))
-                    print(new_data["job"]["data"]["graphs"][index], file=open("/tmp/2.xml", "w"))
-                    print(f"\n\n{graph}\n\n")
-                    print(f"\n\n{new_data['job']['data']['graphs'][index]}\n\n")
 
-                    assert compare_rdf_graphs_canonically(
+                    rdf_check = compare_rdf_graphs_canonically(
                         graph, new_data["job"]["data"]["graphs"][index]
-                    ), "RDF graphs are differents"
+                    )
+
+                    if not rdf_check:
+                        print(graph, file=open("/tmp/1.xml", "w"))
+                        print(
+                            new_data["job"]["data"]["graphs"][index], file=open("/tmp/2.xml", "w")
+                        )
+                        print(f"\n\n{graph}\n\n")
+                        print(f"\n\n{new_data['job']['data']['graphs'][index]}\n\n")
+
+                    assert rdf_check, "RDF graphs are differents"
 
             diff = DeepDiff(
                 data,
@@ -146,7 +144,6 @@ class SnapshotsTest:
             )
 
             print(diff.pretty())
-            print(diff)
             assert not diff, "Global diffs are different"
 
 
@@ -168,64 +165,12 @@ def compare_rdf_graphs_canonically(xml1: str, xml2: str) -> bool:
     return canon1 == canon2
 
 
-def normalize_rdf(xml_string: str) -> str:
-    g = Graph()
-    g.parse(data=xml_string, format="application/rdf+xml")
-    return "\n".join(sorted(g.serialize(format="nt").splitlines()))
+def match_body(request, history):
+    return parse_request_body(request.text) == history["request"]["body"]
 
 
-def compare_rdf_graphs(xml1: str, xml2: str) -> bool:
-    norm1 = normalize_rdf(xml1)
-    norm2 = normalize_rdf(xml2)
-    return norm1 == norm2
-
-
-def get_attributes(e):
-    return sorted(
-        {
-            k: v
-            for k, v in e.attrib.items()
-            if str(k) == "{http://www.w3.org/1999/02/22-rdf-syntax-ns#}nodeID"
-        }
-    )
-
-
-def sort_children(elem):
-    # Trie les enfants par balise + attributs + texte pour stabilité
-    elem[:] = sorted(elem, key=lambda e: (e.tag, get_attributes(e), (e.text or "").strip()))
-    for child in elem:
-        sort_children(child)
-
-
-def elements_equal(e1, e2):
-    if e1.tag != e2.tag:
-        print(f"Tag are different: {e1.tag} != {e2.tag}")
-        return False
-    if get_attributes(e1) != get_attributes(e2):
-        print("Attributes are different:")
-        print(get_attributes(e1))
-        print(get_attributes(e2))
-        return False
-    if (e1.text or "").strip() != (e2.text or "").strip():
-        print("Text are different:")
-        print((e1.text or "").strip())
-        print((e2.text or "").strip())
-        return False
-    if len(e1) != len(e2):
-        print(f"Length is different: {len(e1)} != {len(e2)}")
-        return False
-    for c1, c2 in zip(e1, e2):
-        if not elements_equal(c1, c2):
-            return False
-    return True
-
-
-def compare_xml(xml1_str, xml2_str):
-    root1 = ET.fromstring(xml1_str)
-    root2 = ET.fromstring(xml2_str)
-    sort_children(root1)
-    sort_children(root2)
-    return elements_equal(root1, root2)
+def make_matcher(history):
+    return lambda request: match_body(request, history)
 
 
 class MyMock(requests_mock.Mocker):
