0856eae770b21ab2982f5cd3979e19c37d8765d5
Nicolas Kempf Le Stanc <nicolas@conciergerie.dev>
2025-09-09 09:39:06 +0200

    chore: qa

--- udata/core/dataset/models.py
+++ udata/core/dataset/models.py
@@ -342,7 +342,7 @@ class DatasetQuerySet(OwnedQuerySet):
 
     def hidden(self):
         return self(db.Q(private=True) | db.Q(deleted__ne=None) | db.Q(archived__ne=None))
-    
+
     def with_badge(self, kind):
         return self(badges__kind=kind)
 
