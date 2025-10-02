94eeb3edbb3960fde5fe10e3604b4b9cdc3c162c
Thibaud Dauce <git@hadibut.fr>
2025-10-02 17:19:25 +0200

    Add comment

--- udata/uris.py
+++ udata/uris.py
@@ -69,6 +69,9 @@ def config_for(value, key):
 
 
 def homepage_url(**kwargs) -> str:
+    # Some tests were crashing not finding the route for api.site
+    # while rendering a 404… Not sure why but try/except seems to fix
+    # this :-(
     try:
         return cdata_url("/", **kwargs) or url_for("api.site", **kwargs)
     except Exception:
