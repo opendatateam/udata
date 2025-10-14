6245158f8c9b0f67d17467282cccac93aff8c057
Thibaud Dauce <git@hadibut.fr>
2025-10-14 12:43:56 +0200

    refactor: remove linkchecker

--- udata/entrypoints.py
+++ udata/entrypoints.py
@@ -4,7 +4,6 @@ import pkg_resources
 ENTRYPOINTS = {
     "udata.avatars": "Avatar rendering backends",
     "udata.harvesters": "Harvest backends",
-    "udata.linkcheckers": "Link checker backends",
     "udata.metrics": "Extra metrics",
     "udata.models": "Models and migrations",
     "udata.preview": "Displays preview for resources",
