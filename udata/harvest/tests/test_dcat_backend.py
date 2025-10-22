2d2cf594b73e293addab7c603d07ad564cd83082
streino <streino@disroot.org>
2025-10-22 16:57:16 +0200

    Display org name instead of individual name when contact has both

--- udata/harvest/tests/test_dcat_backend.py
+++ udata/harvest/tests/test_dcat_backend.py
@@ -714,7 +714,7 @@ class DcatBackendTest(PytestOnlyDBTestCase):
         )  # noqa
         assert dataset.harvest.last_update.date() == date.today()
 
-    def test_datara_extended_roles_1(self, rmock):
+    def test_datara_extended_roles_foaf(self, rmock):
         # Converted manually from ISO-19139 using SEMICeu XSLT (tag geodcat-ap-2.0.0)
         url = mock_dcat(rmock, "datara--5a26b0f6-0ccf-46ad-ac58-734054b91977.rdf.xml")
         org = OrganizationFactory()
@@ -729,11 +729,14 @@ class DcatBackendTest(PytestOnlyDBTestCase):
         assert dataset.contact_points[0].email == "sav.bd@ign.fr"
         assert dataset.contact_points[0].role == "rightsHolder"
 
-        assert dataset.contact_points[1].name == "Administrateur de Données"
+        assert (
+            dataset.contact_points[1].name
+            == "Direction Régionale de l’Environnement de l’Aménagement et du Logement d'Auvergne-Rhône-Alpes (DREAL Auvergne-Rhône-Alpes)"
+        )
         assert dataset.contact_points[1].email == "sig.dreal-ara@developpement-durable.gouv.fr"
         assert dataset.contact_points[1].role == "user"
 
-    def test_datara_extended_roles_2(self, rmock):
+    def test_datara_extended_roles_vcard(self, rmock):
         # Converted manually from ISO-19139 using SEMICeu XSLT (tag geodcat-ap-2.0.0)
         url = mock_dcat(rmock, "datara--f40c3860-7236-4b30-a141-23b8ae33f7b2.rdf.xml")
         org = OrganizationFactory()
@@ -744,11 +747,14 @@ class DcatBackendTest(PytestOnlyDBTestCase):
         assert dataset is not None
         assert len(dataset.contact_points) == 3
 
-        assert dataset.contact_points[0].name == "Administrateur de Données"
+        assert (
+            dataset.contact_points[0].name
+            == "Direction Régionale de l’Environnement de l’Aménagement et du Logement d'Auvergne-Rhône-Alpes (DREAL Auvergne-Rhône-Alpes)"
+        )
         assert dataset.contact_points[0].email == "sig.dreal-ara@developpement-durable.gouv.fr"
         assert dataset.contact_points[0].role == "contact"
 
-        assert dataset.contact_points[1].name == "Jean-Michel GENIS"
+        assert dataset.contact_points[1].name == "Conservatoire Botanique National Alpin"
         assert dataset.contact_points[1].email == "jm.genis@cbn-alpin.fr"
         assert dataset.contact_points[1].role == "rightsHolder"
 
