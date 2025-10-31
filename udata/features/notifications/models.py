5401a9193c32ca8ed0835ec1a06ceedf331fc1f9
Nicolas Kempf Le Stanc <nicolas@conciergerie.dev>
2025-10-31 11:40:25 +0100

    feat(notifications): add base Notification model

---
+++ udata/features/notifications/models.py
@@ -0,0 +1,22 @@
+from mongoengine import NULLIFY
+
+from udata.api_fields import field, generate_fields
+from udata.core.user.models import User
+from udata.core.user.api_fields import user_ref_fields
+from udata.models import db
+from udata.mongo.datetime_fields import Datetimed
+
+
+@generate_fields()
+class Notification(Datetimed, db.Document):
+    meta = {"allow_inheritance": True}
+
+    id = field(db.AutoUUIDField(primary_key=True))
+    user = field(
+        db.ReferenceField(User, reverse_delete_rule=NULLIFY),
+        nested_fields=user_ref_fields,
+        readonly=True,
+        allow_null=True,
+        auditable=False,
+        filterable={},
+    )
