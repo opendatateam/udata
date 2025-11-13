a0c77ac2a4b1c96285475b71894c9a701e0d3cb1
Thibaud Dauce <git@hadibut.fr>
2025-11-13 14:49:57 +0100

    Fix datetime to json

--- udata/tests/api/test_security_api.py
+++ udata/tests/api/test_security_api.py
@@ -74,18 +74,17 @@ class SecurityAPITest(PytestOnlyAPITestCase):
         user = UserFactory(
             email="jane@example.org", password="password", confirmed_at=datetime.now()
         )
-        self.login(user)
-
-        response = self.post(
-            url_for("security.change_password"),
-            {
-                "password": "password",
-                "new_password": "New_password123",
-                "new_password_confirm": "New_password123",
-                "submit": True,
-            },
-        )
-        self.assertStatus(response, 200)
+        with self.api_user(user):
+            response = self.post(
+                url_for("security.change_password"),
+                {
+                    "password": "password",
+                    "new_password": "New_password123",
+                    "new_password_confirm": "New_password123",
+                    "submit": True,
+                },
+            )
+            self.assertStatus(response, 200)
 
     @pytest.mark.options(CAPTCHETAT_BASE_URL=None)
     def test_change_email_confirmation(self):
