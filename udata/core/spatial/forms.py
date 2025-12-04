4ad414b0cce1e8c8c706fabc91d56d968226a743
Thibaud Ollagnier <ThibaudDauce@users.noreply.github.com>
2025-12-04 10:48:47 +0100

    Update udata/core/spatial/forms.py
    Co-authored-by: maudetes <maudet.estelle@gmail.com>

--- udata/core/spatial/forms.py
+++ udata/core/spatial/forms.py
@@ -64,7 +64,7 @@ class GeomField(Field):
                     self.data = geojson.GeoJSON.to_instance(value)
             except Exception:
                 self.data = None
-                log.warning("Unable to parse GeoJSON: %s", value)
+                log.warning(f"Unable to parse GeoJSON: {value}")
                 raise validators.ValidationError(self.gettext("Not a valid GeoJSON"))
 
     def pre_validate(self, form):
