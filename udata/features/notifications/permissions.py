3d8b508fee7741b54c573d3f24c14c6a9c4080e9
Nicolas KEMPF <nicolas@conciergerie.dev>
2026-01-22 11:58:17 +0100

    feat(notification): add organization badges notifications (#3628)
    This PR adds notifications for all organization badges.
    
    It follows the same pattern as the existing email sent.

---
+++ udata/features/notifications/permissions.py
@@ -0,0 +1,9 @@
+from udata.auth import Permission, UserNeed
+
+
+class EditNotificationPermission(Permission):
+    """Permissions to edit a notification"""
+
+    def __init__(self, subject):
+        need = UserNeed(subject.user.fs_uniquifier)
+        super(EditNotificationPermission, self).__init__(need)
