15762115e425b714218365c288344ebe9145f92a
Thibaud Dauce <git@hadibut.fr>
2026-03-31 21:15:52 +0200

    fix: filter with multiple tags (test)

--- udata/tests/apiv2/test_reuses.py
+++ udata/tests/apiv2/test_reuses.py
@@ -1,11 +1,29 @@
-from udata.core.reuse.factories import ReuseFactory
+from udata.core.reuse.factories import VisibleReuseFactory
 from udata.tests.api import APITestCase
 
 
 class ReuseSearchAPIV2Test(APITestCase):
     def test_reuse_search_with_model_query_param(self):
         """Searching reuses with 'model' as query param should not crash."""
-        ReuseFactory.create_batch(3)
+        VisibleReuseFactory.create_batch(3)
 
         response = self.get("/api/2/reuses/search/?model=malicious")
         self.assert200(response)
+
+    def test_reuse_search_single_tag(self):
+        tag_reuse = VisibleReuseFactory(tags=["my-tag", "other"])
+        VisibleReuseFactory(tags=["unrelated"])
+
+        response = self.get("/api/2/reuses/search/?tag=my-tag")
+        self.assert200(response)
+        assert len(response.json["data"]) == 1
+        assert response.json["data"][0]["id"] == str(tag_reuse.id)
+
+    def test_reuse_search_multiple_tags(self):
+        tag_reuse = VisibleReuseFactory(tags=["my-tag", "other"])
+        VisibleReuseFactory(tags=["my-tag"])
+
+        response = self.get("/api/2/reuses/search/?tag=my-tag&tag=other")
+        self.assert200(response)
+        assert len(response.json["data"]) == 1
+        assert response.json["data"][0]["id"] == str(tag_reuse.id)
