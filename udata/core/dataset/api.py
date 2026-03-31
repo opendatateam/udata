ae17b386644cd61815474b6b6e36eb3cf60af7c6
maudetes <estelle.maudet@data.gouv.fr>
2026-03-31 11:46:40 +0200

    chore: add access_type filter on list api endpoints and rdf catalogs
    Deprecate is_restricted for dataservices

--- udata/core/dataset/api.py
+++ udata/core/dataset/api.py
@@ -33,6 +33,7 @@ from udata.api import API, api, errors
 from udata.api.parsers import ModelApiParser
 from udata.auth import admin_permission
 from udata.core import storages
+from udata.core.access_type.constants import AccessType
 from udata.core.badges import api as badges_api
 from udata.core.badges.models import Badge
 from udata.core.dataservices.models import Dataservice
@@ -112,6 +113,7 @@ class DatasetApiParser(ModelApiParser):
         self.parser.add_argument("geozone", type=str, location="args")
         self.parser.add_argument("granularity", type=str, location="args")
         self.parser.add_argument("temporal_coverage", type=str, location="args")
+        self.parser.add_argument("access_type", type=str, choices=list(AccessType), location="args")
         self.parser.add_argument("organization", type=str, location="args")
         # Uses __badges__ (not available_badges) so that users can still filter
         # by any existing badge, even hidden ones.
@@ -182,6 +184,8 @@ class DatasetApiParser(ModelApiParser):
                 temporal_coverage__start__gte=args["temporal_coverage"][:9],
                 temporal_coverage__start__lte=args["temporal_coverage"][11:],
             )
+        if args.get("access_type"):
+            datasets = datasets.filter(access_type=args["access_type"])
         if args.get("featured") is not None:
             datasets = datasets.filter(featured=args["featured"])
         if args.get("badge"):
