73462ab93246ca40d7c3d0fb01f8325825c4ffa0
Samuel-Zacharie FAURE <samuel.faure.dev@gmail.com>
2026-05-19 11:36:19 +0200

    Fix/cli (#3776)
    Misc CLI improvements, commit-per-commit

--- udata/harvest/actions.py
+++ udata/harvest/actions.py
@@ -96,7 +96,7 @@ def validate_source(source: HarvestSource, comment=None):
     source.validation.on = datetime.now(UTC)
     source.validation.comment = comment
     source.validation.state = VALIDATION_ACCEPTED
-    if current_user.is_authenticated:
+    if current_user and current_user.is_authenticated:
         source.validation.by = current_user._get_current_object()
     source.save()
     schedule(source, cron=current_app.config["HARVEST_DEFAULT_SCHEDULE"])
@@ -110,7 +110,7 @@ def reject_source(source: HarvestSource, comment):
     source.validation.on = datetime.now(UTC)
     source.validation.comment = comment
     source.validation.state = VALIDATION_REFUSED
-    if current_user.is_authenticated:
+    if current_user and current_user.is_authenticated:
         source.validation.by = current_user._get_current_object()
     source.save()
     signals.harvest_source_refused.send(source)
