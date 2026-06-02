627f9843343c6ce03a19d4ab0784ea4076887c87
maudetes <estelle.maudet@data.gouv.fr>
2026-06-02 16:45:07 +0200

    Partially revert "fix(harvest): normalize tag values in TagListField to match harvester input (#3782)"
    This reverts commit 5138653b450a8f2bada7f6c2591019ace33e4438 except for added tests.

--- udata/mongo/taglist_field.py
+++ udata/mongo/taglist_field.py
@@ -1,4 +1,5 @@
 from mongoengine.fields import ListField, StringField
+from slugify import slugify
 
 from udata import tags
 from udata.i18n import lazy_gettext as _
@@ -19,7 +20,7 @@ class TagListField(ListField):
             return []
 
     def clean(self, value):
-        return sorted({n for n in (tags.normalize(v) for v in value) if n})
+        return sorted(list(set([slugify(v, to_lower=True) for v in value])))
 
     def to_python(self, value):
         return super(TagListField, self).to_python(self.clean(value))
@@ -28,8 +29,6 @@ class TagListField(ListField):
         return super(TagListField, self).to_mongo(self.clean(value))
 
     def validate(self, values):
-        if values is not None:
-            values[:] = self.clean(values)
         super(TagListField, self).validate(values)
 
         for tag in values:
