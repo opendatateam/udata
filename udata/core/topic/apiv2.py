8f9b662f6b6959177a4a4ed9dda961571fb256e7
Alexandre Bulté <alexandre@bulte.net>
2026-07-23 14:15:58 +0200

    fix(topics): check element is part of Topic

--- udata/core/topic/apiv2.py
+++ udata/core/topic/apiv2.py
@@ -270,7 +270,7 @@ class TopicElementAPI(API):
         if not TopicEditPermission(topic).can():
             apiv2.abort(403, "Forbidden")
 
-        element = TopicElement.objects.get_or_404(pk=element_id)
+        element = TopicElement.objects.get_or_404(pk=element_id, topic=topic)
         element.delete()
 
         return None, 204
@@ -287,7 +287,7 @@ class TopicElementAPI(API):
         if not TopicEditPermission(topic).can():
             apiv2.abort(403, "Forbidden")
 
-        element = TopicElement.objects.get_or_404(pk=element_id)
+        element = TopicElement.objects.get_or_404(pk=element_id, topic=topic)
         patch_and_save(element, request)
 
         return element
